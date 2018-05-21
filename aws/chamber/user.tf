25d0b68a56f71b0d43958b5ef56d770e50f69ac8
Igor Rodionov <goruha@gmail.com>
2018-05-22 00:45:09 +0600

    Rename chamber structur

--- aws/chamber/user.tf
+++ aws/chamber/user.tf
@@ -1,11 +1,3 @@
-module "chamber_kms_key" {
-  source      = "git::https://github.com/cloudposse/terraform-aws-kms-key.git?ref=tags/0.1.0"
-  namespace   = "${module.identity.namespace}"
-  stage       = "${module.identity.stage}"
-  name        = "chamber"
-  description = "KMS key for chamber"
-}
-
 # Chamber user for CI/CD systems that cannot leverage IAM instance profiles
 # https://docs.aws.amazon.com/systems-manager/latest/userguide/sysman-paramstore-access.html
 module "chamber_user" {
@@ -16,26 +8,6 @@ module "chamber_user" {
   attributes    = ["codefresh"]
   kms_key_arn   = "${module.chamber_kms_key.key_arn}"
   ssm_resources = ["${format("arn:aws:ssm:%s:%s:parameter/kops/*", module.identity.aws_region, module.identity.account_id)}"]
-}
-
-output "chamber_kms_key_arn" {
-  value       = "${module.chamber_kms_key.key_arn}"
-  description = "KMS key ARN"
-}
-
-output "chamber_kms_key_id" {
-  value       = "${module.chamber_kms_key.key_id}"
-  description = "KMS key ID"
-}
-
-output "chamber_kms_key_alias_arn" {
-  value       = "${module.chamber_kms_key.alias_arn}"
-  description = "KMS key alias ARN"
-}
-
-output "chamber_kms_key_alias_name" {
-  value       = "${module.chamber_kms_key.alias_name}"
-  description = "KMS key alias name"
 }
 
 output "chamber_user_name" {
