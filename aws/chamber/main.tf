d2d91ab10dcc3fd497fd39aa499b071b038ba8bc
Igor Rodionov <goruha@gmail.com>
2018-05-22 01:19:32 +0600

    Merge main and output (#2)
    * Merge main and output
    
    * Fix structure
    
    * Rename chamber structur

--- aws/chamber/main.tf
+++ aws/chamber/main.tf
@@ -15,23 +15,3 @@ provider "aws" {
 module "identity" {
   source = "git::git@github.com:cloudposse/terraform-aws-account-metadata.git?ref=init"
 }
-
-module "chamber_kms_key" {
-  source      = "git::https://github.com/cloudposse/terraform-aws-kms-key.git?ref=tags/0.1.0"
-  namespace   = "${module.identity.namespace}"
-  stage       = "${module.identity.stage}"
-  name        = "chamber"
-  description = "KMS key for chamber"
-}
-
-# Chamber user for CI/CD systems that cannot leverage IAM instance profiles
-# https://docs.aws.amazon.com/systems-manager/latest/userguide/sysman-paramstore-access.html
-module "chamber_user" {
-  source        = "git::https://github.com/cloudposse/terraform-aws-iam-chamber-user.git?ref=tags/0.1.3"
-  namespace     = "${module.identity.namespace}"
-  stage         = "${module.identity.stage}"
-  name          = "chamber"
-  attributes    = ["codefresh"]
-  kms_key_arn   = "${module.chamber_kms_key.key_arn}"
-  ssm_resources = ["${format("arn:aws:ssm:%s:%s:parameter/kops/*", module.identity.aws_region, module.identity.account_id)}"]
-}
