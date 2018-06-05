1d6bd990f11f36fd98d97c392fe938a918a66561
aknysh <andriy.knysh@gmail.com>
2018-06-05 11:02:10 -0400

    Add `organization` project

---
+++ aws/organization/main.tf
@@ -0,0 +1,48 @@
+# NOTE: Organization can only be created from the master account
+# https://www.terraform.io/docs/providers/aws/r/organizations_organization.html
+
+terraform {
+  required_version = ">= 0.11.2"
+
+  backend "s3" {}
+}
+
+variable "aws_assume_role_arn" {
+  type = "string"
+}
+
+variable "organization_feature_set" {
+  type        = "string"
+  default     = "ALL"
+  description = "Specify `ALL` (default) or `CONSOLIDATED_BILLING`"
+}
+
+provider "aws" {
+  assume_role {
+    role_arn = "${var.aws_assume_role_arn}"
+  }
+}
+
+resource "aws_organizations_organization" "default" {
+  feature_set = "${var.organization_feature_set}"
+}
+
+output "organization_id" {
+  value = "${aws_organizations_organization.default.id}"
+}
+
+output "organization_arn" {
+  value = "${aws_organizations_organization.default.arn}"
+}
+
+output "organization_master_account_id" {
+  value = "${aws_organizations_organization.default.master_account_id}"
+}
+
+output "organization_master_account_arn" {
+  value = "${aws_organizations_organization.default.master_account_arn}"
+}
+
+output "organization_master_account_email" {
+  value = "${aws_organizations_organization.default.master_account_email}"
+}
