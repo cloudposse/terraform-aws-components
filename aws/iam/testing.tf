620e757ca4ddb7d5923d9c10c74364111a36cc05
Andriy Knysh <aknysh@users.noreply.github.com>
2018-06-08 17:15:29 -0400

    Add `testing` stage (#12)

---
+++ aws/iam/testing.tf
@@ -0,0 +1,19 @@
+variable "testing_account_id" {
+  type        = "string"
+  description = "Testing account ID"
+}
+
+variable "testing_account_user_names" {
+  type        = "list"
+  description = "IAM user names to grant access to Testing account"
+}
+
+# Provision group access to testing account. Careful! Very few people, if any should have access to this account.
+module "organization_access_group_testing" {
+  source            = "git::https://github.com/cloudposse/terraform-aws-organization-access-group.git?ref=tags/0.1.2"
+  namespace         = "${var.namespace}"
+  stage             = "testing"
+  name              = "admin"
+  user_names        = ["${var.testing_account_user_names}"]
+  member_account_id = "${var.testing_account_id}"
+}
