996e47e0eaafaa282078ff202702d7fd5b6d4ccb
aknysh <andriy.knysh@gmail.com>
2018-06-10 10:18:57 -0400

    Add missing `source` for `cloudtrail_s3_bucket`

--- aws/audit-cloudtrail/main.tf
+++ aws/audit-cloudtrail/main.tf
@@ -49,6 +49,7 @@ module "cloudtrail" {
 }
 
 module "cloudtrail_s3_bucket" {
+  source    = "git::https://github.com/cloudposse/terraform-aws-cloudtrail-s3-bucket.git?ref=tags/0.1.1"
   namespace = "${var.namespace}"
   stage     = "${var.stage}"
   name      = "${var.name}"
