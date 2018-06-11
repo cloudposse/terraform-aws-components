6a6fe4722f8cf672b09fd13cfcce0413ea9da095
Andriy Knysh <aknysh@users.noreply.github.com>
2018-06-10 21:24:06 -0400

    Add `parent-testing-ns` (#13)
    * Add `parent-testing-ns.tf`
    
    * Add missing `source` for `cloudtrail_s3_bucket`

---
+++ aws/root-dns/parent-testing-ns.tf
@@ -0,0 +1,11 @@
+variable "testing_name_servers" {
+  type = "list"
+}
+
+resource "aws_route53_record" "testing_dns_zone_ns" {
+  zone_id = "${aws_route53_zone.parent_dns_zone.zone_id}"
+  name    = "testing"
+  type    = "NS"
+  ttl     = "30"
+  records = ["${var.testing_name_servers}"]
+}
