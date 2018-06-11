065ed8428be0e92d349146d073b279bae1d4978f
Igor Rodionov <goruha@gmail.com>
2018-06-11 23:55:05 +0600

    Added install ts-state script

---
+++ aws/tfstate-backend/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+sed -i "s/backend/#backend/" main.tf
+
+init-terraform
+terraform plan
+
+export TF_BUCKET=$(echo "yes" | terraform apply | grep -o -e "tfstate_backend_s3_bucket_id\s=\s.*" | cut -d ' ' -f 3)
+export TF_BUCKET_REGION=${TF_VAR_region}
+
+sed -i "s/#backend/backend/" main.tf
+
+s3 fstab "${TF_BUCKET}" '/' '/secrets/tf'
+
+echo "yes" | init-terraform
+
+
+echo "Add to the Geodesic Module Dockerfile following"
+echo "#----------------------------------------------"
+echo "ENV TF_BUCKET=\"${TF_BUCKET}\""
+echo "ENV TF_BUCKET_REGION=\"${TF_BUCKET_REGION}\""
+echo "#----------------------------------------------"
+echo "And rebuild the module"
