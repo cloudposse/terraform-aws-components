4210eb3cf2188cc1776bfd98f22fb3a70cd84983
Igor Rodionov <goruha@gmail.com>
2018-06-12 19:03:12 +0600

    Address PRs

--- aws/tfstate-backend/install.sh
+++ aws/tfstate-backend/install.sh
@@ -13,12 +13,10 @@ export TF_BUCKET_REGION=${TF_VAR_region}
 
 ## Spaces before and after `backend` required to select right word, because `backend` appears 3 times in main.tf
 sed -i "s/ #backend / backend /" main.tf
-sed -i "s/ #role_arn / role_arn /" main.tf
-
-s3 fstab "${TF_BUCKET}" '/' '/secrets/tf'
 
 echo "yes" | init-terraform
 
+sed -i "s/ #role_arn / role_arn /" main.tf
 
 echo "Add to the Geodesic Module Dockerfile following"
 echo "#----------------------------------------------"
