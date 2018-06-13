98da1ff078b4c1e4daabc47db5647e8c75b8d270
Igor Rodionov <goruha@gmail.com>
2018-06-13 16:07:57 +0600

    Added terraform provision

---
+++ aws/iam/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+## Spaces before and after `backend` required to select right word, because `backend` appears 3 times in main.tf
+sed -i "s/ role_arn / #role_arn /" main.tf
+
+init-terraform
+terraform plan
+echo "yes" | init-terraform
+
+sed -i "s/ #role_arn / role_arn /" main.tf
+
+echo "IAM provisioned"
