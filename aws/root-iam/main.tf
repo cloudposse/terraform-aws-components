f50ecdfab974aafdb1cdbd42497e60e05f5ce272
Andriy Knysh <aknysh@users.noreply.github.com>
2018-06-23 09:58:53 +0300

    Separate `iam` into `root-iam` and `iam` (#18)

---
+++ aws/root-iam/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 0.11.2"
+
+  backend "s3" {}
+}
+
+variable "aws_assume_role_arn" {
+  type = "string"
+}
+
+variable "namespace" {
+  type        = "string"
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
+
+provider "aws" {
+  assume_role {
+    role_arn = "${var.aws_assume_role_arn}"
+  }
+}
