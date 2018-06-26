92bfaa3a0060eba517ea57735d1dbc693ec86a72
Igor Rodionov <goruha@gmail.com>
2018-06-26 18:21:18 +0600

    Address comments

--- aws/tfstate-backend/install.sh
+++ aws/tfstate-backend/install.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
 
-## Spaces before and after `backend` required to select right word, because `backend` appears 3 times in main.tf
-sed -i "s/ backend / #backend /" main.tf
-sed -i "s/ role_arn / #role_arn /" main.tf
+DISABLE_ROLE_ARN=${DISABLE_ROLE_ARN:-0}
 
+sed -Ei 's/^(\s+backend\s+)/#\1/' main.tf
+[ "${DISABLE_ROLE_ARN}" == "0" ] || sed -Ei 's/^(\s+role_arn\s+)/#\1/' main.tf
 
 init-terraform
-terraform plan
+echo "yes" | terraform apply
 
-export TF_BUCKET=$(echo "yes" | terraform apply | grep -o -e "tfstate_backend_s3_bucket_id\s=\s.*" | cut -d ' ' -f 3)
+export TF_BUCKET=$(terraform output -json | jq -r .tfstate_backend_s3_bucket_id.value)
+export TF_DYNAMODB_TABLE=$(terraform output -json | jq -r .tfstate_backend_dynamodb_table_id.value)
 export TF_BUCKET_REGION=${TF_VAR_region}
 
-## Spaces before and after `backend` required to select right word, because `backend` appears 3 times in main.tf
-sed -i "s/ #backend / backend /" main.tf
+sed -Ei 's/^#(\s+backend\s+)/\1/' main.tf
 
 echo "yes" | init-terraform
 
-sed -i "s/ #role_arn / role_arn /" main.tf
+[ "${DISABLE_ROLE_ARN}" == "0" ] || sed -Ei 's/^#(\s+role_arn\s+)/\1/' main.tf
 
-echo "Add to the Geodesic Module Dockerfile following"
+echo "Add the following to the Geodesic Module's Dockerfile:"
 echo "#----------------------------------------------"
 echo "ENV TF_BUCKET=\"${TF_BUCKET}\""
 echo "ENV TF_BUCKET_REGION=\"${TF_BUCKET_REGION}\""
+echo "ENV TF_DYNAMODB_TABLE=\"${TF_DYNAMODB_TABLE}\""
 echo "#----------------------------------------------"
 echo "And rebuild the module"
