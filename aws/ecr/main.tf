ce857e66b1b9806f66831aae6807b4f261129710
Igor Rodionov <goruha@gmail.com>
2018-07-02 23:24:27 +0600

    Added ecr (#20)
    * Added ecr
    
    * Address PR comments
    
    * Address PR
    
    * Address PR

---
+++ aws/ecr/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 0.11.2"
+
+  backend "s3" {}
+}
+
+variable "aws_assume_role_arn" {
+  type = "string"
+}
+
+variable "namespace" {
+  type        = "string"
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
+
+variable "stage" {
+  type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+}
+
+variable "region" {
+  type        = "string"
+  description = "AWS region"
+}
+
+variable "zone_name" {
+  type        = "string"
+  description = "DNS zone name"
+}
+
+variable "repository_name" {
+  description = "Repository name"
+}
+
+provider "aws" {
+  assume_role {
+    role_arn = "${var.aws_assume_role_arn}"
+  }
+}
