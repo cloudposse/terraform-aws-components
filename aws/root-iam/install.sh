931b2b1d40b598ed740efdefa096692f2c3e7e7d
Igor Rodionov <goruha@gmail.com>
2018-07-02 23:12:28 +0600

    Address PR comments

--- aws/root-iam/install.sh
+++ aws/root-iam/install.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 ## Spaces before and after `backend` required to select right word, because `backend` appears 3 times in main.tf
-sed -i "s/ role_arn / #role_arn /" main.tf
+sed -Ei 's/^(\s+role_arn\s+)/#\1/' main.tf
 
 init-terraform
-terraform plan
 echo "yes" | terraform apply
 
-sed -i "s/ #role_arn / role_arn /" main.tf
+sed -Ei 's/^#(\s+role_arn\s+)/\1/' main.tf
 
 echo "Root IAM Role provisioned"
