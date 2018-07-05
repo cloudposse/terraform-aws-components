ee38a40c0ba3481a27a8863286b0369b909bf97c
Igor Rodionov <goruha@gmail.com>
2018-07-06 02:18:50 +0600

    Fix root dns (#22)

--- aws/root-dns/parent-local-ns.tf
+++ aws/root-dns/parent-local-ns.tf
@@ -3,6 +3,7 @@ variable "local_name_servers" {
 }
 
 resource "aws_route53_record" "local_dns_zone_ns" {
+  count   = "${signum(length(var.local_name_servers))}"
   zone_id = "${aws_route53_zone.parent_dns_zone.zone_id}"
   name    = "local"
   type    = "NS"
--- aws/root-dns/parent-staging-ns.tf
+++ aws/root-dns/parent-staging-ns.tf
@@ -3,6 +3,7 @@ variable "staging_name_servers" {
 }
 
 resource "aws_route53_record" "staging_dns_zone_ns" {
+  count   = "${signum(length(var.staging_name_servers))}"
   zone_id = "${aws_route53_zone.parent_dns_zone.zone_id}"
   name    = "staging"
   type    = "NS"
