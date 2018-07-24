c9c395a0589c3bc66c03472df0e33be19f5af06d
aknysh <andriy.knysh@gmail.com>
2018-07-24 16:04:48 -0400

    Update `kops_metadata` version

--- aws/backing-services/kops-metadata.tf
+++ aws/backing-services/kops-metadata.tf
@@ -1,4 +1,4 @@
 module "kops_metadata" {
-  source   = "git::https://github.com/cloudposse/terraform-aws-kops-metadata.git?ref=tags/0.2.0"
+  source   = "git::https://github.com/cloudposse/terraform-aws-kops-metadata.git?ref=tags/0.1.2"
   dns_zone = "${var.region}.${var.zone_name}"
 }
