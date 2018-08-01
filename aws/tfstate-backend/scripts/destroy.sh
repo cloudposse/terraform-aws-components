52e84bdf5de60e566e0ecb0ea618f8d37f98a57d
Erik Osterman <e@osterman.com>
2018-07-31 22:11:29 -0700

    Improve annotations

--- aws/tfstate-backend/scripts/destroy.sh
+++ aws/tfstate-backend/scripts/destroy.sh
@@ -4,19 +4,19 @@ rm -rf .terraform terraform.tfstate
 # Init terraform with S3 state enabled. Assumes state was previously initialized.
 init-terraform
 
-# Unmount remote bucket
+# Unmount remote bucket (if mounted)
 s3 unmount
 
-# Store the current state
+# Store the current state so we can destroy resources without catch-22
 terraform state pull > terraform.tfstate
 
-# Delete current state folder
+# Delete current state folder to remove all hints of local & remote state
 rm -rf .terraform
 
-# Disable S3 state backend
+# Disable S3 state backend so that we use local state file
 sed -Ei 's/^(\s+backend\s+)/#\1/' main.tf
 
-# Reintialize TF state without backend
+# Reintialize TF state without backend, using local `terraform.tfstate`
 terraform init
 
 # Destroy terraform state. Note, only buckets that were created with `force_destroy=true` will successfully be destroyed.
