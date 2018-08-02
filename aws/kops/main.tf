2ba2ae2aabe9c3b1df41c995b61aff4a2437bd10
Erik Osterman <erik@cloudposse.com>
2018-08-01 18:35:43 -0700

    Parameterize kops setup

--- aws/kops/main.tf
+++ aws/kops/main.tf
@@ -18,6 +18,12 @@ variable "stage" {
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
 }
 
+variable "stage" {
+  type        = "string"
+  description = "Name  (e.g. `kops`)"
+  default     = "kops"
+}
+
 variable "region" {
   type        = "string"
   description = "AWS region"
@@ -26,6 +32,12 @@ variable "region" {
 variable "zone_name" {
   type        = "string"
   description = "DNS zone name"
+}
+
+variable "ssh_public_key_path" {
+  type        = "string"
+  description = "SSH public key path to write master public/private key pair for cluster"
+  default     = ""
 }
 
 provider "aws" {
@@ -38,7 +50,8 @@ module "kops_state_backend" {
   source           = "git::https://github.com/cloudposse/terraform-aws-kops-state-backend.git?ref=tags/0.1.3"
   namespace        = "${var.namespace}"
   stage            = "${var.stage}"
-  name             = "kops-state"
+  name             = "${var.name}"
+  attributes       = "state"
   cluster_name     = "${var.region}"
   parent_zone_name = "${var.zone_name}"
   zone_name        = "$${name}.$${parent_zone_name}"
@@ -49,7 +62,8 @@ module "ssh_key_pair" {
   source              = "git::https://github.com/cloudposse/terraform-aws-key-pair.git?ref=tags/0.2.3"
   namespace           = "${var.namespace}"
   stage               = "${var.stage}"
-  name                = "kops-${var.region}"
-  ssh_public_key_path = "/secrets/tf/ssh"
+  name                = "${var.name}"
+  attributes          = "${var.region}"
+  ssh_public_key_path = "${var.ssh_public_key_path}"
   generate_ssh_key    = "true"
 }
