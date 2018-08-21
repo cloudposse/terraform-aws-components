54fdada916de8cd2a934d95c84d259b26e07203c
Igor Rodionov <goruha@gmail.com>
2018-08-21 18:43:26 +0600

    Split backing services

---
+++ aws/backing-services-elasticsearch/vpc.tf
@@ -0,0 +1,24 @@
+locals {
+  name = "backing-services"
+}
+
+module "vpc" {
+  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.3.3"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  name       = "${local.name}"
+  cidr_block = "10.0.0.0/16"
+}
+
+module "subnets" {
+  source              = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.3.4"
+  availability_zones  = ["${data.aws_availability_zones.available.names}"]
+  namespace           = "${var.namespace}"
+  stage               = "${var.stage}"
+  name                = "${local.name}"
+  region              = "${var.region}"
+  vpc_id              = "${module.vpc.vpc_id}"
+  igw_id              = "${module.vpc.igw_id}"
+  cidr_block          = "${module.vpc.vpc_cidr_block}"
+  nat_gateway_enabled = "true"
+}
--- aws/backing-services-vpc/kops-metadata.tf
+++
@@ -1,4 +0,0 @@
-module "kops_metadata" {
-  source   = "git::https://github.com/cloudposse/terraform-aws-kops-metadata.git?ref=tags/0.2.0"
-  dns_zone = "${var.region}.${var.zone_name}"
-}
