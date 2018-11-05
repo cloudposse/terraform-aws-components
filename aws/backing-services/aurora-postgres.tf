681dcb2f31844255c6c5cc90640c8fc907152cee
aknysh <andriy.knysh@gmail.com>
2018-11-05 15:26:13 -0500

    Add `aurora-mysql`

--- aws/backing-services/aurora-postgres.tf
+++ aws/backing-services/aurora-postgres.tf
@@ -1,3 +1,9 @@
+variable "POSTGRES_NAME" {
+  type        = "string"
+  description = "Name of the application, e.g. `app` or `analytics`"
+  default     = "postgres"
+}
+
 # Don't use `admin`
 # ("MasterUsername admin cannot be used as it is a reserved word used by the engine")
 variable "POSTGRES_ADMIN_NAME" {
@@ -38,24 +44,23 @@ variable "POSTGRES_CLUSTER_ENABLED" {
 }
 
 module "aurora_postgres" {
-  source             = "git::https://github.com/cloudposse/terraform-aws-rds-cluster.git?ref=tags/0.3.5"
-  namespace          = "${var.namespace}"
-  stage              = "${var.stage}"
-  name               = "postgres"
-  engine             = "aurora-postgresql"
-  cluster_family     = "aurora-postgresql9.6"
-  instance_type      = "${var.POSTGRES_INSTANCE_TYPE}"
-  cluster_size       = "${var.POSTGRES_CLUSTER_SIZE}"
-  admin_user         = "${var.POSTGRES_ADMIN_NAME}"
-  admin_password     = "${var.POSTGRES_ADMIN_PASSWORD}"
-  db_name            = "${var.POSTGRES_DB_NAME}"
-  db_port            = "5432"
-  vpc_id             = "${module.vpc.vpc_id}"
-  availability_zones = ["${data.aws_availability_zones.available.names}"]
-  subnets            = ["${module.subnets.private_subnet_ids}"]
-  zone_id            = "${var.zone_id}"
-  security_groups    = ["${module.kops_metadata.nodes_security_group_id}"]
-  enabled            = "${var.POSTGRES_CLUSTER_ENABLED}"
+  source          = "git::https://github.com/cloudposse/terraform-aws-rds-cluster.git?ref=tags/0.7.0"
+  namespace       = "${var.namespace}"
+  stage           = "${var.stage}"
+  name            = "${var.POSTGRES_NAME}"
+  engine          = "aurora-postgresql"
+  cluster_family  = "aurora-postgresql9.6"
+  instance_type   = "${var.POSTGRES_INSTANCE_TYPE}"
+  cluster_size    = "${var.POSTGRES_CLUSTER_SIZE}"
+  admin_user      = "${var.POSTGRES_ADMIN_NAME}"
+  admin_password  = "${var.POSTGRES_ADMIN_PASSWORD}"
+  db_name         = "${var.POSTGRES_DB_NAME}"
+  db_port         = "5432"
+  vpc_id          = "${module.vpc.vpc_id}"
+  subnets         = ["${module.subnets.private_subnet_ids}"]
+  zone_id         = "${var.zone_id}"
+  security_groups = ["${module.kops_metadata.nodes_security_group_id}"]
+  enabled         = "${var.POSTGRES_CLUSTER_ENABLED}"
 }
 
 output "aurora_postgres_database_name" {
