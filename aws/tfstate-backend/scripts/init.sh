0aaa1d45cf954f90cf17d3adac389222f72ff5ac
Erik Osterman <erik@cloudposse.com>
2018-12-20 13:49:37 -0800

    [tfstate-backend] Update scripts to use true/false (#60)
    * Update logic

--- aws/tfstate-backend/scripts/init.sh
+++ aws/tfstate-backend/scripts/init.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 # This script automates the cold-start process of provisioning the Terraform state backend using terraform
 
-DISABLE_ROLE_ARN=${DISABLE_ROLE_ARN:-0}
+set -e
+
+# We use this variable consistently to pass the role we wish to assume in our root modules
+export TF_VAR_aws_assume_role_arn="${TF_VAR_aws_assume_role_arn:-false}"
+
+DISABLE_ROLE_ARN=${DISABLE_ROLE_ARN:-false}
 
 # Start from a clean slate
 rm -rf .terraform terraform.tfstate
 
-# Disable S3 backend
+# Disable S3 backend. We'll import state afterwards.
 sed -Ei 's/^(\s+backend\s+)/#\1/' main.tf
 
 # Disable Role ARN (necessary for root account on cold-start)
-[ "${DISABLE_ROLE_ARN}" == "0" ] || sed -Ei 's/^(\s+role_arn\s+)/#\1/' main.tf
+[ "${DISABLE_ROLE_ARN}" == "true" ] || sed -Ei 's/^(\s+role_arn\s+)/#\1/' main.tf
 
 # Initialize terraform modules and providers
 init-terraform
 
 # Provision S3 bucket and dynamodb tables
-terraform apply -auto-approve
+terraform apply -auto-approve -input=false
 
-export TF_BUCKET=$(terraform output -json | jq -r .tfstate_backend_s3_bucket_id.value)
-export TF_DYNAMODB_TABLE=$(terraform output -json | jq -r .tfstate_backend_dynamodb_table_id.value)
+export TF_BUCKET=$(terraform output tfstate_backend_s3_bucket_id)
+export TF_DYNAMODB_TABLE=$(terraform output tfstate_backend_dynamodb_table_id)
 export TF_BUCKET_REGION=${TF_VAR_region}
 
 # Re-enable S3 backend
@@ -29,7 +34,7 @@ sed -Ei 's/^#(\s+backend\s+)/\1/' main.tf
 echo "yes" | init-terraform
 
 # Re-enable Role ARN
-[ "${DISABLE_ROLE_ARN}" == "0" ] || sed -Ei 's/^#(\s+role_arn\s+)/\1/' main.tf
+[ "${DISABLE_ROLE_ARN}" == "true" ] || sed -Ei 's/^#(\s+role_arn\s+)/\1/' main.tf
 
 # Describe how to use the S3/DynamoDB resources with Geodesic
 echo "Add the following to the Geodesic Module's Dockerfile:"
