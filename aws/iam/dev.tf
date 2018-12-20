3914d3b8b8beb4f3cab02cb2074fc549cba25462
Josh Myers <joshuajmyers@gmail.com>
2018-12-20 22:46:32 +0000

    [iam] Use account remote state to fetch sub account IDs (#63)
    * Use account remote state to fetch sub account IDs
    
    So that we don’t need to hardcode these as vars in the top root
    Dockerfile.
    
    * avoid sprawl of variable names. upgrde org module.
    
    * add accounts and enabled flag
    
    * fix typo
    
    * Fix descriptions

--- aws/iam/dev.tf
+++ aws/iam/dev.tf
@@ -1,20 +1,17 @@
-variable "dev_account_id" {
-  type        = "string"
-  description = "Dev account ID"
-}
-
 variable "dev_account_user_names" {
   type        = "list"
-  description = "IAM user names to grant access to Dev account"
+  description = "IAM user names to grant access to the `dev` account"
+  default     = []
 }
 
 # Provision group access to dev account
 module "organization_access_group_dev" {
-  source            = "git::https://github.com/cloudposse/terraform-aws-organization-access-group.git?ref=tags/0.1.3"
+  source            = "git::https://github.com/cloudposse/terraform-aws-organization-access-group.git?ref=tags/0.2.1"
+  enabled           = "${contains(var.accounts_enabled, "dev") == true ? "true" : "false"}"
   namespace         = "${var.namespace}"
   stage             = "dev"
   name              = "admin"
-  user_names        = ["${var.dev_account_user_names}"]
-  member_account_id = "${var.dev_account_id}"
+  user_names        = "${var.dev_account_user_names}"
+  member_account_id = "${data.terraform_remote_state.accounts.dev_account_id}"
   require_mfa       = "true"
 }
--- aws/iam/main.tf
+++ aws/iam/main.tf
@@ -4,17 +4,19 @@ terraform {
   backend "s3" {}
 }
 
-variable "aws_assume_role_arn" {
-  type = "string"
-}
-
-variable "namespace" {
-  type        = "string"
-  description = "Namespace (e.g. `cp` or `cloudposse`)"
-}
-
 provider "aws" {
   assume_role {
     role_arn = "${var.aws_assume_role_arn}"
   }
 }
+
+data "terraform_remote_state" "accounts" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.namespace}-${var.stage}-terraform-state"
+    key    = "accounts/terraform.tfstate"
+  }
+}
+
+locals {}
