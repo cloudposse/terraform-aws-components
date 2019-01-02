8c1a1ec07acd0d84347a4a3ee86edcd95f736136
Erik Osterman <erik@cloudposse.com>
2019-01-01 18:47:16 -0800

    Write kops settings to chamber

---
+++ aws/kops/subnets/subnets/variables.tf
@@ -0,0 +1,15 @@
+variable "iprange" {
+  default = "172.20.32.0/16"
+}
+
+variable "newbits" {
+  default = "3"
+}
+
+variable "netnum" {
+  default = "0"
+}
+
+variable "subnet_count" {
+  default = "5"
+}
