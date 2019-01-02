6e567edfb08a8911450c67b3258797c877a09e3b
Andriy Knysh <aknysh@users.noreply.github.com>
2019-01-02 08:59:45 -0800

    Update aws/kops/variables.tf
    Co-Authored-By: osterman <erik@cloudposse.com>

--- aws/kops/variables.tf
+++ aws/kops/variables.tf
@@ -95,5 +95,6 @@ variable "utility_subnets_netnum" {
 }
 
 variable "chamber_service" {
-  default = ""
+  default     = ""
+  description = "`chamber` service name. See [chamber usage](https://github.com/segmentio/chamber#usage) for more details"
 }
