5d2918da3548a114df2b02469b79e4d1e31b5c95
Erik Osterman <erik@cloudposse.com>
2019-01-01 19:42:14 -0800

    remove dupe

--- aws/kops/subnets/subnets/main.tf
+++
@@ -1,7 +0,0 @@
-data "null_data_source" "subnets" {
-  count = "${var.subnet_count}"
-
-  inputs = {
-    cidr = "${cidrsubnet(var.iprange, var.newbits, var.netnum + count.index)}"
-  }
-}
--- aws/kops/subnets/subnets/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "iprange" {
-  value = "${var.iprange}"
-}
-
-output "cidrs" {
-  value = "${join(",", data.null_data_source.subnets.*.outputs.cidr)}"
-}
