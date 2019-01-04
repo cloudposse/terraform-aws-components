848d5c71aceacb3fdb91691a89ec93de92ff2008
Andriy Knysh <aknysh@users.noreply.github.com>
2019-01-04 00:54:09 -0800

    Update aws/kops/main.tf
    Co-Authored-By: osterman <erik@cloudposse.com>

--- aws/kops/main.tf
+++ aws/kops/main.tf
@@ -147,7 +147,7 @@ resource "aws_ssm_parameter" "kops_utility_subnets" {
 resource "aws_ssm_parameter" "kops_non_masquerade_cidr" {
   name        = "${format(var.chamber_parameter_name, local.chamber_service, "kops_non_masquerade_cidr")}"
   value       = "${var.kops_non_masquerade_cidr}"
-  description = "Kops utility subnet CIDRs"
+  description = "The CIDR range for Pod IPs"
   type        = "String"
   overwrite   = "true"
 }
