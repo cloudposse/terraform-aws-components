e6f8f76cc472b3985ce04b28a6d9bbda33a41b06
Erik Osterman <erik@cloudposse.com>
2019-01-06 22:29:16 -0800

    Support cross-account peering

---
+++ aws/vpc-peering/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_assume_role_arn" {}
+
+variable "enabled" {
+  type        = "string"
+  description = "Whether to create the resources. Set to `false` to prevent the module from creating any resources"
+  default     = "true"
+}
+
+variable "namespace" {
+  type        = "string"
+  description = "Namespace (e.g. `eg` or `cp`)"
+}
+
+variable "stage" {
+  type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+}
+
+variable "name" {
+  type        = "string"
+  description = "Application or solution name (e.g. `app`)"
+  default     = "vpc-peering"
+}
