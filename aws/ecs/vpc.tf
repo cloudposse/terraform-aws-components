f51bd341d4d9e15489f49ee895d78437a1ead68e
Josh Myers <joshuajmyers@gmail.com>
2019-01-10 16:50:02 +0000

    Add missing limit_availability_zones var
    I’m not 100% on the intent of this code given that it was not using
    the number provided, but if any non empty string is given to the
    limit_availbility_zones variable e.g. “foo”, we would end up with a
    hardcoded 2 AZs. Likely due to terrible Terraform types.

--- aws/ecs/vpc.tf
+++ aws/ecs/vpc.tf
@@ -1,3 +1,9 @@
+variable "limit_availability_zones" {
+  type        = "string"
+  description = "Whether we should limit the number of AZs in use to 2"
+  default     = "true"
+}
+
 locals {
   name = "${var.region}"
 }
@@ -11,7 +17,7 @@ module "vpc" {
 }
 
 locals {
-  max_availability_zones = "${length(var.limit_availability_zones) == 0 ? 2 : length(data.aws_availability_zones.available.names)}"
+  max_availability_zones = "${var.limit_availability_zones == "true" ? 2 : length(data.aws_availability_zones.available.names)}"
   availability_zones     = "${slice(data.aws_availability_zones.available.names, 0, local.max_availability_zones)}"
 }
 
