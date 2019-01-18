02c367a00f31327e27ce02510b5e5f80ba5a6680
Erik Osterman <erik@cloudposse.com>
2019-01-17 18:14:54 -0800

    parameterize

--- aws/ecs/atlantis.tf
+++ aws/ecs/atlantis.tf
@@ -1,25 +1,78 @@
+variable "atlantis_enabled" {
+  type        = "string"
+  description = "Whether to create the resources. Set to `false` to prevent the module from creating any resources"
+  default     = "true"
+}
+
+variable "atlantis_branch" {
+  type        = "string"
+  default     = "master"
+  description = "Atlantis branch Branch of the GitHub repository, _e.g._ `master`"
+}
+
+variable "atlantis_gh_team_whitelist" {
+  type        = "string"
+  description = "Atlantis GitHub team whitelist"
+  default     = ""
+}
+
+variable "atlantis_gh_user" {
+  type        = "string"
+  description = "Atlantis GitHub user"
+  default     = ""
+}
+
+variable "atlantis_repo_name" {
+  type        = "string"
+  description = "GitHub repository name of the atlantis to be built and deployed to ECS."
+  default     = ""
+}
+
+variable "atlantis_repo_owner" {
+  type        = "string"
+  description = "GitHub organization containing the Atlantis repository"
+  default     = ""
+}
+
+variable "atlantis_repo_whitelist" {
+  type        = "list"
+  description = "Whitelist of repositories Atlantis will accept webhooks from"
+  default     = []
+}
+
+variable "atlantis_wake_word" {
+  type        = "string"
+  description = "Wake world for Atlantis"
+  default     = "atlantis"
+}
+
 module "atlantis" {
-  source                        = "git::https://github.com/cloudposse/terraform-aws-ecs-atlantis.git?ref=initial_implementation"
-  account_id                    = "${data.aws_caller_identity.default.account_id}"
-  alb_arn_suffix                = "${module.alb.alb_arn_suffix}"
-  alb_dns_name                  = "${module.alb.alb_dns_name}"
-  alb_name                      = "${module.alb.alb_name}"
-  alb_zone_id                   = "${module.alb.alb_zone_id}"
-  atlantis_branch               = "ecs-atlantis"
-  atlantis_enabled              = "true"
-  atlantis_gh_team_whitelist    = "terraform:plan,engineering:*"
-  atlantis_gh_user              = "cloudpossebot"
-  atlantis_repo_name            = "testing.cloudposse.co"
-  atlantis_repo_owner           = "cloudposse"
-  atlantis_repo_whitelist       = ["github.com/cloudposse/*"]
-  atlantis_wake_word            = "atlantis"
-  domain_name                   = "${var.domain_name}"
-  ecs_cluster_arn               = "${aws_ecs_cluster.default.arn}"
-  ecs_cluster_name              = "${aws_ecs_cluster.default.name}"
-  name                          = "${var.name}"
-  namespace                     = "${var.namespace}"
-  region                        = "${var.region}"
-  stage                         = "${var.stage}"
+  source    = "git::https://github.com/cloudposse/terraform-aws-ecs-atlantis.git?ref=initial_implementation"
+  enabled   = "${var.atlantis_enabled}"
+  name      = "${var.name}"
+  namespace = "${var.namespace}"
+  region    = "${var.region}"
+  stage     = "${var.stage}"
+
+  alb_arn_suffix = "${module.alb.alb_arn_suffix}"
+  alb_dns_name   = "${module.alb.alb_dns_name}"
+  alb_name       = "${module.alb.alb_name}"
+  alb_zone_id    = "${module.alb.alb_zone_id}"
+
+  repo_name  = "${var.atlantis_repo_name}"
+  repo_owner = "${var.atlantis_repo_owner}"
+  branch     = "${var.atlantis_branch}"
+
+  atlantis_gh_team_whitelist = "${var.atlantis_gh_team_whitelist}"
+  atlantis_gh_user           = "${var.atlantis_gh_user}"
+  atlantis_repo_whitelist    = ["${var.atlantis_repo_whitelist}"]
+  atlantis_wake_word         = "${var.atlantis_wake_word}"
+
+  domain_name = "${var.domain_name}"
+
+  ecs_cluster_arn  = "${aws_ecs_cluster.default.arn}"
+  ecs_cluster_name = "${aws_ecs_cluster.default.name}"
+
   vpc_default_security_group_id = "${module.vpc.vpc_default_security_group_id}"
   vpc_id                        = "${module.vpc.vpc_id}"
 }
