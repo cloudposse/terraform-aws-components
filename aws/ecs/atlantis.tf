e8cdce1160362ecd656f72091cc5d04f13ce2360
Erik Osterman <erik@cloudposse.com>
2019-01-20 19:34:10 -0800

    Pin to release

--- aws/ecs/atlantis.tf
+++ aws/ecs/atlantis.tf
@@ -39,7 +39,7 @@ variable "atlantis_repo_owner" {
 }
 
 module "atlantis" {
-  source    = "git::https://github.com/cloudposse/terraform-aws-ecs-atlantis.git?ref=zone-id"
+  source    = "git::https://github.com/cloudposse/terraform-aws-ecs-atlantis.git?ref=tags/0.3.0"
   enabled   = "${var.atlantis_enabled}"
   name      = "${var.name}"
   namespace = "${var.namespace}"
