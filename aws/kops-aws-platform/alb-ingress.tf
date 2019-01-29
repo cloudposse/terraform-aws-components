cbc553612115241e410f90d0125bbc35ee262d13
Igor Rodionov <goruha@gmail.com>
2019-01-29 14:17:17 +0300

    Added ALB ingress permissions

---
+++ aws/kops-aws-platform/alb-ingress.tf
@@ -0,0 +1,35 @@
+module "kops_external_dns" {
+  source       = "git::https://github.com/cloudposse/terraform-aws-kops-external-dns.git?ref=tags/0.1.2"
+  namespace    = "${var.namespace}"
+  stage        = "${var.stage}"
+  name         = "external-dns"
+  cluster_name = "${var.region}.${var.zone_name}"
+
+  tags = {
+    Cluster = "${var.region}.${var.zone_name}"
+  }
+}
+
+output "kops_external_dns_role_name" {
+  value = "${module.kops_external_dns.role_name}"
+}
+
+output "kops_external_dns_role_unique_id" {
+  value = "${module.kops_external_dns.role_unique_id}"
+}
+
+output "kops_external_dns_role_arn" {
+  value = "${module.kops_external_dns.role_arn}"
+}
+
+output "kops_external_dns_policy_name" {
+  value = "${module.kops_external_dns.policy_name}"
+}
+
+output "kops_external_dns_policy_id" {
+  value = "${module.kops_external_dns.policy_id}"
+}
+
+output "kops_external_dns_policy_arn" {
+  value = "${module.kops_external_dns.policy_arn}"
+}
