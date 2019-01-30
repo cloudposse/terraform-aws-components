d0b964bbc0d90c7df17ca6781efbd749e5e72c36
Josh Myers <joshuajmyers@gmail.com>
2019-01-30 11:18:09 +0000

    Bump codefresh-backing-services to 0.2.0

--- aws/codefresh-onprem/main.tf
+++ aws/codefresh-onprem/main.tf
@@ -44,7 +44,7 @@ data "terraform_remote_state" "backing_services" {
 }
 
 module "codefresh_enterprise_backing_services" {
-  source          = "git::https://github.com/cloudposse/terraform-aws-codefresh-backing-services.git?ref=tags/0.1.0"
+  source          = "git::https://github.com/cloudposse/terraform-aws-codefresh-backing-services.git?ref=tags/0.2.0"
   namespace       = "${var.namespace}"
   stage           = "${var.stage}"
   vpc_id          = "${data.terraform_remote_state.backing_services.vpc_id}"
