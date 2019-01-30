1a583824e29dd6e54b3c55209e890f24baeeb651
Josh Myers <joshuajmyers@gmail.com>
2019-01-30 10:46:43 +0000

    Pass codefresh-onprem outputs back through
    This ensures that we pass the outputs back up through all the caller 
    modules to surface.

--- aws/codefresh-onprem/main.tf
+++ aws/codefresh-onprem/main.tf
@@ -53,3 +53,75 @@ module "codefresh_enterprise_backing_services" {
   zone_name       = "${var.zone_name}"
   chamber_service = "codefresh"
 }
+
+output "elasticache_redis_id" {
+  value       = "${module.codefresh_enterprise_backing_services.elasticache_redis_id}"
+  description = "Elasticache Redis cluster ID"
+}
+
+output "elasticache_redis_security_group_id" {
+  value       = "${module.codefresh_enterprise_backing_services.elasticache_redis_security_group_id}"
+  description = "Elasticache Redis security group ID"
+}
+
+output "elasticache_redis_host" {
+  value       = "${module.codefresh_enterprise_backing_services.elasticache_redis_security_group_id}"
+  description = "Elasticache Redis host"
+}
+
+output "aurora_postgres_database_name" {
+  value       = "${module.codefresh_enterprise_backing_services.aurora_postgres_database_name}"
+  description = "Aurora Postgres Database name"
+}
+
+output "aurora_postgres_master_username" {
+  value       = "${module.codefresh_enterprise_backing_services.aurora_postgres_master_username}"
+  description = "Aurora Postgres Username for the master DB user"
+}
+
+output "aurora_postgres_master_hostname" {
+  value       = "${module.codefresh_enterprise_backing_services.aurora_postgres_master_hostname}"
+  description = "Aurora Postgres DB Master hostname"
+}
+
+output "aurora_postgres_replicas_hostname" {
+  value       = "${module.codefresh_enterprise_backing_services.aurora_postgres_replicas_hostname}"
+  description = "Aurora Postgres Replicas hostname"
+}
+
+output "aurora_postgres_cluster_name" {
+  value       = "${module.codefresh_enterprise_backing_services.aurora_postgres_cluster_name}"
+  description = "Aurora Postgres Cluster Identifier"
+}
+
+output "s3_user_name" {
+  value       = "${module.codefresh_enterprise_backing_services.s3_user_name}"
+  description = "Normalized IAM user name"
+}
+
+output "s3_user_arn" {
+  value       = "${module.codefresh_enterprise_backing_services.s3_user_arn}"
+  description = "The ARN assigned by AWS for the user"
+}
+
+output "s3_user_unique_id" {
+  value       = "${module.codefresh_enterprise_backing_services.s3_user_unique_id}"
+  description = "The user unique ID assigned by AWS"
+}
+
+output "s3_access_key_id" {
+  sensitive   = true
+  value       = "${module.codefresh_enterprise_backing_services.s3_access_key_id}"
+  description = "The access key ID"
+}
+
+output "s3_secret_access_key" {
+  sensitive   = true
+  value       = "${module.codefresh_enterprise_backing_services.s3_secret_access_key}"
+  description = "The secret access key. This will be written to the state file in plain-text"
+}
+
+output "s3_bucket_arn" {
+  value       = "${module.codefresh_enterprise_backing_services.s3_bucket_arn}"
+  description = "The s3 bucket ARN"
+}
