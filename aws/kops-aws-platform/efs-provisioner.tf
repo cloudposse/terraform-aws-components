d26d569a0ae81fa13ac9e2dd92d6b7953dd9c0b8
Josh Myers <joshuajmyers@gmail.com>
2019-02-01 13:19:50 +0000

    EFS provisioning

--- aws/kops-aws-platform/efs-provisioner.tf
+++ aws/kops-aws-platform/efs-provisioner.tf
@@ -1,9 +1,21 @@
+data "terraform_remote_state" "kops" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.namespace}-${var.stage}-terraform-state"
+    key    = "kops/terraform.tfstate"
+  }
+}
+
 module "kops_efs_provisioner" {
-  source       = "git::https://github.com/cloudposse/terraform-aws-kops-efs.git?ref=tags/0.1.0"
-  namespace    = "${var.namespace}"
-  stage        = "${var.stage}"
-  name         = "efs-provisioner"
-  cluster_name = "${var.region}.${var.zone_name}"
+  source             = "git::https://github.com/cloudposse/terraform-aws-kops-efs.git?ref=tags/0.2.0"
+  namespace          = "${var.namespace}"
+  stage              = "${var.stage}"
+  name               = "efs-provisioner"
+  region             = "${var.region}"
+  availability_zones = ["${data.terraform_remote_state.kops.availability_zones}"]
+  zone_id            = "${data.terraform_remote_state.kops.zone_id}"
+  cluster_name       = "${var.region}.${var.zone_name}"
 
   tags = {
     Cluster = "${var.region}.${var.zone_name}"
@@ -20,4 +32,44 @@ output "kops_efs_provisioner_role_unique_id" {
 
 output "kops_efs_provisioner_role_arn" {
   value = "${module.kops_efs_provisioner.role_arn}"
+}
+
+output "efs_arn" {
+  value       = "${module.kops_efs_provisioner.arn}"
+  description = "EFS ARN"
+}
+
+output "efs_id" {
+  value       = "${module.kops_efs_provisioner.id}"
+  description = "EFS ID"
+}
+
+output "efs_host" {
+  value       = "${module.kops_efs_provisioner.host}"
+  description = "EFS host"
+}
+
+output "efs_dns_name" {
+  value       = "${module.kops_efs_provisioner.dns_name}"
+  description = "EFS DNS name"
+}
+
+output "efs_mount_target_dns_names" {
+  value       = "${module.kops_efs_provisioner.mount_target_dns_names}"
+  description = "EFS mount target DNS name"
+}
+
+output "efs_mount_target_ids" {
+  value       = "${module.kops_efs_provisioner.mount_target_ids}"
+  description = "EFS mount target IDs"
+}
+
+output "efs_mount_target_ips" {
+  value       = "${module.kops_efs_provisioner.mount_target_ips}"
+  description = "EFS mount target IPs"
+}
+
+output "efs_network_interface_ids" {
+  value       = "${module.kops_efs_provisioner.network_interface_ids}"
+  description = "EFS network interface IDs"
 }
