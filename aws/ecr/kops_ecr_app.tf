484cf078d2a6b582991b9e818f6a438bd3b9cedc
Igor Rodionov <goruha@gmail.com>
2019-02-05 23:47:58 +0300

    Update kops_ecr_app.tf

--- aws/ecr/kops_ecr_app.tf
+++ aws/ecr/kops_ecr_app.tf
@@ -8,15 +8,21 @@ variable "kops_ecr_app_enabled" {
 }
 
 module "kops_ecr_app" {
-  source    = "git::https://github.com/cloudposse/terraform-aws-ecr.git?ref=atlantis plan -p ecr"
+  source    = "git::https://github.com/cloudposse/terraform-aws-ecr.git?ref=use-only-participals-access-model"
   namespace = "${var.namespace}"
   stage     = "${var.stage}"
   name      = "${var.kops_ecr_app_repository_name}"
 
   enabled = "${var.kops_ecr_app_enabled}"
   
-  principals_full_access    = ["${local.principals_full_access}"]
-  principals_readonly_access = ["${local.principals_readonly_access}"]
+  principals_full_access = [
+    "${module.kops_ecr_user.user_arn}"
+  ]
+
+  principals_readonly_access = [
+    "${module.kops_metadata.masters_role_arn}",
+    "${module.kops_metadata.nodes_role_arn}"
+  ]
 
   tags = "${module.label.tags}"
 }
