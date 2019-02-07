8ab81991f36b0f78026ea9e280e23627e7983446
Igor Rodionov <goruha@gmail.com>
2019-02-07 16:05:13 +0600

    [aws/ecr] Authorize a user to access multiple ECR repos (#65)

--- aws/ecr/main.tf
+++ aws/ecr/main.tf
@@ -28,8 +28,39 @@ variable "zone_name" {
   description = "DNS zone name"
 }
 
+variable "masters_name" {
+  type        = "string"
+  default     = "masters"
+  description = "Kops masters subdomain name in the cluster DNS zone"
+}
+
+variable "nodes_name" {
+  type        = "string"
+  default     = "nodes"
+  description = "Kops nodes subdomain name in the cluster DNS zone"
+}
+
 provider "aws" {
   assume_role {
     role_arn = "${var.aws_assume_role_arn}"
   }
+}
+
+locals {
+  dns_zone = "${var.region}.${var.zone_name}"
+}
+
+module "label" {
+  source    = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.5.4"
+  namespace = "${var.namespace}"
+  stage     = "${var.stage}"
+  name      = "ecr"
+  tags      = "${map("Cluster", local.dns_zone)}"
+}
+
+module "kops_metadata" {
+  source       = "git::https://github.com/cloudposse/terraform-aws-kops-metadata.git?ref=tags/0.2.1"
+  dns_zone     = "${local.dns_zone}"
+  masters_name = "${var.masters_name}"
+  nodes_name   = "${var.nodes_name}"
 }
