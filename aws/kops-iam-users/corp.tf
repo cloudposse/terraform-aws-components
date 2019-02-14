2ad7e497e9bbf3c10d1fd55cb86a53d03be5aa7e
Josh Myers <joshuajmyers@gmail.com>
2019-02-14 13:20:14 +0000

    Label per account (#115)
    Otherwise we are passing the wrong stage e.g. root, when it should be
    testing for this role.

--- aws/kops-iam-users/corp.tf
+++ aws/kops-iam-users/corp.tf
@@ -1,11 +1,33 @@
+module "kops_admin_corp_label" {
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.2.1"
+  namespace  = "${var.namespace}"
+  name       = "kops"
+  stage      = "corp"
+  attributes = ["admin"]
+  delimiter  = "${var.delimiter}"
+  tags       = "${var.tags}"
+  enabled    = "true"
+}
+
+module "kops_readonly_corp_label" {
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.2.1"
+  namespace  = "${var.namespace}"
+  name       = "kops"
+  stage      = "corp"
+  attributes = ["readonly"]
+  delimiter  = "${var.delimiter}"
+  tags       = "${var.tags}"
+  enabled    = "true"
+}
+
 module "kops_admin_access_group_corp" {
   source            = "git::https://github.com/cloudposse/terraform-aws-organization-access-group.git?ref=tags/0.4.0"
   enabled           = "${contains(var.kops_iam_accounts_enabled, "corp") == true ? "true" : "false"}"
   namespace         = "${var.namespace}"
   stage             = "corp"
-  name              = "admin"
-  attributes        = ["kops"]
-  role_name         = "${module.kops_admin_label.id}"
+  name              = "kops"
+  attributes        = ["admin"]
+  role_name         = "${module.kops_admin_corp_label.id}"
   user_names        = []
   member_account_id = "${data.terraform_remote_state.accounts.corp_account_id}"
   require_mfa       = "true"
@@ -16,9 +38,9 @@ module "kops_readonly_access_group_corp" {
   enabled           = "${contains(var.kops_iam_accounts_enabled, "corp") == true ? "true" : "false"}"
   namespace         = "${var.namespace}"
   stage             = "corp"
-  name              = "readonly"
-  attributes        = ["kops"]
-  role_name         = "${module.kops_readonly_label.id}"
+  name              = "kops"
+  attributes        = ["readonly"]
+  role_name         = "${module.kops_readonly_corp_label.id}"
   user_names        = []
   member_account_id = "${data.terraform_remote_state.accounts.corp_account_id}"
   require_mfa       = "true"
