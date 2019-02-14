4fc3043732bf7e249b9bfcf9646456859a29bf34
Josh Myers <joshuajmyers@gmail.com>
2019-02-14 12:50:44 +0000

    Label per account
    Otherwise we are passing the wrong stage e.g. root, when it should be
    testing for this role.

--- aws/kops-iam-users/dev.tf
+++ aws/kops-iam-users/dev.tf
@@ -1,11 +1,33 @@
+module "kops_admin_dev_label" {
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.2.1"
+  namespace  = "${var.namespace}"
+  name       = "kops"
+  stage      = "dev"
+  attributes = ["admin"]
+  delimiter  = "${var.delimiter}"
+  tags       = "${var.tags}"
+  enabled    = "true"
+}
+
+module "kops_readonly_dev_label" {
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.2.1"
+  namespace  = "${var.namespace}"
+  name       = "kops"
+  stage      = "dev"
+  attributes = ["readonly"]
+  delimiter  = "${var.delimiter}"
+  tags       = "${var.tags}"
+  enabled    = "true"
+}
+
 module "kops_admin_access_group_dev" {
   source            = "git::https://github.com/cloudposse/terraform-aws-organization-access-group.git?ref=tags/0.4.0"
   enabled           = "${contains(var.kops_iam_accounts_enabled, "dev") == true ? "true" : "false"}"
   namespace         = "${var.namespace}"
   stage             = "dev"
-  name              = "admin"
-  attributes        = ["kops"]
-  role_name         = "${module.kops_admin_label.id}"
+  name              = "kops"
+  attributes        = ["admin"]
+  role_name         = "${module.kops_admin_dev_label.id}"
   user_names        = []
   member_account_id = "${data.terraform_remote_state.accounts.dev_account_id}"
   require_mfa       = "true"
@@ -16,9 +38,9 @@ module "kops_readonly_access_group_dev" {
   enabled           = "${contains(var.kops_iam_accounts_enabled, "dev") == true ? "true" : "false"}"
   namespace         = "${var.namespace}"
   stage             = "dev"
-  name              = "readonly"
-  attributes        = ["kops"]
-  role_name         = "${module.kops_readonly_label.id}"
+  name              = "kops"
+  attributes        = ["readonly"]
+  role_name         = "${module.kops_readonly_dev_label.id}"
   user_names        = []
   member_account_id = "${data.terraform_remote_state.accounts.dev_account_id}"
   require_mfa       = "true"
--- aws/kops-iam-users/staging.tf
+++ aws/kops-iam-users/staging.tf
@@ -1,11 +1,33 @@
+module "kops_admin_staging_label" {
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.2.1"
+  namespace  = "${var.namespace}"
+  name       = "kops"
+  stage      = "staging"
+  attributes = ["admin"]
+  delimiter  = "${var.delimiter}"
+  tags       = "${var.tags}"
+  enabled    = "true"
+}
+
+module "kops_readonly_staging_label" {
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.2.1"
+  namespace  = "${var.namespace}"
+  name       = "kops"
+  stage      = "staging"
+  attributes = ["readonly"]
+  delimiter  = "${var.delimiter}"
+  tags       = "${var.tags}"
+  enabled    = "true"
+}
+
 module "kops_admin_access_group_staging" {
   source            = "git::https://github.com/cloudposse/terraform-aws-organization-access-group.git?ref=tags/0.4.0"
   enabled           = "${contains(var.kops_iam_accounts_enabled, "staging") == true ? "true" : "false"}"
   namespace         = "${var.namespace}"
   stage             = "staging"
-  name              = "admin"
-  attributes        = ["kops"]
-  role_name         = "${module.kops_admin_label.id}"
+  name              = "kops"
+  attributes        = ["admin"]
+  role_name         = "${module.kops_admin_staging_label.id}"
   user_names        = []
   member_account_id = "${data.terraform_remote_state.accounts.staging_account_id}"
   require_mfa       = "true"
@@ -16,9 +38,9 @@ module "kops_readonly_access_group_staging" {
   enabled           = "${contains(var.kops_iam_accounts_enabled, "staging") == true ? "true" : "false"}"
   namespace         = "${var.namespace}"
   stage             = "staging"
-  name              = "readonly"
-  attributes        = ["kops"]
-  role_name         = "${module.kops_readonly_label.id}"
+  name              = "kops"
+  attributes        = ["readonly"]
+  role_name         = "${module.kops_readonly_staging_label.id}"
   user_names        = []
   member_account_id = "${data.terraform_remote_state.accounts.staging_account_id}"
   require_mfa       = "true"
