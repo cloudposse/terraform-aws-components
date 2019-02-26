1335660e67c10c1fc8e425f74f4cd24cba6ca665
Jeremy Grodberg <Nuru@users.noreply.github.com>
2019-02-25 23:16:02 -0800

    Add Datadog provisioning

---
+++ aws/datadog/main.tf
@@ -0,0 +1,14 @@
+module "datadog_ids" {
+  source         = "git::https://github.com/cloudposse/terraform-aws-ssm-parameter-store?ref=tags/0.1.5"
+  parameter_read = ["/datadog/datadog_external_id"]
+}
+
+module "datadog_aws_integration" {
+  source                     = "git::https://github.com/cloudposse/terraform-datadog-aws-integration.git?ref=tags/0.1.5"
+  namespace                  = "${var.namespace}"
+  stage                      = "${var.stage}"
+  name                       = "datadog"
+  datadog_external_id        = "${lookup(module.datadog_ids.map, "/datadog/datadog_external_id")}"
+  integrations               = "${var.integrations}"
+}
+
---
+++ aws/datadog/variables.tf
@@ -0,0 +1,13 @@
+
+variable "namespace" {
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
+
+variable "stage" {
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+}
+
+variable "integrations" {
+  type        = "list"
+  description = "List of AWS integration permissions sets to apply (all, core, rds)"
+}
