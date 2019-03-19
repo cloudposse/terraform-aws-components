97b23ac8deb7abf87aacecca4f9d063cd22a85fb
Igor Rodionov <goruha@gmail.com>
2019-03-20 01:19:13 +0600

    Added domain zone

--- aws/kops-aws-platform/acm.tf
+++ aws/kops-aws-platform/acm.tf
@@ -1,19 +1,19 @@
-variable "acm_enabled" {
+variable "kops_acm_enabled" {
   description = "Set to false to prevent the acm module from creating any resources"
   default     = "false"
 }
 
-variable "acm_san_domains" {
+variable "kops_acm_san_domains" {
   type        = "list"
   default     = []
   description = "A list of domains (except *.{cluster_name}) that should be SANs in the issued certificate"
 }
 
 resource "aws_acm_certificate" "default" {
-  count                     = "${var.acm_enabled ? 1 : 0}"
+  count                     = "${var.kops_acm_enabled ? 1 : 0}"
   domain_name               = "*.${var.region}.${var.zone_name}"
   validation_method         = "DNS"
-  subject_alternative_names = ["${var.acm_san_domains}"]
+  subject_alternative_names = ["${var.kops_acm_san_domains}"]
   tags                      = "${var.tags}"
 
   lifecycle {
@@ -21,12 +21,12 @@ resource "aws_acm_certificate" "default" {
   }
 }
 
-output "acm_arn" {
+output "kops_acm_arn" {
   value       = "${join("", aws_acm_certificate.default.*.arn)}"
   description = "The ARN of the certificate"
 }
 
-output "acm_domain_validation_options" {
+output "kops_acm_domain_validation_options" {
   value       = "${flatten(aws_acm_certificate.default.*.domain_validation_options)}"
   description = "CNAME records that are added to the DNS zone to complete certificate validation"
 }
