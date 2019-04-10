9818bfd11fa6e6bbb8334f8ede3f223c308d7c8a
Igor Rodionov <goruha@gmail.com>
2019-04-11 02:14:50 +0600

    Fix avilability zone (#154)
    * Fix avilability zone
    
    * Fix formatting
    
    * Fix subnets

--- aws/backing-services/elasticache-redis.tf
+++ aws/backing-services/elasticache-redis.tf
@@ -59,7 +59,7 @@ module "elasticache_redis" {
   alarm_cpu_threshold_percent  = "75"
   alarm_memory_threshold_bytes = "10000000"
   apply_immediately            = "true"
-  availability_zones           = ["${data.aws_availability_zones.available.names}"]
+  availability_zones           = ["${local.availability_zones}"]
   automatic_failover           = "false"
   enabled                      = "${var.redis_cluster_enabled}"
   auth_token                   = "${var.redis_auth_token}"
