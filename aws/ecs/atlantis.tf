afc9cce44dbab4ad4454d2924e7e875bc3451f6e
Andriy Knysh <aknysh@users.noreply.github.com>
2019-05-21 16:03:18 -0400

    [ecs/atlantis] Bump `terraform-aws-ecs-atlantis` version (#175)

--- aws/ecs/atlantis.tf
+++ aws/ecs/atlantis.tf
@@ -171,7 +171,7 @@ variable "atlantis_alb_ingress_authenticated_paths" {
 }
 
 module "atlantis" {
-  source    = "git::https://github.com/cloudposse/terraform-aws-ecs-atlantis.git?ref=tags/0.10.0"
+  source    = "git::https://github.com/cloudposse/terraform-aws-ecs-atlantis.git?ref=tags/0.10.1"
   enabled   = "${var.atlantis_enabled}"
   name      = "${var.name}"
   namespace = "${var.namespace}"
