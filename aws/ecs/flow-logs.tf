d4efe45ffd157905632fbe23ff87ce7da8e1974d
Igor Rodionov <goruha@gmail.com>
2019-08-05 19:45:01 +0600

    Vpc flow logs (#188)
    * Added flow logs
    
    * Fix webhook github
    
    * Fix vpc flow

---
+++ aws/ecs/flow-logs.tf
@@ -0,0 +1,21 @@
+variable "flow_logs_enabled" {
+  type    = "string"
+  default = "true"
+}
+
+module "flow_logs" {
+  source = "git::https://github.com/cloudposse/terraform-aws-vpc-flow-logs-s3-bucket.git?ref=init"
+
+  name       = "${var.name}"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  tags       = "${var.tags}"
+  attributes = "${concat(var.attributes, list("flow-logs"))}"
+  delimiter  = "${var.delimiter}"
+
+  region = "${var.region}"
+
+  enabled = "${var.flow_logs_enabled}"
+
+  vpc_id = "${module.vpc.vpc_id}"
+}
