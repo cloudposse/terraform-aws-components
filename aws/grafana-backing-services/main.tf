40d54ff2376ee1310a929a538e27c48209708972
aknysh <andriy.knysh@gmail.com>
2019-08-08 16:25:25 -0400

    TF 0.12 cleanup

--- aws/grafana-backing-services/main.tf
+++ aws/grafana-backing-services/main.tf
@@ -8,56 +8,58 @@ provider "aws" {
   version = "~> 2.17"
 
   assume_role {
-    role_arn = "${var.aws_assume_role_arn}"
+    role_arn = var.aws_assume_role_arn
   }
 }
 
 variable "aws_assume_role_arn" {
-  type = "string"
+  type = string
 }
 
 variable "namespace" {
-  type        = "string"
+  type        = string
   description = "Namespace (e.g. `eg` or `cp`)"
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
 }
 
 variable "region" {
-  type        = "string"
+  type        = string
   description = "AWS region"
 }
 
 variable "dns_zone_name" {
-  type        = "string"
-  default     = ""
+  type        = string
   description = "The DNS domain under which to put entries for the database. Usually the same as the cluster name, e.g. us-west-2.prod.cpco.io"
 }
 
 variable "chamber_service" {
+  type        = string
   default     = "grafana"
   description = "`chamber` service name. See [chamber usage](https://github.com/segmentio/chamber#usage) for more details"
 }
 
 variable "chamber_parameter_name_pattern" {
+  type        = string
   default     = "/%s/%s"
   description = "Format string for creating SSM parameter name used to store chamber parameters. The default is usually best."
 }
 
 variable "chamber_kms_key_id" {
-  type        = "string"
+  type        = string
   default     = "alias/aws/ssm"
   description = "KMS key ID, ARN, or alias to use for encrypting SSM secrets"
 }
 
-data "aws_route53_zone" "default" {
-  name = "${var.dns_zone_name}"
+variable "vpc_id" {
+  type        = string
+  description = "The ID of the VPC to create the resources in, or SSM parameter key for it"
 }
 
-locals {
-  chamber_service = "${var.chamber_service == "" ? basename(pathexpand(path.module)) : var.chamber_service}"
-  dns_zone_id     = "${data.aws_route53_zone.default.zone_id}"
+variable "vpc_subnet_ids" {
+  type        = string
+  description = "Comma separated string list of AWS Subnet IDs in which to place the database, or SSM parameter key for it"
 }
