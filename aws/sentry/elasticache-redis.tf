a0c1a5d8535c84a0f69b18854d41c78878a03642
Nuru <Nuru@users.noreply.github.com>
2019-09-17 04:55:49 -0700

    Create sentry

---
+++ aws/sentry/elasticache-redis.tf
@@ -0,0 +1,98 @@
+variable "redis_name" {
+  type        = string
+  default     = "redis"
+  description = "Redis name"
+}
+
+variable "redis_instance_type" {
+  type        = string
+  default     = "cache.t2.medium"
+  description = "EC2 instance type for Redis cluster"
+}
+
+variable "redis_cluster_size" {
+  type        = string
+  default     = "2"
+  description = "Redis cluster size"
+}
+
+variable "redis_cluster_enabled" {
+  type        = string
+  default     = "true"
+  description = "Set to false to prevent the module from creating any resources"
+}
+
+variable "redis_params" {
+  type = list(object({
+    name  = string
+    value = string
+  }))
+  default     = [{ name = "maxmemory-policy", value = "allkeys-lru" }]
+  description = "A list of Redis parameters to apply. Note that parameters may differ from a Redis family to another"
+}
+
+resource "random_string" "elasticache_auth_token" {
+  count   = var.redis_cluster_enabled ? 1 : 0
+  length  = 31
+  special = false
+}
+
+
+module "elasticache_redis" {
+  source                       = "git::https://github.com/cloudposse/terraform-aws-elasticache-redis.git?ref=tags/0.13.0"
+  namespace                    = var.namespace
+  stage                        = var.stage
+  name                         = var.redis_name
+  attributes                   = ["sentry"]
+  zone_id                      = local.zone_id
+  security_groups              = [module.kops_metadata.nodes_security_group_id]
+  vpc_id                       = module.kops_metadata.vpc_id
+  subnets                      = module.kops_metadata.private_subnet_ids
+  maintenance_window           = "sun:03:00-sun:04:00"
+  cluster_size                 = var.redis_cluster_size
+  instance_type                = var.redis_instance_type
+  transit_encryption_enabled   = true # required for auth_token
+  engine_version               = "4.0.10"
+  family                       = "redis4.0"
+  port                         = "6379"
+  alarm_cpu_threshold_percent  = "75"
+  alarm_memory_threshold_bytes = "10000000"
+  apply_immediately            = "true"
+  availability_zones           = local.availability_zones
+  automatic_failover           = "true"
+  enabled                      = var.redis_cluster_enabled
+  auth_token                   = join("", random_string.elasticache_auth_token.*.result)
+
+  parameter = var.redis_params
+}
+
+
+resource "aws_ssm_parameter" "elasticache_redis_host" {
+  count       = local.postgres_cluster_enabled ? 1 : 0
+  name        = format(local.chamber_parameter_format, local.chamber_service, "sentry_redis_host")
+  value       = module.elasticache_redis.host
+  description = "Elasticache host for Sentry"
+  type        = "String"
+  overwrite   = "true"
+}
+resource "aws_ssm_parameter" "elasticache_redis_password" {
+  count       = local.postgres_cluster_enabled ? 1 : 0
+  name        = format(local.chamber_parameter_format, local.chamber_service, "sentry_redis_password")
+  value       = join("", random_string.elasticache_auth_token.*.result)
+  description = "Elasticache host for Sentry"
+  type        = "String"
+  overwrite   = "true"
+}
+
+
+output "elasticache_redis_id" {
+  value = module.elasticache_redis.id
+}
+
+output "elasticache_redis_security_group_id" {
+  value = module.elasticache_redis.security_group_id
+}
+
+output "elasticache_redis_host" {
+  value = module.elasticache_redis.host
+}
---
+++ aws/sentry/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  backend "s3" {}
+}
+
+
+data "aws_availability_zones" "available" {}
+
+data "aws_route53_zone" "default" {
+  name = var.kops_cluster_name
+}
+
+module "kops_metadata" {
+  source       = "git::https://github.com/cloudposse/terraform-aws-kops-data-network.git?ref=tags/0.2.0"
+  enabled      = true
+  cluster_name = var.kops_cluster_name
+}
+
+locals {
+  null               = ""
+  zone_id            = data.aws_route53_zone.default.zone_id
+  availability_zones = split(",", length(var.elasticache_availability_zones) == 0 ? join(",", data.aws_availability_zones.available.names) : join(",", var.elasticache_availability_zones))
+  chamber_service    = var.chamber_service == "" ? basename(pathexpand(path.module)) : var.chamber_service
+
+  chamber_parameter_format = "/%s/%s"
+
+}
+
+provider "aws" {
+  assume_role {
+    role_arn = var.aws_assume_role_arn
+  }
+}
+
+
+resource "random_string" "sentry_secret_key" {
+  length  = 48
+  special = true
+}
+resource "random_string" "sentry_admin_user_password" {
+  length  = 21
+  special = false
+}
+
+resource "aws_ssm_parameter" "sentry_secret" {
+  name        = format(local.chamber_parameter_format, local.chamber_service, "sentry_secret")
+  value       = random_string.sentry_secret_key.result
+  description = "Secret Key for Sentry to encrypt sessions"
+  type        = "String"
+  overwrite   = "true"
+}
+
+resource "aws_ssm_parameter" "sentry_admin_user_password" {
+  name        = format(local.chamber_parameter_format, local.chamber_service, "sentry_admin_user_password")
+  value       = random_string.sentry_admin_user_password.result
+  description = "Password for Sentry admin user"
+  type        = "String"
+  overwrite   = "true"
+}
+
