3436e61a12588ac8624ee80fd308aa9ecaf45220
Maxim Mironenko <maxim@cloudposse.com>
2019-10-03 18:43:16 +0600

    alerts-ns added

--- aws/root-dns/parent-alerts-ns.tf
+++ aws/root-dns/parent-alerts-ns.tf
@@ -4,6 +4,8 @@ module "alerts" {
   namespace        = "${var.namespace}"
   stage            = "alerts"
   zone_id          = "${aws_route53_zone.parent_dns_zone.zone_id}"
+  account          = "corp"
+  key              = "alerts-dns/terraform.tfstate"
 }
 
 output "alerts_name_servers" {
