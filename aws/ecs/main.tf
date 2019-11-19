2d48b0cd81dfec96d9dbdff2109667c4de9f7c10
Igor Rodionov <goruha@gmail.com>
2019-11-19 18:45:40 +0600

    Extract provider

--- aws/ecs/main.tf
+++ aws/ecs/main.tf
@@ -10,12 +10,12 @@ provider "aws" {
   }
 }
 
-variable "github_webhooks_token" {
+variable "GITHUB_WEBHOOKS_TOKEN" {
   type        = "string"
   description = "GitHub Webhook Token with permissions to access private repositories"
 }
 
 provider "github" {
-  token        = "${var.github_webhooks_token}"
+  token        = "${var.GITHUB_WEBHOOKS_TOKEN}"
   organization = "${var.atlantis_repo_owner}"
 }
