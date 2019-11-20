0d6aa2022daf312f402ccdc4e32ff80e5c315456
Igor Rodionov <goruha@gmail.com>
2019-11-20 19:48:27 +0600

    Read values from ssm parameters

--- aws/spotinst/main.tf
+++ aws/spotinst/main.tf
@@ -13,18 +13,59 @@ provider "aws" {
 }
 
 locals {
-  template_url       = "https://s3.amazonaws.com/spotinst-public/assets/cloudformation/templates/spotinst_aws_cfn_account_credentials_iam_stack.template.json"
+  template_url = "https://s3.amazonaws.com/spotinst-public/assets/cloudformation/templates/spotinst_aws_cfn_account_credentials_iam_stack.template.json"
+  parameters   = "${map("AccountId", module.account_id.value, "ExternalId", module.external_id.value,"Principal", module.principal.value, "Token", module.token.value)}"
+}
+
+module "account_id" {
+  source = "git::https://github.com/cloudposse/terraform-aws-ssm-parameter-chamber-reader.git?ref=init"
+
+  enabled         = "${var.enabled}"
+  chamber_format  = "${var.chamber_format}"
+  chamber_service = "${var.chamber_service}"
+  parameter       = "${var.chamber_name_account_id}"
+  override_value  = "${var.override_account_id}"
+}
+
+module "external_id" {
+  source = "git::https://github.com/cloudposse/terraform-aws-ssm-parameter-chamber-reader.git?ref=init"
+
+  enabled         = "${var.enabled}"
+  chamber_format  = "${var.chamber_format}"
+  chamber_service = "${var.chamber_service}"
+  parameter       = "${var.chamber_name_external_id}"
+  override_value  = "${var.override_external_id}"
+}
+
+module "principal" {
+  source = "git::https://github.com/cloudposse/terraform-aws-ssm-parameter-chamber-reader.git?ref=init"
+
+  enabled         = "${var.enabled}"
+  chamber_format  = "${var.chamber_format}"
+  chamber_service = "${var.chamber_service}"
+  parameter       = "${var.chamber_name_principal}"
+  override_value  = "${var.override_principal}"
+}
+
+module "token" {
+  source = "git::https://github.com/cloudposse/terraform-aws-ssm-parameter-chamber-reader.git?ref=init"
+
+  enabled         = "${var.enabled}"
+  chamber_format  = "${var.chamber_format}"
+  chamber_service = "${var.chamber_service}"
+  parameter       = "${var.chamber_name_token}"
+  override_value  = "${var.override_token}"
 }
 
 module "default" {
   source = "git::https://github.com/cloudposse/terraform-aws-cloudformation-stack.git?ref=init"
 
-  enabled            = "${var.enabled}"
-  namespace          = "${var.namespace}"
-  stage              = "${var.stage}"
-  name               = "${var.name}"
-  attributes         = ["${var.attributes}"]
-  parameters         = "${var.parameters}"
-  template_url       = "${local.template_url}"
-  capabilities       = "${var.capabilities}"
+  enabled      = "${var.enabled}"
+  namespace    = "${var.namespace}"
+  stage        = "${var.stage}"
+  name         = "${var.name}"
+  attributes   = ["${var.attributes}"]
+  parameters   = "${local.parameters}"
+  template_url = "${local.template_url}"
+  capabilities = "${var.capabilities}"
 }
--- aws/spotinst/variables.tf
+++ aws/spotinst/variables.tf
@@ -40,14 +40,67 @@ variable "tags" {
   default     = {}
 }
 
-variable "parameters" {
-  type        = "map"
-  description = "Key-value map of input parameters for the Stack Set template. (_e.g._ map(\"BusinessUnit\",\"ABC\")"
-  default     = {}
-}
-
 variable "capabilities" {
   type        = "list"
   description = "A list of capabilities. Valid values: CAPABILITY_IAM, CAPABILITY_NAMED_IAM, CAPABILITY_AUTO_EXPAND"
   default     = []
+}
+
+variable "chamber_format" {
+  default     = "/%s/%s"
+  description = "Format to store parameters in SSM, for consumption with chamber"
+}
+
+variable "chamber_service" {
+  type        = "string"
+  default     = "kops"
+  description = "SSM parameter service name for use with chamber. This is used in chamber_format where /$chamber_service/$parameter would be the default."
+}
+
+variable "chamber_name_account_id" {
+  type        = "string"
+  default     = "spotinst_account_id"
+  description = "Chamber parameter name store Spotinst account id"
+}
+
+variable "override_account_id" {
+  type        = "string"
+  default     = ""
+  description = "Override Spotinst account id"
+}
+
+variable "chamber_name_external_id" {
+  type        = "string"
+  default     = "spotinst_external_id"
+  description = "Chamber parameter name store Spotinst external id"
+}
+
+variable "override_external_id" {
+  type        = "string"
+  default     = ""
+  description = "Override Spotinst external id"
+}
+
+variable "chamber_name_principal" {
+  type        = "string"
+  default     = "spotinst_principal"
+  description = "Chamber parameter name store Spotinst principal"
+}
+
+variable "override_principal" {
+  type        = "string"
+  default     = ""
+  description = "Override Spotinst principal"
+}
+
+variable "chamber_name_token" {
+  type        = "string"
+  default     = "spotinst_token"
+  description = "Chamber parameter name store Spotinst token"
+}
+
+variable "override_token" {
+  type        = "string"
+  default     = ""
+  description = "Override Spotinst token"
 }
