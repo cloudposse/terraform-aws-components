d38294c112122e0efdc224287cd97c189a1bfde2
Andriy Knysh <aknysh@users.noreply.github.com>
2019-11-22 00:11:25 -0500

    Fix `ecs/atlantis` (#219)
    * Fix `ecs/atlantis`
    
    * Fix `ecs/atlantis`

--- aws/ecs/default-backend.tf
+++ aws/ecs/default-backend.tf
@@ -49,4 +49,8 @@ module "default_backend_web_app" {
   alb_ingress_authenticated_paths   = []
 
   repo_owner = "${var.atlantis_repo_owner}"
+
+  webhook_enabled       = "false"
+  github_oauth_token    = "NO_TOKEN"
+  github_webhooks_token = "NO_TOKEN"
 }
