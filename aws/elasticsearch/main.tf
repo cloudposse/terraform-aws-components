0d1f53b381559c62b306b8c2cc8c65478c729eea
aknysh <andriy.knysh@gmail.com>
2020-04-28 22:48:58 -0400

    [elasticsearch] Convert to TF 0.12

--- aws/elasticsearch/main.tf
+++ aws/elasticsearch/main.tf
@@ -1,47 +1,47 @@
 terraform {
-  required_version = ">= 0.11.2"
-  backend          "s3"             {}
+  required_version = ">= 0.12.0"
+  backend "s3" {}
 }
 
 provider "aws" {
   assume_role {
-    role_arn = "${var.aws_assume_role_arn}"
+    role_arn = var.aws_assume_role_arn
   }
 }
 
 module "kops_metadata" {
-  source       = "git::https://github.com/cloudposse/terraform-aws-kops-data-iam.git?ref=tags/0.1.0"
-  cluster_name = "${var.cluster_name}"
+  source       = "git::https://github.com/cloudposse/terraform-aws-kops-data-iam.git?ref=tags/0.2.0"
+  cluster_name = var.cluster_name
 }
 
 module "kops_vpc" {
-  source       = "git::https://github.com/cloudposse/terraform-aws-kops-data-network.git?ref=tags/0.1.1"
-  cluster_name = "${var.cluster_name}"
-  vpc_id       = "${var.vpc_id}"
+  source       = "git::https://github.com/cloudposse/terraform-aws-kops-data-network.git?ref=tags/0.2.0"
+  cluster_name = var.cluster_name
+  vpc_id       = var.vpc_id
 }
 
 data "aws_route53_zone" "default" {
-  name = "${var.zone_name == "" ? var.cluster_name : var.zone_name}"
+  name = var.zone_name == "" ? var.cluster_name : var.zone_name
 }
 
 locals {
-  zone_id    = "${data.aws_route53_zone.default.zone_id}"
+  zone_id = data.aws_route53_zone.default.zone_id
 }
 
 locals {
   role_arns = {
-    masters = ["${module.kops_metadata.masters_role_arn}"]
-    nodes   = ["${module.kops_metadata.nodes_role_arn}"]
-    both    = ["${module.kops_metadata.masters_role_arn}", "${module.kops_metadata.nodes_role_arn}"]
+    masters = [module.kops_metadata.masters_role_arn]
+    nodes   = [module.kops_metadata.nodes_role_arn]
+    both    = [module.kops_metadata.masters_role_arn, module.kops_metadata.nodes_role_arn]
     any     = ["*"]
     none    = []
   }
 
   security_groups = {
-    masters = ["${module.kops_vpc.masters_security_group_id}"]
-    nodes   = ["${module.kops_vpc.nodes_security_group_id}"]
-    both    = ["${module.kops_vpc.masters_security_group_id}", "${module.kops_vpc.nodes_security_group_id}"]
-    any     = ["${module.kops_vpc.masters_security_group_id}", "${module.kops_vpc.nodes_security_group_id}"]
+    masters = [module.kops_vpc.masters_security_group_id]
+    nodes   = [module.kops_vpc.nodes_security_group_id]
+    both    = [module.kops_vpc.masters_security_group_id, module.kops_vpc.nodes_security_group_id]
+    any     = [module.kops_vpc.masters_security_group_id, module.kops_vpc.nodes_security_group_id]
     none    = []
   }
 
@@ -54,117 +54,99 @@ locals {
 }
 
 module "elasticsearch" {
-  source                          = "git::https://github.com/cloudposse/terraform-aws-elasticsearch.git?ref=tags/0.3.5"
-  namespace                       = "${var.namespace}"
-  stage                           = "${var.stage}"
-  name                            = "${var.elasticsearch_name}"
-  dns_zone_id                     = "${local.zone_id}"
-  security_groups                 = ["${local.security_groups[var.elasticsearch_network_permitted_nodes]}"]
-  vpc_id                          = "${module.kops_vpc.vpc_id}"
-  subnet_ids                      = "${module.kops_vpc.private_subnet_ids}"
-  zone_awareness_enabled          = "${length(module.kops_vpc.private_subnet_ids) > 1 ? "true" : "false"}"
-  elasticsearch_version           = "${var.elasticsearch_version}"
-  instance_type                   = "${var.elasticsearch_instance_type}"
-  instance_count                  = "${var.elasticsearch_instance_count}"
-  iam_role_arns                   = ["${distinct(concat(local.role_arns[var.elasticsearch_iam_permitted_nodes], var.iam_role_arns))}"]
-  iam_authorizing_role_arns       = "${coalescelist(local.role_arns[lookup(local.option_keys, var.elasticsearch_iam_authorizing_role_arn, "none")], list(var.elasticsearch_iam_authorizing_role_arn))}"
-  iam_actions                     = ["${var.elasticsearch_iam_actions}"]
-  kibana_subdomain_name           = "${var.kibana_subdomain_name}"
-  elasticsearch_subdomain_name    = "${var.elasticsearch_subdomain_name}"
-  ebs_volume_size                 = "${var.elasticsearch_ebs_volume_size}"
-  encrypt_at_rest_enabled         = "${var.elasticsearch_encrypt_at_rest_enabled}"
-  node_to_node_encryption_enabled = "${var.elasticsearch_node_to_node_encryption_enabled}"
-  enabled                         = "${var.elasticsearch_enabled}"
-  iam_role_max_session_duration   = "${var.elasticsearch_iam_role_max_session_duration}"
-  create_iam_service_linked_role  = "${var.create_iam_service_linked_role}"
+  source                 = "git::https://github.com/cloudposse/terraform-aws-elasticsearch.git?ref=tags/0.12.0"
+  namespace              = var.namespace
+  stage                  = var.stage
+  name                   = var.elasticsearch_name
+  dns_zone_id            = local.zone_id
+  security_groups        = local.security_groups[var.elasticsearch_network_permitted_nodes]
+  vpc_id                 = module.kops_vpc.vpc_id
+  subnet_ids             = module.kops_vpc.private_subnet_ids
+  zone_awareness_enabled = length(module.kops_vpc.private_subnet_ids) > 1 ? true : false
+  elasticsearch_version  = var.elasticsearch_version
+  instance_type          = var.elasticsearch_instance_type
+  instance_count         = var.elasticsearch_instance_count
 
-  advanced_options {
+  iam_role_arns = distinct(
+    concat(
+      local.role_arns[var.elasticsearch_iam_permitted_nodes],
+      var.iam_role_arns,
+    )
+  )
+
+  iam_authorizing_role_arns = coalescelist(
+    local.role_arns[lookup(
+      local.option_keys,
+      var.elasticsearch_iam_authorizing_role_arn,
+      "none",
+    )],
+    [var.elasticsearch_iam_authorizing_role_arn]
+  )
+
+  iam_actions                     = var.elasticsearch_iam_actions
+  kibana_subdomain_name           = var.kibana_subdomain_name
+  elasticsearch_subdomain_name    = var.elasticsearch_subdomain_name
+  ebs_volume_size                 = var.elasticsearch_ebs_volume_size
+  encrypt_at_rest_enabled         = var.elasticsearch_encrypt_at_rest_enabled
+  node_to_node_encryption_enabled = var.elasticsearch_node_to_node_encryption_enabled
+  enabled                         = var.elasticsearch_enabled
+  iam_role_max_session_duration   = var.elasticsearch_iam_role_max_session_duration
+  create_iam_service_linked_role  = var.create_iam_service_linked_role
+
+  advanced_options = {
     "rest.action.multi.allow_explicit_index" = "true"
   }
 }
 
 locals {
-  elasticsearch_chamber_service = "${coalesce(var.elasticsearch_chamber_service, var.chamber_service, basename(pathexpand(path.module)))}"
+  elasticsearch_chamber_service = coalesce(
+    var.elasticsearch_chamber_service,
+    var.chamber_service,
+    basename(pathexpand(path.module))
+  )
 }
 
 resource "aws_ssm_parameter" "elasticsearch_domain_endpoint" {
-  count       = "${var.chamber_parameters_enabled ? 1 : 0}"
-  name        = "${format(var.chamber_parameter_name, local.elasticsearch_chamber_service, "elasticsearch_domain_endpoint")}"
-  value       = "${module.elasticsearch.domain_endpoint}"
+  count = var.chamber_parameters_enabled ? 1 : 0
+  name = format(
+    var.chamber_parameter_name,
+    local.elasticsearch_chamber_service,
+    "elasticsearch_domain_endpoint"
+  )
+  value       = module.elasticsearch.domain_endpoint
   description = "Domain-specific endpoint used to submit index, search, and data upload requests"
   type        = "String"
-  overwrite   = "true"
+  overwrite   = true
 }
 
 resource "aws_ssm_parameter" "elasticsearch_kibana_endpoint" {
-  count       = "${var.chamber_parameters_enabled ? 1 : 0}"
-  name        = "${format(var.chamber_parameter_name, local.elasticsearch_chamber_service, "elasticsearch_kibana_endpoint")}"
-  value       = "${module.elasticsearch.kibana_endpoint}"
+  count = var.chamber_parameters_enabled ? 1 : 0
+  name = format(
+    var.chamber_parameter_name,
+    local.elasticsearch_chamber_service,
+    "elasticsearch_kibana_endpoint"
+  )
+  value       = module.elasticsearch.kibana_endpoint
   description = "Domain-specific endpoint for Kibana without https scheme"
   type        = "String"
-  overwrite   = "true"
-}
-
-output "elasticsearch_security_group_id" {
-  value       = "${module.elasticsearch.security_group_id}"
-  description = "Security Group ID to control access to the Elasticsearch domain"
-}
-
-output "elasticsearch_domain_arn" {
-  value       = "${module.elasticsearch.domain_arn}"
-  description = "ARN of the Elasticsearch domain"
-}
-
-output "elasticsearch_domain_id" {
-  value       = "${module.elasticsearch.domain_id}"
-  description = "Unique identifier for the Elasticsearch domain"
-}
-
-output "elasticsearch_domain_endpoint" {
-  value       = "${module.elasticsearch.domain_endpoint}"
-  description = "Domain-specific endpoint used to submit index, search, and data upload requests"
-}
-
-output "elasticsearch_kibana_endpoint" {
-  value       = "${module.elasticsearch.kibana_endpoint}"
-  description = "Domain-specific endpoint for Kibana without https scheme"
-}
-
-output "elasticsearch_domain_hostname" {
-  value       = "${module.elasticsearch.domain_hostname}"
-  description = "Elasticsearch domain hostname to submit index, search, and data upload requests"
-}
-
-output "elasticsearch_kibana_hostname" {
-  value       = "${module.elasticsearch.kibana_hostname}"
-  description = "Kibana hostname"
-}
-
-output "elasticsearch_user_iam_role_name" {
-  value       = "${module.elasticsearch.elasticsearch_user_iam_role_name}"
-  description = "IAM name of role for Elasticsearch users"
-}
-
-output "elasticsearch_user_iam_role_arn" {
-  value       = "${module.elasticsearch.elasticsearch_user_iam_role_arn}"
-  description = "IAM ARN of role for Elasticsearch users"
+  overwrite   = true
 }
 
 module "elasticsearch_log_cleanup" {
   source    = "git::https://github.com/cloudposse/terraform-aws-lambda-elasticsearch-cleanup.git?ref=tags/0.4.2"
-  enabled   = "${var.elasticsearch_enabled == "true" ? var.elasticsearch_log_cleanup_enabled : "false"}"
-  namespace = "${var.namespace}"
-  stage     = "${var.stage}"
-  name      = "${var.elasticsearch_name}"
+  enabled   = var.elasticsearch_enabled && var.elasticsearch_log_cleanup_enabled
+  namespace = var.namespace
+  stage     = var.stage
+  name      = var.elasticsearch_name
 
-  es_endpoint          = "${module.elasticsearch.domain_endpoint}"
-  es_domain_arn        = "${module.elasticsearch.domain_arn}"
-  es_security_group_id = "${module.elasticsearch.security_group_id}"
-  vpc_id               = "${module.kops_vpc.vpc_id}"
-  subnet_ids           = "${module.kops_vpc.private_subnet_ids}"
+  es_endpoint          = module.elasticsearch.domain_endpoint
+  es_domain_arn        = module.elasticsearch.domain_arn
+  es_security_group_id = module.elasticsearch.security_group_id
+  vpc_id               = module.kops_vpc.vpc_id
+  subnet_ids           = module.kops_vpc.private_subnet_ids
 
-  index        = "${var.elasticsearch_log_index_name}"
-  delete_after = "${var.elasticsearch_log_retention_days}"
+  index        = var.elasticsearch_log_index_name
+  delete_after = var.elasticsearch_log_retention_days
 
-  sns_arn = "${var.sns_arn}"
+  sns_arn = var.sns_arn
 }
--- aws/elasticsearch/variables.tf
+++ aws/elasticsearch/variables.tf
@@ -1,180 +1,188 @@
 variable "aws_assume_role_arn" {
-  type = "string"
+  type = string
 }
 
 variable "namespace" {
-  type        = "string"
+  type        = string
   description = "Namespace (e.g. `eg` or `cp`)"
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
 }
 
 variable "region" {
-  type        = "string"
+  type        = string
   description = "AWS region"
 }
 
 variable "zone_name" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "Domain name of DNS zone in which to add elasticsearch, optional, defaults to cluster_name"
 }
 
 variable "chamber_parameters_enabled" {
-  default     = "false"
+  type        = bool
+  default     = false
   description = "Set true to store endpoints in Chamber/SSM parameter store"
 }
 
 variable "chamber_service" {
+  type        = string
   default     = ""
   description = "`chamber` service name shared by all services. See [chamber usage](https://github.com/segmentio/chamber#usage) for more details"
 }
 
 variable "elasticsearch_chamber_service" {
+  type        = string
   default     = ""
   description = "`chamber` service name specific to elasticsearch"
 }
 
 variable "chamber_parameter_name" {
+  type        = string
   default     = "/%s/%s"
   description = "Format string for converting `chamber` service and parameter names to SSM parameter name"
 }
 
 variable "cluster_name" {
-  type        = "string"
+  type        = string
   description = "Kops cluster name (e.g. `us-east-1.prod.cloudposse.co` or `cluster-1.cloudposse.co`)"
 }
 
 variable "vpc_id" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "VPC ID of the Kubernetes cluster (optional, will try to auto-detect)"
 }
 
 variable "elasticsearch_name" {
-  type        = "string"
+  type        = string
   default     = "elasticsearch"
   description = "Elasticsearch cluster name"
 }
 
 variable "elasticsearch_version" {
-  type        = "string"
+  type        = string
   default     = "6.5"
   description = "Version of Elasticsearch to deploy"
 }
 
 # Encryption at rest is not supported with t2.small.elasticsearch instances
 variable "elasticsearch_encrypt_at_rest_enabled" {
-  type        = "string"
-  default     = "false"
+  type        = bool
+  default     = false
   description = "Whether to enable encryption at rest"
 }
 
 variable "elasticsearch_node_to_node_encryption_enabled" {
-  type        = "string"
-  default     = "true"
+  type        = bool
+  default     = true
   description = "Whether to enable node-to-node encryption"
 }
 
 # EBS storage must be selected for t2.small.elasticsearch
 variable "elasticsearch_ebs_volume_size" {
+  type        = number
   default     = 20
   description = "Optionally use EBS volumes for data storage by specifying volume size in GB"
 }
 
 variable "elasticsearch_instance_type" {
-  type        = "string"
+  type        = string
   default     = "t2.small.elasticsearch"
   description = "Elasticsearch instance type for data nodes in the cluster"
 }
 
 variable "elasticsearch_instance_count" {
+  type        = number
   description = "Number of data nodes in the cluster"
   default     = 4
 }
 
 # https://docs.aws.amazon.com/elasticsearch-service/latest/developerguide/es-ac.html
 variable "elasticsearch_iam_actions" {
-  type        = "list"
+  type        = list(string)
   default     = ["es:ESHttpGet", "es:ESHttpPut", "es:ESHttpPost", "es:ESHttpHead", "es:Describe*", "es:List*"]
   description = "List of actions to allow for the IAM roles, _e.g._ `es:ESHttpGet`, `es:ESHttpPut`, `es:ESHttpPost`"
 }
 
 variable "elasticsearch_enabled" {
-  type        = "string"
-  default     = "true"
+  type        = bool
+  default     = true
   description = "Set to false to prevent the module from creating any resources"
 }
 
 variable "elasticsearch_network_permitted_nodes" {
-  type        = "string"
+  type        = string
   description = "Kops kubernetes nodes that are permitted to access the elastic search network (e.g. 'nodes', 'masters', 'both' or 'any')"
   default     = "nodes"
 }
 
 variable "elasticsearch_iam_permitted_nodes" {
-  type        = "string"
+  type        = string
   description = "Kops kubernetes nodes that are permitted to use the elastic search API (e.g. 'nodes', 'masters', 'both' or 'any')"
   default     = "nodes"
 }
 
 variable "elasticsearch_iam_authorizing_role_arn" {
-  type        = "string"
+  type        = string
   description = "IAM role allowed to assume the Elasticsearch user role. Typically the role that `kiam` runs as. Full ARN, or 'nodes' or 'masters')"
   default     = "masters"
 }
 
 variable "iam_role_arns" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "List of additional IAM role ARNs to permit access to the Elasticsearch domain"
 }
 
 variable "elasticsearch_log_cleanup_enabled" {
-  type        = "string"
+  type        = bool
   description = "Set to \"true\" to enable automatic deletion of old logs"
-  default     = "true"
+  default     = true
 }
 
 variable "elasticsearch_log_retention_days" {
+  type        = number
   default     = 15
   description = "Number of days of logs to retain; logs older than this many days will be deleted."
 }
 
 variable "elasticsearch_log_index_name" {
-  type        = "string"
+  type        = string
   default     = "all"
   description = "Index/indices to process. Use a comma-separated list. Specify `all` to match every index except for `.kibana` and `.kibana_1`"
 }
 
 variable "sns_arn" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "SNS ARN to publish alerts"
 }
 
 variable "elasticsearch_iam_role_max_session_duration" {
+  type        = number
   default     = 3600
   description = "The maximum session duration (in seconds) for the role. Can have a value from 1 hour to 12 hours"
 }
 
 variable "kibana_subdomain_name" {
-  type        = "string"
+  type        = string
   default     = "kibana-elasticsearch"
   description = "Kubana subdomain"
 }
 
 variable "elasticsearch_subdomain_name" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "The name of the subdomain for Elasticsearch endpoint in the DNS zone (_e.g._ `elasticsearch`). If empty then module name will be used."
 }
 
 variable "create_iam_service_linked_role" {
-  type        = "string"
-  default     = "true"
+  type        = bool
+  default     = true
   description = "Whether to create `AWSServiceRoleForAmazonElasticsearchService` service-linked role. Set it to `false` if you already have an ElasticSearch cluster created in the AWS account and AWSServiceRoleForAmazonElasticsearchService already exists. See https://github.com/terraform-providers/terraform-provider-aws/issues/5218 for more info"
 }
