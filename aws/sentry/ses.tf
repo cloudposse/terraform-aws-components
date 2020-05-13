8ca455ae2415f57f30e9a5dd9bb5e28a33b504fb
Marcin Brański <marcin@cloudposse.com>
2020-05-13 21:54:13 +0200

    Rename output and add smtp credentials

--- aws/sentry/ses.tf
+++ aws/sentry/ses.tf
@@ -38,30 +38,28 @@ module "ses" {
   verify_dkim   = var.ses_verify_dkim
   verify_domain = var.ses_verify_domain
 }
-
-output "access_key_id" {
-  value       = module.ses.access_key_id
-  description = "The access key ID"
+output "smtp_password" {
+  sensitive   = true
+  value       = module.ses.smtp_password
+  description = "The SMTP password. This will be written to the state file in plain text."
 }
 
-output "secret_access_key" {
-  sensitive   = true
-  value       = module.ses.secret_access_key
-  description = "The secret access key. This will be written to the state file in plain-text"
+output "smtp_user" {
+  value       = module.ses.smtp_user
+  description = "The SMTP user which is access key ID."
 }
 
 output "user_name" {
   value       = module.ses.user_name
-  description = "Normalized IAM user name"
-}
-
-output "user_arn" {
-  value       = module.ses.user_arn
-  description = "The ARN assigned by AWS for this user"
+  description = "Normalized IAM user name."
 }
 
 output "user_unique_id" {
   value       = module.ses.user_unique_id
-  description = "The unique ID assigned by AWS"
+  description = "The unique ID assigned by AWS."
 }
 
+output "user_arn" {
+  value       = module.ses.user_arn
+  description = "The ARN assigned by AWS for this user."
+}
