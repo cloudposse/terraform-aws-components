53479b759e43a072e1933df2b80bc4f47d822735
Marcin Brański <marcin@cloudposse.com>
2020-07-06 18:38:46 +0200

    Modules should use proper versions

--- aws/sns-topic/main.tf
+++ aws/sns-topic/main.tf
@@ -9,20 +9,20 @@ provider "aws" {
 }
 
 module "sns" {
-  source = "git::https://github.com/cloudposse/terraform-aws-sns-topic.git?ref=0.0.1"
+  source = "git::https://github.com/cloudposse/terraform-aws-sns-topic.git?ref=0.1.0"
 
   attributes = var.attributes
   name       = var.name
   namespace  = var.namespace
   stage      = var.stage
 
-  subscribers     = var.subscribers
+  subscribers = var.subscribers
   # TODO enable after this PR gets merged https://github.com/terraform-providers/terraform-provider-aws/issues/10931
   # sqs_dlq_enabled = var.sqs_dlq_enabled
 }
 
 module "sns_monitoring" {
-  source  = "git::https://github.com/cloudposse/terraform-aws-sns-cloudwatch-alarms.git?ref=0.0.1"
+  source  = "git::https://github.com/cloudposse/terraform-aws-sns-cloudwatch-alarms.git?ref=0.1.0"
   enabled = var.monitoring_enabled
 
   sns_topic_name       = module.sns.sns_topic.name
