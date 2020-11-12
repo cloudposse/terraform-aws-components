0e690ed01323ded40ea6e3da02793a5c732e2726
aknysh <andriy.knysh@gmail.com>
2020-11-12 00:15:43 -0500

    Bump `null-label` versions

--- modules/iam-delegated-roles/main.tf
+++ modules/iam-delegated-roles/main.tf
@@ -23,7 +23,7 @@ locals {
 }
 
 module "label" {
-  source = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.2"
+  source = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.21.0"
 
   name = ""
 
