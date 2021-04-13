340ea41ac5c5339d2804e5d8816ce5046b1e6d55
Matt Gowie <matt@masterpoint.io>
2021-04-13 15:55:28 -0600

    feat: updates datadog-monitor

--- modules/datadog-monitor/default.auto.tfvars
+++ modules/datadog-monitor/default.auto.tfvars
@@ -3,7 +3,6 @@
 enabled = true
 
 datadog_monitors_config_paths = [
-  "https://raw.githubusercontent.com/cloudposse/terraform-datadog-monitor/0.9.0/catalog/aurora.yaml",
   "https://raw.githubusercontent.com/cloudposse/terraform-datadog-monitor/0.9.0/catalog/ec2.yaml",
   "https://raw.githubusercontent.com/cloudposse/terraform-datadog-monitor/0.9.0/catalog/k8s.yaml"
 ]
--- modules/datadog-monitor/ssm.tf
+++ modules/datadog-monitor/ssm.tf
@@ -1,9 +1,11 @@
 data "aws_ssm_parameter" "datadog_api_key" {
-  name            = format(var.ssm_parameter_name_format, var.ssm_path, "datadog_api_key")
+  count           = var.secrets_store_type == "SSM" ? 1 : 0
+  name            = format("/%s", var.datadog_api_secret_key)
   with_decryption = true
 }
 
 data "aws_ssm_parameter" "datadog_app_key" {
-  name            = format(var.ssm_parameter_name_format, var.ssm_path, "datadog_app_key")
+  count           = var.secrets_store_type == "SSM" ? 1 : 0
+  name            = format("/%s", var.datadog_app_secret_key)
   with_decryption = true
 }
