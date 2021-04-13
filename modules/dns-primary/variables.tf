71df2d2b5ed31893ee4a77facfc674603843987d
Matt Gowie <matt@masterpoint.io>
2021-04-13 15:26:21 -0600

    feat: upstreams dns-primary

--- modules/dns-primary/variables.tf
+++ modules/dns-primary/variables.tf
@@ -6,7 +6,7 @@ variable "region" {
 variable "domain_names" {
   type        = list(string)
   default     = null
-  description = "Root domain name list"
+  description = "Root domain name list, e.g. `[\"example.net\"]`"
 }
 
 variable "record_config" {
@@ -19,4 +19,24 @@ variable "record_config" {
     records   = list(string)
   }))
   default = []
+}
+
+# zone_ids can be found here: https://docs.aws.amazon.com/general/latest/gr/elb.html 
+variable "alias_record_config" {
+  description = "DNS Alias Record config"
+  type = list(object({
+    root_zone     = string
+    name          = string
+    type          = string
+    zone_id       = string
+    record        = string
+    target_health = string
+  }))
+  default = []
+}
+
+variable "dns_soa_config" {
+  type        = string
+  default     = "awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"
+  description = "Root domain name DNS SOA record"
 }
