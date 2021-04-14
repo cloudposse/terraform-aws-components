041b8893e86a063400464a514d0158288c983f4e
Matt Gowie <matt@masterpoint.io>
2021-04-14 12:37:00 -0600

    feat: adds spacelift component

---
+++ modules/spacelift/default.auto.tfvars
@@ -0,0 +1,15 @@
+runner_image = "TODO" # "999999999999.dkr.ecr.us-east-1.amazonaws.com/image-name:latest"
+
+worker_pool_id = "TODO"
+
+terraform_version = "0.14.10"
+
+autodeploy = false
+
+git_repository = "TODO"
+
+git_branch = "main"
+
+terraform_version_map = {
+  "0.14" = "0.14.10"
+}
---
+++ modules/spacelift/main.tf
@@ -0,0 +1,26 @@
+provider "spacelift" {}
+
+locals {
+  config_filenames   = fileset("../../../stacks", "*.yaml")
+  stack_config_files = [for f in local.config_filenames : f if(replace(f, "globals", "") == f)]
+}
+
+module "spacelift" {
+  source  = "cloudposse/cloud-infrastructure-automation/spacelift"
+  version = "0.10.2"
+
+  branch             = var.git_branch
+  components_path    = "components/terraform"
+  manage_state       = false
+  repository         = var.git_repository
+  runner_image       = var.runner_image
+  stack_config_files = local.stack_config_files
+  stack_config_path  = "../../../stacks"
+  worker_pool_id     = var.worker_pool_id
+
+  # Global defaults for all Spacelift stacks created by this project.
+  terraform_version = var.terraform_version
+  autodeploy        = var.autodeploy
+
+  terraform_version_map = var.terraform_version_map
+}
