c82cceb6f6b5dea189d1eba592f47881db61d027
nitrocode <nitrocode@users.noreply.github.com>
2021-07-23 12:33:09 -0400

    Update modules/elasticsearch/versions.tf
    Co-authored-by: Yonatan Koren <me@yonatankoren.com>

--- modules/elasticsearch/versions.tf
+++ modules/elasticsearch/versions.tf
@@ -6,13 +6,5 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.8"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = ">= 2.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 2.0"
-    }
   }
 }
