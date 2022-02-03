95ade5b36b61d2432179399bd0e9fa8639eeb899
nitrocode <nitrocode@users.noreply.github.com>
2022-02-03 10:20:47 -0600

    Revert "upgrade eks" (#392)
    * Revert "upgrade eks"
    
    * pre-commit fixes
    
    Co-authored-by: cloudpossebot <cloudpossebot@users.noreply.github.com>

--- modules/eks/versions.tf
+++ modules/eks/versions.tf
@@ -1,14 +1,18 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 0.13"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "> 3.0"
+      version = ">= 3.0"
     }
-    # spotinst = {
-    #   source  = "spotinst/spotinst"
-    #   version = ">= 1.30"
-    # }
+    template = {
+      source  = "hashicorp/template"
+      version = ">= 2.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 1.3"
+    }
   }
 }
