3a58d4439319a954d6facb901b7167c835cd5304
Benjamin Smith <ben.smith.developer@gmail.com>
2022-05-13 13:28:51 -0700

    Upgrade Component: `tfstate-backend` (#411)
    * upgrade to latest
    
    * Update modules/tfstate-backend/versions.tf
    
    Co-authored-by: Dan Miller <daniel@cloudposse.com>
    
    * pre-commit fixes
    
    Co-authored-by: Dan Miller <daniel@cloudposse.com>
    Co-authored-by: cloudpossebot <cloudpossebot@users.noreply.github.com>

--- modules/tfstate-backend/main.tf
+++ modules/tfstate-backend/main.tf
@@ -1,10 +1,10 @@
 module "tfstate_backend" {
   source  = "cloudposse/tfstate-backend/aws"
-  version = "0.33.0"
+  version = "0.38.1"
 
   force_destroy                 = var.force_destroy
   prevent_unencrypted_uploads   = var.prevent_unencrypted_uploads
   enable_server_side_encryption = var.enable_server_side_encryption
 
-  context = module.this.context
+  context = module.introspection.context
 }
