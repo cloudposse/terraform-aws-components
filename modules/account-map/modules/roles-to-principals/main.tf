92ee79a7d9f12930efe36adafdb9ce81960c8972
Benjamin Smith <ben.smith.developer@gmail.com>
2022-05-16 11:58:02 -0700

    account-map update

--- modules/account-map/modules/roles-to-principals/main.tf
+++ modules/account-map/modules/roles-to-principals/main.tf
@@ -10,23 +10,27 @@ module "always" {
 
 module "account_map" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
-  version = "0.19.0"
+  version = "0.22.1"
 
-  component               = "account-map"
-  privileged              = var.privileged
-  environment             = var.global_environment_name
-  stack_config_local_path = "../../../stacks"
-  stage                   = var.root_account_stage_name
-  tenant                  = var.global_tenant_name
+  component   = "account-map"
+  privileged  = var.privileged
+  tenant      = var.global_tenant_name
+  environment = var.global_environment_name
+  stage       = var.root_account_stage_name
 
   context = module.always.context
 }
 
 locals {
   principals = distinct(compact(flatten([for acct, v in var.role_map : (
-    contains(v, "*") ? [module.account_map.outputs.full_account_map[acct]] :
+    contains(v, "*") ? [format("arn:aws:iam::%s:root", module.account_map.outputs.full_account_map[acct])] :
     [
       for role in v : format(module.account_map.outputs.iam_role_arn_templates[acct], role)
     ]
   )])))
+
+  # Support for AWS SSO Permission Sets
+  permission_set_arn_like = distinct(compact(flatten([for acct, v in var.permission_set_map : formatlist(
+    format("arn:aws:iam::%s:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_%%s_*", module.account_map.outputs.full_account_map[acct]),
+  v)])))
 }
--- modules/account-map/modules/roles-to-principals/outputs.tf
+++ modules/account-map/modules/roles-to-principals/outputs.tf
@@ -2,3 +2,13 @@ output "principals" {
   value       = local.principals
   description = "List of AWS principals corresponding to given input role_map"
 }
+
+output "permission_set_arn_like" {
+  value       = local.permission_set_arn_like
+  description = "List of Role ARN regexes suitable for IAM Condition `ArnLike` corresponding to given input `permission_set_map`"
+}
+
+output "full_account_map" {
+  value       = module.account_map.outputs.full_account_map
+  description = "Map of account names to account IDs"
+}
