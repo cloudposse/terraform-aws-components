840ec01f911c3a9bd372ded34d8e23b9d232605e
Benjamin Smith <ben.smith.developer@gmail.com>
2022-05-16 12:21:28 -0700

    more updates to submodules of account-map

--- modules/account-map/modules/roles-to-principals/main.tf
+++ modules/account-map/modules/roles-to-principals/main.tf
@@ -1,9 +1,6 @@
-module "always" {
-  source  = "cloudposse/label/null"
-  version = "0.25.0"
-
-  # account_map must always be enabled, even for components that are disabled
-  enabled = true
+module "stack_config" {
+  source  = "cloudposse/stack-config/yaml"
+  version = "0.22.1"
 
   context = module.this.context
 }
@@ -12,25 +9,23 @@ module "account_map" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
   version = "0.22.1"
 
+  config      = module.stack_config.config
   component   = "account-map"
   privileged  = var.privileged
-  tenant      = var.global_tenant_name
-  environment = var.global_environment_name
-  stage       = var.root_account_stage_name
 
-  context = module.always.context
+  context = module.this.context
 }
 
 locals {
   principals = distinct(compact(flatten([for acct, v in var.role_map : (
-    contains(v, "*") ? [format("arn:aws:iam::%s:root", module.account_map.outputs.full_account_map[acct])] :
-    [
-      for role in v : format(module.account_map.outputs.iam_role_arn_templates[acct], role)
+    contains(v, "*") ? [module.account_map.outputs.full_account_map[acct]] : [
+      for role in v : format(var.iam_role_arn_template,
+        module.account_map.outputs.full_account_map[acct],
+        module.this.namespace,
+        module.this.environment,
+        acct,
+        role
+      )
     ]
   )])))
-
-  # Support for AWS SSO Permission Sets
-  permission_set_arn_like = distinct(compact(flatten([for acct, v in var.permission_set_map : formatlist(
-    format("arn:aws:iam::%s:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_%%s_*", module.account_map.outputs.full_account_map[acct]),
-  v)])))
 }
