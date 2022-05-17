d4c75a9d274fd4fff7c802342bb5060861bc1991
milldr <miller0daniel@gmail.com>
2022-05-17 10:25:55 -0400

    upstream s3-bucket

--- modules/s3-bucket/default.auto.tfvars
+++ modules/s3-bucket/default.auto.tfvars
@@ -1,1 +1,5 @@
+# This file is included by default in terraform plans
+
 enabled = false
+
+name = "s3-bucket"
--- modules/s3-bucket/providers.tf
+++ modules/s3-bucket/providers.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = var.region
 
   profile = module.iam_roles.profiles_enabled ? coalesce(var.import_profile_name, module.iam_roles.terraform_profile_name) : null
+
   dynamic "assume_role" {
     for_each = module.iam_roles.profiles_enabled ? [] : ["role"]
     content {
@@ -11,8 +12,9 @@ provider "aws" {
 }
 
 module "iam_roles" {
-  source  = "../account-map/modules/iam-roles"
-  context = module.this.context
+  source             = "../account-map/modules/iam-roles"
+  global_tenant_name = var.root_account_tenant_name
+  context            = module.this.context
 }
 
 variable "import_profile_name" {
@@ -25,4 +27,10 @@ variable "import_role_arn" {
   type        = string
   default     = null
   description = "IAM Role ARN to use when importing a resource"
+}
+
+variable "root_account_tenant_name" {
+  type        = string
+  description = "The tenant name for the root account"
+  default     = null
 }
