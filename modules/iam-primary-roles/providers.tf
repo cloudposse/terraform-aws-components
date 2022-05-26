d5749437d308f077542061398620f2ea840f3eec
Benjamin Smith <ben.smith.developer@gmail.com>
2022-05-26 11:15:46 -0700

    update iam components with dynamic profiles

--- modules/iam-primary-roles/providers.tf
+++ modules/iam-primary-roles/providers.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region = var.region
 
+  profile = module.iam_roles.profiles_enabled ? coalesce(var.import_profile_name, module.iam_roles.terraform_profile_name) : null
+
   dynamic "assume_role" {
     for_each = var.import_role_arn == null ? (module.iam_roles.org_role_arn != null ? [true] : []) : ["import"]
     content {
@@ -13,6 +15,12 @@ module "iam_roles" {
   source     = "../account-map/modules/iam-roles"
   privileged = true
   context    = module.this.context
+}
+
+variable "import_profile_name" {
+  type        = string
+  default     = null
+  description = "AWS Profile name to use when importing a resource"
 }
 
 variable "import_role_arn" {
