5d60991566348683fb9be842669e4ff48cd51033
Benjamin Smith <ben.smith.developer@gmail.com>
2022-05-26 17:15:36 -0700

    PR comments

--- modules/aws-sso/versions.tf
+++ modules/aws-sso/versions.tf
@@ -4,23 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.63"
-    }
-    external = {
-      source  = "hashicorp/external"
-      version = "~> 2.1"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.2"
-    }
-    utils = {
-      source  = "cloudposse/utils"
-      version = ">= 0.17.23"
+      version = "~> 4.0"
     }
   }
 }
--- modules/iam-delegated-roles/versions.tf
+++ modules/iam-delegated-roles/versions.tf
@@ -4,19 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 1.3"
-    }
-    utils = {
-      source  = "cloudposse/utils"
-      version = ">= 0.17.23"
+      version = "~> 4.0"
     }
   }
 }
