a6c884d5ec4d8f82bced7c32f1b2346603874c7c
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-06-07 15:02:24 -0400

    Add delegated identity for aws-sso component (#419)
    * Use aws.root to create root aws_ssoadmin_account_assignment
    
    * Ignore .terraform-* directories

--- modules/aws-sso/providers.tf
+++ modules/aws-sso/providers.tf
@@ -1,3 +1,27 @@
 provider "aws" {
   region = var.region
+
+  dynamic "assume_role" {
+    for_each = module.iam_roles.org_role_arn != null ? [true] : []
+    content {
+      role_arn = coalesce(var.import_role_arn, module.iam_roles.org_role_arn)
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "root"
+  region = var.region
+}
+
+module "iam_roles" {
+  source     = "../account-map/modules/iam-roles"
+  privileged = true
+  context    = module.this.context
+}
+
+variable "import_role_arn" {
+  type        = string
+  default     = null
+  description = "IAM Role ARN to use when importing a resource"
 }
