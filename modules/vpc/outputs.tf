edc396d75c6af2347fb37d4a1ba3773986841ee9
Benjamin Smith <ben.smith.developer@gmail.com>
2022-06-08 09:15:46 -0700

    `[VPC]` Upstream (#418)
    * upstream vpc
    
    * pre-commit fixes
    
    * upstream newest vpc
    
    * pre-commit fixes
    
    Co-authored-by: cloudpossebot <cloudpossebot@users.noreply.github.com>

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -18,6 +18,30 @@ output "private_subnet_cidrs" {
   description = "Private subnet CIDRs"
 }
 
+output "subnets" {
+  value = {
+    public : {
+      ids : module.subnets.public_subnet_ids
+      cidr : module.subnets.public_subnet_cidrs
+    }
+    private : {
+      ids : module.subnets.private_subnet_ids
+      cidr : module.subnets.private_subnet_cidrs
+    }
+  }
+  description = "Subnets info map"
+}
+
+output "vpc_default_network_acl_id" {
+  value       = module.vpc.vpc_default_network_acl_id
+  description = "The ID of the network ACL created by default on VPC creation"
+}
+
+output "vpc_default_security_group_id" {
+  value       = module.vpc.vpc_default_security_group_id
+  description = "The ID of the security group created by default on VPC creation"
+}
+
 output "vpc_id" {
   value       = module.vpc.vpc_id
   description = "VPC ID"
@@ -28,6 +52,16 @@ output "vpc_cidr" {
   description = "VPC CIDR"
 }
 
+output "vpc" {
+  value = {
+    id : module.vpc.vpc_id
+    cidr : module.vpc.vpc_cidr_block
+    subnet_type_tag_key : var.subnet_type_tag_key
+    # subnet_type_tag_value_format : var.subnet_type_tag_value_format
+  }
+  description = "VPC info map"
+}
+
 output "private_route_table_ids" {
   value       = module.subnets.private_route_table_ids
   description = "Private subnet route table IDs"
@@ -36,6 +70,18 @@ output "private_route_table_ids" {
 output "public_route_table_ids" {
   value       = module.subnets.public_route_table_ids
   description = "Public subnet route table IDs"
+}
+
+output "route_tables" {
+  value = {
+    public : {
+      ids : module.subnets.public_route_table_ids
+    }
+    private : {
+      ids : module.subnets.private_route_table_ids
+    }
+  }
+  description = "Route tables info map"
 }
 
 output "nat_gateway_ids" {
@@ -56,4 +102,19 @@ output "nat_gateway_public_ips" {
 output "max_subnet_count" {
   value       = local.max_subnet_count
   description = "Maximum allowed number of subnets before all subnet CIDRs need to be recomputed"
+}
+
+output "nat_eip_protections" {
+  description = "List of AWS Shield Advanced Protections for NAT Elastic IPs."
+  value       = aws_shield_protection.nat_eip_shield_protection
+}
+
+output "interface_vpc_endpoints" {
+  description = "List of Interface VPC Endpoints in this VPC."
+  value       = try(module.vpc_endpoints[0].interface_vpc_endpoints, [])
+}
+
+output "availability_zones" {
+  description = "List of Availability Zones where subnets were created"
+  value       = local.availability_zones
 }
