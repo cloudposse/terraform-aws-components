e9686974697785c2939cebd3d60245fe156e9a67
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-06-28 14:06:54 -0400

    Init rds component (#431)
    * Init rds component
    
    * Bump providers

---
+++ modules/rds/rds-variables.tf
@@ -0,0 +1,351 @@
+variable "dns_zone_id" {
+  type        = string
+  default     = ""
+  description = "The ID of the DNS Zone in Route53 where a new DNS record will be created for the DB host name"
+}
+
+variable "host_name" {
+  type        = string
+  default     = "db"
+  description = "The DB host name created in Route53"
+}
+
+variable "security_group_ids" {
+  type        = list(string)
+  default     = []
+  description = "The IDs of the security groups from which to allow `ingress` traffic to the DB instance"
+}
+
+variable "allowed_cidr_blocks" {
+  type        = list(string)
+  default     = []
+  description = "The whitelisted CIDRs which to allow `ingress` traffic to the DB instance"
+}
+
+variable "associate_security_group_ids" {
+  type        = list(string)
+  default     = []
+  description = "The IDs of the existing security groups to associate with the DB instance"
+}
+
+variable "database_name" {
+  type        = string
+  description = "The name of the database to create when the DB instance is created"
+}
+
+# Don't use `admin`
+# Read more: <https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_Limits.html>
+# ("MasterUsername admin cannot be used as it is a reserved word used by the engine")
+variable "database_user" {
+  type        = string
+  description = "Database admin user name"
+  default     = ""
+
+  validation {
+    condition = (
+      length(var.database_user) == 0 ||
+      (var.database_user != "admin" &&
+        length(var.database_user) >= 1 &&
+      length(var.database_user) <= 16)
+    )
+    error_message = "Per the RDS API, admin cannot be used as it is a reserved word used by the engine. Master username must be between 1 and 16 characters. If null is provided then a random string will be used."
+  }
+}
+
+# Must be longer than 8 chars
+# Read more: <https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_Limits.html>
+# ("The parameter MasterUserPassword is not a valid password because it is shorter than 8 characters")
+variable "database_password" {
+  type        = string
+  description = "Database password for the admin user"
+  default     = ""
+
+  # "sensitive" required Terraform 0.14 or later
+  #  sensitive   = true
+
+  validation {
+    condition = (
+      length(var.database_password) == 0 ||
+      (length(var.database_password) >= 8 &&
+      length(var.database_password) <= 128)
+    )
+    error_message = "Per the RDS API, master password must be between 8 and 128 characters. If null is provided then a random password will be used."
+  }
+}
+
+variable "database_port" {
+  type        = number
+  description = "Database port (_e.g._ `3306` for `MySQL`). Used in the DB Security Group to allow access to the DB instance from the provided `security_group_ids`"
+}
+
+variable "deletion_protection" {
+  type        = bool
+  description = "Set to true to enable deletion protection on the RDS instance"
+  default     = false
+}
+
+variable "multi_az" {
+  type        = bool
+  description = "Set to true if multi AZ deployment must be supported"
+  default     = false
+}
+
+variable "storage_type" {
+  type        = string
+  description = "One of 'standard' (magnetic), 'gp2' (general purpose SSD), or 'io1' (provisioned IOPS SSD)"
+  default     = "standard"
+}
+
+variable "storage_encrypted" {
+  type        = bool
+  description = "(Optional) Specifies whether the DB instance is encrypted. The default is false if not specified"
+  default     = true
+}
+
+variable "iops" {
+  type        = number
+  description = "The amount of provisioned IOPS. Setting this implies a storage_type of 'io1'. Default is 0 if rds storage type is not 'io1'"
+  default     = 0
+}
+
+variable "allocated_storage" {
+  type        = number
+  description = "The allocated storage in GBs"
+}
+
+variable "max_allocated_storage" {
+  type        = number
+  description = "The upper limit to which RDS can automatically scale the storage in GBs"
+  default     = 0
+}
+
+variable "engine" {
+  type        = string
+  description = "Database engine type"
+  # http://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html
+  # - mysql
+  # - postgres
+  # - oracle-*
+  # - sqlserver-*
+}
+
+variable "engine_version" {
+  type        = string
+  description = "Database engine version, depends on engine type"
+  # http://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html
+}
+
+variable "major_engine_version" {
+  type        = string
+  description = "Database MAJOR engine version, depends on engine type"
+  default     = ""
+  # https://docs.aws.amazon.com/cli/latest/reference/rds/create-option-group.html
+}
+
+variable "charset_name" {
+  type        = string
+  description = "The character set name to use for DB encoding. [Oracle & Microsoft SQL only](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_instance#character_set_name). For other engines use `db_parameter`"
+  default     = null
+}
+
+variable "license_model" {
+  type        = string
+  description = "License model for this DB. Optional, but required for some DB Engines. Valid values: license-included | bring-your-own-license | general-public-license"
+  default     = ""
+}
+
+variable "instance_class" {
+  type        = string
+  description = "Class of RDS instance"
+  # https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.DBInstanceClass.html
+}
+
+# This is for custom parameters to be passed to the DB
+# We're "cloning" default ones, but we need to specify which should be copied
+variable "db_parameter_group" {
+  type        = string
+  description = "The DB parameter group family name. The value depends on DB engine used. See [DBParameterGroupFamily](https://docs.aws.amazon.com/AmazonRDS/latest/APIReference/API_CreateDBParameterGroup.html#API_CreateDBParameterGroup_RequestParameters) for instructions on how to retrieve applicable value."
+  # "mysql5.6"
+  # "postgres9.5"
+}
+
+variable "publicly_accessible" {
+  type        = bool
+  description = "Determines if database can be publicly available (NOT recommended)"
+  default     = false
+}
+
+variable "availability_zone" {
+  type        = string
+  default     = null
+  description = "The AZ for the RDS instance. Specify one of `subnet_ids`, `db_subnet_group_name` or `availability_zone`. If `availability_zone` is provided, the instance will be placed into the default VPC or EC2 Classic"
+}
+
+variable "db_subnet_group_name" {
+  type        = string
+  default     = null
+  description = "Name of DB subnet group. DB instance will be created in the VPC associated with the DB subnet group. Specify one of `subnet_ids`, `db_subnet_group_name` or `availability_zone`"
+}
+
+variable "auto_minor_version_upgrade" {
+  type        = bool
+  description = "Allow automated minor version upgrade (e.g. from Postgres 9.5.3 to Postgres 9.5.4)"
+  default     = true
+}
+
+variable "allow_major_version_upgrade" {
+  type        = bool
+  description = "Allow major version upgrade"
+  default     = false
+}
+
+variable "apply_immediately" {
+  type        = bool
+  description = "Specifies whether any database modifications are applied immediately, or during the next maintenance window"
+  default     = false
+}
+
+variable "maintenance_window" {
+  type        = string
+  description = "The window to perform maintenance in. Syntax: 'ddd:hh24:mi-ddd:hh24:mi' UTC "
+  default     = "Mon:03:00-Mon:04:00"
+}
+
+variable "skip_final_snapshot" {
+  type        = bool
+  description = "If true (default), no snapshot will be made before deleting DB"
+  default     = true
+}
+
+variable "copy_tags_to_snapshot" {
+  type        = bool
+  description = "Copy tags from DB to a snapshot"
+  default     = true
+}
+
+variable "backup_retention_period" {
+  type        = number
+  description = "Backup retention period in days. Must be > 0 to enable backups"
+  default     = 0
+}
+
+variable "backup_window" {
+  type        = string
+  description = "When AWS can perform DB snapshots, can't overlap with maintenance window"
+  default     = "22:00-03:00"
+}
+
+variable "db_parameter" {
+  type = list(object({
+    apply_method = string
+    name         = string
+    value        = string
+  }))
+  default     = []
+  description = "A list of DB parameters to apply. Note that parameters may differ from a DB family to another"
+}
+
+variable "db_options" {
+  type = list(object({
+    db_security_group_memberships  = list(string)
+    option_name                    = string
+    port                           = number
+    version                        = string
+    vpc_security_group_memberships = list(string)
+
+    option_settings = list(object({
+      name  = string
+      value = string
+    }))
+  }))
+
+  default     = []
+  description = "A list of DB options to apply with an option group. Depends on DB engine"
+}
+
+variable "snapshot_identifier" {
+  type        = string
+  description = "Snapshot identifier e.g: rds:production-2019-06-26-06-05. If specified, the module create cluster from the snapshot"
+  default     = null
+}
+
+variable "final_snapshot_identifier" {
+  type        = string
+  description = "Final snapshot identifier e.g.: some-db-final-snapshot-2019-06-26-06-05"
+  default     = ""
+}
+
+variable "parameter_group_name" {
+  type        = string
+  description = "Name of the DB parameter group to associate"
+  default     = ""
+}
+
+variable "option_group_name" {
+  type        = string
+  description = "Name of the DB option group to associate"
+  default     = ""
+}
+
+variable "kms_key_arn" {
+  type        = string
+  description = "The ARN of the existing KMS key to encrypt storage"
+  default     = ""
+}
+
+variable "performance_insights_enabled" {
+  type        = bool
+  default     = false
+  description = "Specifies whether Performance Insights are enabled."
+}
+
+variable "performance_insights_kms_key_id" {
+  type        = string
+  default     = null
+  description = "The ARN for the KMS key to encrypt Performance Insights data. Once KMS key is set, it can never be changed."
+}
+
+variable "performance_insights_retention_period" {
+  type        = number
+  default     = 7
+  description = "The amount of time in days to retain Performance Insights data. Either 7 (7 days) or 731 (2 years)."
+}
+
+variable "enabled_cloudwatch_logs_exports" {
+  type        = list(string)
+  default     = []
+  description = "List of log types to enable for exporting to CloudWatch logs. If omitted, no logs will be exported. Valid values (depending on engine): alert, audit, error, general, listener, slowquery, trace, postgresql (PostgreSQL), upgrade (PostgreSQL)."
+}
+
+variable "ca_cert_identifier" {
+  type        = string
+  description = "The identifier of the CA certificate for the DB instance"
+  default     = null
+}
+
+variable "monitoring_interval" {
+  description = "The interval, in seconds, between points when Enhanced Monitoring metrics are collected for the DB instance. To disable collecting Enhanced Monitoring metrics, specify 0. Valid Values are 0, 1, 5, 10, 15, 30, 60."
+  default     = "0"
+}
+
+variable "monitoring_role_arn" {
+  type        = string
+  description = "The ARN for the IAM role that permits RDS to send enhanced monitoring metrics to CloudWatch Logs"
+  default     = null
+}
+
+variable "iam_database_authentication_enabled" {
+  description = "Specifies whether or mappings of AWS Identity and Access Management (IAM) accounts to database accounts is enabled"
+  default     = false
+}
+
+variable "replicate_source_db" {
+  description = "If the rds db instance is a replica, supply the source database identifier here"
+  default     = null
+}
+
+variable "timezone" {
+  type        = string
+  description = "Time zone of the DB instance. timezone is currently only supported by Microsoft SQL Server. The timezone can only be set on creation. See [MSSQL User Guide](http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_SQLServer.html#SQLServer.Concepts.General.TimeZone) for more information."
+  default     = null
+}
