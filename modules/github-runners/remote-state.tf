e5d1732ab030fc0d9762beff9395d446babbd37b
Benjamin Smith <ben.smith.developer@gmail.com>
2022-06-29 12:47:11 -0700

    upstream `github-runners` (#432)
    * upstream github runners
    
    * bump versions
    
    * remove introspection
    
    * readme update
    
    * Update variables.tf
    
    Co-authored-by: nitrocode <7775707+nitrocode@users.noreply.github.com>

--- modules/github-runners/remote-state.tf
+++ modules/github-runners/remote-state.tf
@@ -1,22 +1,20 @@
 module "vpc" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
-  version = "0.19.0"
+  version = "0.22.3"
 
-  stack_config_local_path = "../../../stacks"
-  component               = "vpc"
+  component = "vpc"
 
   context = module.this.context
 }
 
 module "account_map" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
-  version = "0.19.0"
+  version = "0.22.3"
 
-  stack_config_local_path = "../../../stacks"
-  component               = "account-map"
-  environment             = var.account_map_environment_name
-  stage                   = var.account_map_stage_name
-  tenant                  = var.account_map_tenant_name
+  component   = "account-map"
+  environment = var.account_map_environment_name
+  stage       = var.account_map_stage_name
+  tenant      = var.account_map_tenant_name
 
   context = module.this.context
 }
