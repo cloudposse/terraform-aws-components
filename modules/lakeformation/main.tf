66439947f9663850fb0477331edcbbab13bd751d
Dan Meyers <danjbh@users.noreply.github.com>
2022-08-01 16:27:48 -0700

    Fixing local admin_arn_list reference

--- modules/lakeformation/main.tf
+++ modules/lakeformation/main.tf
@@ -14,7 +14,7 @@ module "lakeformation" {
   s3_bucket_arn                = var.s3_bucket_arn
   role_arn                     = var.role_arn
   catalog_id                   = var.catalog_id
-  admin_arn_list               = var.admin_arn_list
+  admin_arn_list               = local.admin_arn_list
   trusted_resource_owners      = var.trusted_resource_owners
   database_default_permissions = var.database_default_permissions
   table_default_permissions    = var.table_default_permissions
