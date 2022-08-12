6382ada1298cd3f1cd0e3bba7026d8e6787544b2
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-08-12 10:52:23 -0500

    Github runner fixes (#454)

--- modules/github-runners/remote-state.tf
+++ modules/github-runners/remote-state.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
-  version = "0.22.3"
+  version = "0.22.4"
 
   component = "vpc"
 
@@ -9,7 +9,7 @@ module "vpc" {
 
 module "account_map" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
-  version = "0.22.3"
+  version = "0.22.4"
 
   component   = "account-map"
   environment = var.account_map_environment_name
