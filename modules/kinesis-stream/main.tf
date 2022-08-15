5c479d99b927292f5ddd5c4e5ec18255c0498317
Dan Meyers <danjbh@users.noreply.github.com>
2022-08-15 09:58:28 -0700

    Adding new kinesis-stream component

---
+++ modules/kinesis-stream/main.tf
@@ -0,0 +1,14 @@
+module "kinesis" {
+  source  = "cloudposse/kinesis-stream/aws"
+  version = "0.1.0"
+
+  shard_count               = var.shard_count
+  retention_period          = var.retention_period
+  shard_level_metrics       = var.shard_level_metrics
+  enforce_consumer_deletion = var.enforce_consumer_deletion
+  encryption_type           = var.encryption_type
+  kms_key_id                = var.kms_key_id
+  stream_mode               = var.stream_mode
+
+  context = module.this.context
+}
