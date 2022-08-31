87e41980c8968a5afc3117d6c38646fbb5b1b41d
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-08-31 11:24:43 -0500

    Update providers.tf

--- modules/sftp/providers.tf
+++ modules/sftp/providers.tf
@@ -1,16 +1,26 @@
 provider "aws" {
   region = var.region
 
-  assume_role {
-    role_arn = coalesce(var.import_role_arn, module.iam_roles.terraform_role_arn)
+  profile = module.iam_roles.profiles_enabled ? coalesce(var.import_profile_name, module.iam_roles.terraform_profile_name) : null
+
+  dynamic "assume_role" {
+    for_each = module.iam_roles.profiles_enabled ? [] : ["role"]
+    content {
+      role_arn = coalesce(var.import_role_arn, module.iam_roles.terraform_role_arn)
+    }
   }
 }
 
 provider "awsutils" {
   region = var.region
 
-  assume_role {
-    role_arn = coalesce(var.import_role_arn, module.iam_roles.terraform_role_arn)
+  profile = module.iam_roles.profiles_enabled ? coalesce(var.import_profile_name, module.iam_roles.terraform_profile_name) : null
+
+  dynamic "assume_role" {
+    for_each = module.iam_roles.profiles_enabled ? [] : ["role"]
+    content {
+      role_arn = coalesce(var.import_role_arn, module.iam_roles.terraform_role_arn)
+    }
   }
 }
 
