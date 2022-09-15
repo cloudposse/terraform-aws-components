0d7cf9be235f140f153fe38885f0e9f06679bf39
Joe Niland <joe@originalmind.com.au>
2022-09-16 07:08:13 +1000

    s3-bucket: allow usage without providing a value for `tenant`

--- modules/s3-bucket/remote-state.tf
+++ modules/s3-bucket/remote-state.tf
@@ -5,7 +5,7 @@ module "account_map" {
   component   = "account-map"
   environment = var.account_map_environment_name
   stage       = var.account_map_stage_name
-  tenant      = coalesce(var.account_map_tenant_name, module.this.tenant)
+  tenant      = try(coalesce(var.account_map_tenant_name, module.this.tenant), "")
 
   context = module.this.context
 }
