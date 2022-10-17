4390df1f208bc54b97bf693873b13543be02232a
milldr <miller0daniel@gmail.com>
2022-10-17 16:19:09 -0700

    upstream github-runners

--- modules/github-runners/variables.tf
+++ modules/github-runners/variables.tf
@@ -68,6 +68,7 @@ variable "github_scope" {
 variable "wait_for_capacity_timeout" {
   type        = string
   description = "A maximum duration that Terraform should wait for ASG instances to be healthy before timing out. (See also Waiting for Capacity below.) Setting this to '0' causes Terraform to skip all Capacity Waiting behavior"
+  default     = "10m"
 }
 
 variable "cpu_utilization_high_evaluation_periods" {
