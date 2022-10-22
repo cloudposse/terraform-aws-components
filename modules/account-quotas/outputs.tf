80d80ae704e030bcba833d20f0a858b0e6ba81a0
cloudpossebot <cloudpossebot@users.noreply.github.com>
2022-10-22 03:47:44 +0000

    pre-commit fixes

--- modules/account-quotas/outputs.tf
+++ modules/account-quotas/outputs.tf
@@ -1,4 +1,4 @@
 output "quotas" {
-  value = aws_servicequotas_service_quota.this
+  value       = aws_servicequotas_service_quota.this
   description = "Full report on all service quotas managed by this component."
 }
