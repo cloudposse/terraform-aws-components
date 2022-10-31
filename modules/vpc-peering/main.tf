4145947482fd1e4c8087cc7cffd564754ca0c208
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-10-31 08:27:17 -0500

    Allow vpc-peering to peer v2 to v2 (#521)

--- modules/vpc-peering/main.tf
+++ modules/vpc-peering/main.tf
@@ -1,10 +1,25 @@
 locals {
+  enabled = module.this.enabled
+
   requester_vpc_id = module.requester_vpc.outputs.vpc_id
+
+  accepter_aws_assume_role_arn = var.accepter_stage_name != null ? module.iam_roles.terraform_role_arns[var.accepter_stage_name] : var.accepter_aws_assume_role_arn
+}
+
+data "aws_vpc" "accepter" {
+  count = local.enabled ? 1 : 0
+
+  provider = aws.accepter
+
+  id         = lookup(var.accepter_vpc, "id", null)
+  cidr_block = lookup(var.accepter_vpc, "cidr_block", null)
+  default    = lookup(var.accepter_vpc, "default", null)
+  tags       = lookup(var.accepter_vpc, "tags", null)
 }
 
 module "vpc_peering" {
   source  = "cloudposse/vpc-peering-multi-account/aws"
-  version = "0.17.1"
+  version = "0.19.1"
 
   auto_accept = var.auto_accept
 
@@ -14,9 +29,9 @@ module "vpc_peering" {
   requester_vpc_id                          = local.requester_vpc_id
 
   accepter_allow_remote_vpc_dns_resolution = var.accepter_allow_remote_vpc_dns_resolution
-  accepter_aws_assume_role_arn             = var.accepter_aws_assume_role_arn
+  accepter_aws_assume_role_arn             = local.accepter_aws_assume_role_arn
   accepter_region                          = var.accepter_region
-  accepter_vpc_id                          = var.accepter_vpc_id
+  accepter_vpc_id                          = one(data.aws_vpc.accepter[*].id)
 
   context = module.this.context
 }
