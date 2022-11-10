55495449c9101b779385a62439e2755a9d3b7b82
Matt Gowie <matt@masterpoint.io>
2022-11-10 12:11:49 -0700

    feat: updates kms comp with embedded policy creation

--- modules/kms/variables.tf
+++ modules/kms/variables.tf
@@ -3,6 +3,18 @@ variable "region" {
   description = "AWS Region"
 }
 
+variable "alias" {
+  type        = string
+  description = "The display name of the alias. The name must start with the word alias followed by a forward slash. If not specified, the alias name will be auto-generated."
+  default     = null
+}
+
+variable "description" {
+  type        = string
+  description = "The description for the KMS Key."
+  default     = null
+}
+
 variable "deletion_window_in_days" {
   type        = number
   default     = 10
@@ -15,28 +27,16 @@ variable "enable_key_rotation" {
   description = "Specifies whether key rotation is enabled"
 }
 
-variable "description" {
-  type        = string
-  default     = "Parameter Store KMS master key"
-  description = "The description of the key as viewed in AWS console"
-}
-
-variable "alias" {
-  type        = string
-  default     = ""
-  description = "The display name of the alias. The name must start with the word `alias` followed by a forward slash. If not specified, the alias name will be auto-generated."
-}
-
-variable "policy" {
-  type        = string
-  default     = ""
-  description = "A valid KMS policy JSON document. Note that if the policy document is not specific enough (but still valid), Terraform may view the policy as constantly changing in a terraform plan. In this case, please make sure you use the verbose/specific version of the policy."
-}
-
 variable "key_usage" {
   type        = string
   default     = "ENCRYPT_DECRYPT"
   description = "Specifies the intended use of the key. Valid values: `ENCRYPT_DECRYPT` or `SIGN_VERIFY`."
+}
+
+variable "multi_region" {
+  type        = bool
+  default     = false
+  description = "Indicates whether the KMS key is a multi-Region (true) or regional (false) key."
 }
 
 variable "customer_master_key_spec" {
@@ -45,8 +45,17 @@ variable "customer_master_key_spec" {
   description = "Specifies whether the key contains a symmetric key or an asymmetric key pair and the encryption algorithms or signing algorithms that the key supports. Valid values: `SYMMETRIC_DEFAULT`, `RSA_2048`, `RSA_3072`, `RSA_4096`, `ECC_NIST_P256`, `ECC_NIST_P384`, `ECC_NIST_P521`, or `ECC_SECG_P256K1`."
 }
 
-variable "multi_region" {
-  type        = bool
-  default     = false
-  description = "Indicates whether the KMS key is a multi-Region (true) or regional (false) key."
+variable "allowed_roles" {
+  type        = map(list(string))
+  description = <<-EOT
+    Map of account:[role, role...] specifying roles allowed to assume the role.
+    Roles are symbolic names like `ops` or `terraform`. Use `*` as role for entire account.
+    EOT
+  default     = {}
+}
+
+variable "allowed_principal_arns" {
+  type        = list(string)
+  description = "List of AWS principal ARNs allowed to assume the role."
+  default     = []
 }
