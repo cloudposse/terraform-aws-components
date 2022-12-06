fa25adff5934ad13a007abf60f19d3a196d1fb23
Benjamin Smith <ben.smith.developer@gmail.com>
2022-12-06 14:56:48 -0800

    upstream spacelift

--- modules/spacelift/versions.tf
+++ modules/spacelift/versions.tf
@@ -4,12 +4,7 @@ terraform {
   required_providers {
     spacelift = {
       source  = "spacelift-io/spacelift"
-      version = ">= 0.1.29"
-    }
-    utils = {
-      source = "cloudposse/utils"
-      # problem with 1.4.0
-      version = ">= 1.3.0, != 1.4.0"
+      version = "~> 0.1.31"
     }
   }
 }
