e12edc2b2c1c06e68af56ef1b6c941bb03062225
Benjamin Smith <ben.smith.developer@gmail.com>
2022-12-21 10:34:08 -0800

    update providers

--- modules/datadog-integration/providers.tf
+++ modules/datadog-integration/providers.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = var.region
 
   profile = module.iam_roles.profiles_enabled ? coalesce(var.import_profile_name, module.iam_roles.terraform_profile_name) : null
+
   dynamic "assume_role" {
     for_each = module.iam_roles.profiles_enabled ? [] : ["role"]
     content {
@@ -25,17 +26,4 @@ variable "import_role_arn" {
   type        = string
   default     = null
   description = "IAM Role ARN to use when importing a resource"
-}
-
-module "datadog_configuration" {
-  source  = "../datadog-configuration/modules/datadog_keys"
-  region  = var.region
-  context = module.this.context
-}
-
-provider "datadog" {
-  api_key  = module.datadog_configuration.datadog_api_key
-  app_key  = module.datadog_configuration.datadog_app_key
-  api_url  = module.datadog_configuration.datadog_api_url
-  validate = local.enabled
 }
