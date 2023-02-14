bff3ec929a891d40b03bb10ccf701b4d0bfdc42e
Benjamin Smith <ben.smith.developer@gmail.com>
2023-02-14 09:29:57 -0800

    aws backup upstream

--- modules/aws-backup/versions.tf
+++ modules/aws-backup/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 1.0.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
+      version = ">= 4.9.0"
     }
   }
 }
