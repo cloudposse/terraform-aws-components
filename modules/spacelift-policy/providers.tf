790a2981e4d5a3cf8415662ab03a5a952a5e0f1d
nitrocode <7775707+nitrocode@users.noreply.github.com>
2023-02-22 22:33:08 -0600

    Add spacelift-policy component (#556)
    Co-authored-by: Andriy Knysh <aknysh@users.noreply.github.com>

---
+++ modules/spacelift-policy/providers.tf
@@ -0,0 +1,5 @@
+# Purposely did not add spacelift inputs since this is a sensitive change and
+# it should not be an easy thing to plan this locally. Best to use the exported
+# inputs when this is necessary to plan locally.
+
+provider "spacelift" {}
