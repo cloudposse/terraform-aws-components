3ca8daf52016d135a54358ab51e7a5cbb2f83ace
Igor Rodionov <goruha@gmail.com>
2023-03-13 17:47:37 +0300

    Fixe SSO SAML provider fixes

--- modules/sso-saml-provider/variables.tf
+++ modules/sso-saml-provider/variables.tf
@@ -7,3 +7,21 @@ variable "ssm_path_prefix" {
   type        = string
   description = "Top level SSM path prefix (without leading or trailing slash)"
 }
+
+variable "usernameAttr" {
+  type        = string
+  description = "User name attribute"
+  default     = null
+}
+
+variable "emailAttr" {
+  type        = string
+  description = "Email attribute"
+  default     = null
+}
+
+variable "groupsAttr" {
+  type        = string
+  description = "Group attribute"
+  default     = null
+}
