3df711c7013aa3762c004d122477175fa4125452
nitrocode <7775707+nitrocode@users.noreply.github.com>
2023-03-26 21:32:51 -0500

    Apply suggestions from code review

--- modules/eks/datadog-agent/versions.tf
+++ modules/eks/datadog-agent/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.38"
+      version = ">= 4.9.0"
     }
     helm = {
       source  = "hashicorp/helm"
