1aa55ad8497d224e387339a33fff4f7b775132e2
milldr <miller0daniel@gmail.com>
2023-04-06 11:08:50 -0700

    missing descriptions

--- modules/tgw/cross-region-spoke/modules/tgw_routes/outputs.tf
+++ modules/tgw/cross-region-spoke/modules/tgw_routes/outputs.tf
@@ -1,3 +1,4 @@
 output "aws_ec2_transit_gateway_routes" {
-  value = aws_ec2_transit_gateway_route.default
+  value       = aws_ec2_transit_gateway_route.default
+  description = "AWS EC2 Transit Gateway routes"
 }
