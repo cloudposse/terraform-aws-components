dfe53fe067a77bd2b673b4a17f48debab4371a82
Dan Miller <daniel@cloudposse.com>
2023-04-25 07:28:53 -0700

    Upstream: `eks/ebs-controller` (#640)
    Co-authored-by: Andriy Knysh <aknysh@users.noreply.github.com>

---
+++ modules/eks/ebs-controller/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.7.1"
+    }
+  }
+}
