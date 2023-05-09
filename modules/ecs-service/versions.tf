45fee37879148d3ec52d8ff9f8a24c8d17a07419
Igor Rodionov <goruha@gmail.com>
2023-05-09 18:40:18 +0300

    [ecs-service] Added IAM policies for ecspresso deployments (#659)
    Co-authored-by: cloudpossebot <cloudpossebot@users.noreply.github.com>
    Co-authored-by: nitrocode <7775707+nitrocode@users.noreply.github.com>

--- modules/ecs-service/versions.tf
+++ modules/ecs-service/versions.tf
@@ -3,8 +3,16 @@ terraform {
 
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
+      source = "hashicorp/aws"
+      ## 4.66.0 version cause error
+      ## │ Error: listing tags for Application Auto Scaling Target (): InvalidParameter: 1 validation error(s) found.
+      ## │ - minimum field size of 1, ListTagsForResourceInput.ResourceARN.
+      ## │
+      ## │
+      ## │   with module.ecs_cloudwatch_autoscaling[0].aws_appautoscaling_target.default[0],
+      ## │   on .terraform/modules/ecs_cloudwatch_autoscaling/main.tf line 15, in resource "aws_appautoscaling_target" "default":
+      ## │   15: resource "aws_appautoscaling_target" "default" {
+      version = ">= 4.66.1"
     }
     template = {
       source  = "cloudposse/template"
