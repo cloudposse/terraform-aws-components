4168538e8bbb115dce392ea4f58da054034848eb
Zinovii Dmytriv <zdmytriv@gmail.com>
2023-05-31 15:40:33 +0300

    updaate

--- modules/guardduty/common/variables.tf
+++ modules/guardduty/common/variables.tf
@@ -3,12 +3,6 @@ variable "region" {
   description = "AWS Region"
 }
 
-variable "account_map_tenant" {
-  type        = string
-  default     = ""
-  description = "The tenant where the `account_map` component required by remote-state is deployed"
-}
-
 variable "root_account_stage" {
   type        = string
   default     = "root"
