4e995d2fe6ea4006e1959af6d81d70899b3b9924
Nuru <Nuru@users.noreply.github.com>
2023-06-02 22:46:19 -0700

    Additional documentation on adding custom IAM policies

--- modules/aws-team-roles/additional-policy-map.tf
+++ modules/aws-team-roles/additional-policy-map.tf
@@ -2,8 +2,9 @@ locals {
   # If you have custom policies, override this declaration by creating
   # a file called `additional-policy-map_override.tf`.
   # Then add the custom policies to the additional_custom_policy_map in that file.
+  # See the README for more details.
   overridable_additional_custom_policy_map = {
     # Example:
-    #   eks_viewer        = try(aws_iam_policy.eks_viewer[0].arn, null)
+    #   eks_viewer = aws_iam_policy.eks_viewer.arn
   }
 }
