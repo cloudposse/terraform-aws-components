5109eaede6abd5c26e016359da105249349dd5a4
Zinovii Dmytriv <zdmytriv@gmail.com>
2023-06-06 17:59:08 +0300

    update

--- modules/securityhub/outputs.tf
+++ modules/securityhub/outputs.tf
@@ -1,14 +1,14 @@
 output "enabled_subscriptions" {
   description = "A list of subscriptions that have been enabled"
-  value       = local.enabled && local.is_global_collector_account ? module.security_hub[0].enabled_subscriptions : []
+  value       = local.enabled && local.is_global_collector_account ? module.security_hub_primary[0].enabled_subscriptions : []
 }
 
 output "sns_topic_name" {
   description = "The SNS topic name that was created"
-  value       = local.create_sns_topic && local.is_global_collector_account ? module.security_hub[0].sns_topic.name : null
+  value       = local.create_sns_topic && local.is_global_collector_account ? module.security_hub_primary[0].sns_topic.name : null
 }
 
 output "sns_topic_subscriptions" {
   description = "The SNS topic subscriptions"
-  value       = local.create_sns_topic && local.is_global_collector_account ? module.security_hub[0].sns_topic_subscriptions : null
+  value       = local.create_sns_topic && local.is_global_collector_account ? module.security_hub_primary[0].sns_topic_subscriptions : null
 }
