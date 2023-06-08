c6ee933c955e131efbe087e77068606f6cd97405
cloudpossebot <cloudpossebot@users.noreply.github.com>
2023-06-08 22:01:47 +0000

    pre-commit fixes

--- modules/tfstate-backend/main.tf
+++ modules/tfstate-backend/main.tf
@@ -6,8 +6,8 @@ module "tfstate_backend" {
   source  = "cloudposse/tfstate-backend/aws"
   version = "1.1.1"
 
-  force_destroy               = var.force_destroy
-  prevent_unencrypted_uploads = var.prevent_unencrypted_uploads
+  force_destroy                     = var.force_destroy
+  prevent_unencrypted_uploads       = var.prevent_unencrypted_uploads
   enable_point_in_time_recovery     = var.enable_point_in_time_recovery
   bucket_ownership_enforced_enabled = false
 
