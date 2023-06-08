e3534099a88f5c232437beb3a42e93c690cb79ba
Max Lobur <max_lobur@outlook.com>
2023-06-08 18:50:21 +0300

    Update main.tf

--- modules/tfstate-backend/main.tf
+++ modules/tfstate-backend/main.tf
@@ -4,7 +4,7 @@ locals {
 
 module "tfstate_backend" {
   source  = "cloudposse/tfstate-backend/aws"
-  version = "0.38.1"
+  version = "1.1.1"
 
   force_destroy                 = var.force_destroy
   prevent_unencrypted_uploads   = var.prevent_unencrypted_uploads
