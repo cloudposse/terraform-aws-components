e636764f2fe4c29799c9de37a5bbedff0d286e32
aknysh <andriy.knysh@gmail.com>
2023-06-17 16:26:05 -0400

    Add EKS addons

--- modules/eks/cluster/additional-addon-irsa-map.tf
+++ modules/eks/cluster/additional-addon-irsa-map.tf
@@ -1,6 +1,6 @@
 locals {
   # If you have custom addons that require EKS IAM Roles for Kubernetes Service Accounts,
-  # override this declaration by creating a file called `additional-addon-service-account-role-map_override.tf`.
+  # override this declaration by creating a file called `additional-addon-irsa-map_override.tf`.
   # Then add the custom map of addon names to the service account role ARNs to the `overridable_additional_addon_service_account_role_arn_map` in that file.
   # See the README for more details.
   overridable_additional_addon_service_account_role_arn_map = {
