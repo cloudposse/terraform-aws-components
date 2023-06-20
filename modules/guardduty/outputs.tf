26cf3e19dc9918b67199b8174c89204bacaecc62
Matt Calhoun <matt@mattcalhoun.com>
2023-06-20 11:11:57 -0400

    cleanup outputs

--- modules/guardduty/outputs.tf
+++ modules/guardduty/outputs.tf
@@ -1,19 +1,19 @@
 output "guardduty_detector_arn" {
   value       = local.create_guardduty_collector ? try(module.guardduty[0].guardduty_detector.arn, null) : null
-  description = "Member GuardDuty detector ARN"
+  description = "The ARN of the GuardDuty detector created by the component"
 }
 
 output "guardduty_detector_id" {
   value       = local.create_guardduty_collector ? try(module.guardduty[0].guardduty_detector.id, null) : null
-  description = "Member GuardDuty detector ID"
+  description = "The ID of the GuardDuty detector created by the component"
 }
 
 output "sns_topic_name" {
-  description = "SNS topic name"
   value       = local.create_guardduty_collector ? try(module.guardduty[0].sns_topic.name, null) : null
+  description = "The name of the SNS topic created by the component"
 }
 
 output "sns_topic_subscriptions" {
-  description = "SNS topic subscriptions"
   value       = local.create_guardduty_collector ? try(module.guardduty[0].sns_topic_subscriptions, null) : null
+  description = "The SNS topic subscriptions created by the component"
 }
