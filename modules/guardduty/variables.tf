551c7f92e5f713e7e4b6af55e440bb3445a073cd
Matt Calhoun <matt@mattcalhoun.com>
2023-06-20 11:33:02 -0400

    cleanup vars

--- modules/guardduty/variables.tf
+++ modules/guardduty/variables.tf
@@ -8,28 +8,27 @@ variable "admin_delegated" {
   type        = bool
   default     = false
   description = <<DOC
-  A flag to indicate if the GuardDuty Admininstrator account has already been designated from the AWS Org management
-  account (usually 'root').
-
-  This component should be applied to the delegated_administrator_account with this variable set to false, then this
-  component should be applied to the organization_management_account in order to delegate to the
-  delegated_administrator_account, then this component should be applied again in the delegated_administrator_account
-  and all other accounts with this variable set to `true`.
+  A flag to indicate if the AWS Organization-wide settings should be created. This can only be done after the GuardDuty
+  Admininstrator account has already been delegated from the AWS Org Management account (usually 'root'). See the
+  Deployment section of the README for more information.
   DOC
 }
 
 variable "auto_enable_organization_members" {
+  type        = string
+  default     = "NEW"
   description = <<-DOC
   Indicates the auto-enablement configuration of GuardDuty for the member accounts in the organization. Valid values are `ALL`, `NEW`, `NONE`.
 
   For more information, see:
   https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/guardduty_organization_configuration#auto_enable_organization_members
   DOC
-  type        = string
-  default     = "NEW"
+
 }
 
 variable "cloudwatch_event_rule_pattern_detail_type" {
+  type        = string
+  default     = "GuardDuty Finding"
   description = <<-DOC
   The detail-type pattern used to match events that will be sent to SNS.
 
@@ -38,41 +37,40 @@ variable "cloudwatch_event_rule_pattern_detail_type" {
   https://docs.aws.amazon.com/eventbridge/latest/userguide/event-types.html
   https://docs.aws.amazon.com/guardduty/latest/ug/guardduty_findings_cloudwatch.html
   DOC
-  type        = string
-  default     = "GuardDuty Finding"
 }
 
 variable "create_sns_topic" {
+  type        = bool
+  default     = false
   description = <<-DOC
-  Flag to indicate whether an SNS topic should be created for notifications.
-  If you want to send findings to a new SNS topic, set this to true and provide a valid configuration for subscribers.
+  Flag to indicate whether an SNS topic should be created for notifications. If you want to send findings to a new SNS
+  topic, set this to true and provide a valid configuration for subscribers.
   DOC
-
-  type    = bool
-  default = false
 }
 
 variable "delegated_admininstrator_component_name" {
   type        = string
-  description = "The name of the component that created the guardduty detector."
   default     = "guardduty/delegated-administrator"
+  description = "The name of the component that created the guardduty detector."
 }
 
 variable "delegated_administrator_account_name" {
-  description = "The name of the account that is the AWS Organization delegated administrator account"
   type        = string
   default     = "core-security"
+  description = "The name of the account that is the AWS Organization delegated administrator account"
 }
 
 variable "enable_cloudwatch" {
+  type        = bool
+  default     = false
   description = <<-DOC
   Flag to indicate whether an CloudWatch logging should be enabled for GuardDuty
   DOC
-  type        = bool
-  default     = false
 }
 
 variable "finding_publishing_frequency" {
+  type        = string
+  default     = null
   description = <<-DOC
   The frequency of notifications sent for finding occurrences. If the detector is a GuardDuty member account, the value
   is determined by the GuardDuty master account and cannot be modified, otherwise it defaults to SIX_HOURS.
@@ -83,18 +81,16 @@ variable "finding_publishing_frequency" {
   For more information, see:
   https://docs.aws.amazon.com/guardduty/latest/ug/guardduty_findings_cloudwatch.html#guardduty_findings_cloudwatch_notification_frequency
   DOC
-  type        = string
-  default     = null
 }
 
 variable "findings_notification_arn" {
+  default     = null
+  type        = string
   description = <<-DOC
   The ARN for an SNS topic to send findings notifications to. This is only used if create_sns_topic is false.
   If you want to send findings to an existing SNS topic, set the value of this to the ARN of the existing topic and set
   create_sns_topic to false.
   DOC
-  default     = null
-  type        = string
 }
 
 variable "global_environment" {
@@ -104,37 +100,37 @@ variable "global_environment" {
 }
 
 variable "kubernetes_audit_logs_enabled" {
+  type        = bool
+  default     = false
   description = <<-DOC
   If `true`, enables Kubernetes audit logs as a data source for Kubernetes protection.
 
   For more information, see:
   https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/guardduty_detector#audit_logs
   DOC
-  type        = bool
-  default     = false
 }
 
 variable "malware_protection_scan_ec2_ebs_volumes_enabled" {
+  type        = bool
+  default     = false
   description = <<-DOC
   Configure whether Malware Protection is enabled as data source for EC2 instances EBS Volumes in GuardDuty.
 
   For more information, see:
   https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/guardduty_detector#malware-protection
   DOC
-  type        = bool
-  default     = false
 }
 
 variable "organization_management_account_name" {
-  description = "The name of the AWS Organization management account"
   type        = string
   default     = null
+  description = "The name of the AWS Organization management account"
 }
 
 variable "privileged" {
   type        = bool
-  description = "true if the default provider already has access to the backend"
   default     = false
+  description = "true if the default provider already has access to the backend"
 }
 
 variable "region" {
@@ -145,18 +141,21 @@ variable "region" {
 variable "root_account_stage" {
   type        = string
   default     = "root"
-  description = "The stage name for the Organization root (management) account"
+  description = <<-DOC
+  The stage name for the Organization root (management) account. This is used to lookup account IDs from account names
+  using the `account-map` component.
+  DOC
 }
 
 variable "s3_protection_enabled" {
+  type        = bool
+  default     = true
   description = <<-DOC
   If `true`, enables S3 protection.
 
   For more information, see:
   https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/guardduty_detector#s3-logs
   DOC
-  type        = bool
-  default     = true
 }
 
 variable "subscribers" {
@@ -166,6 +165,7 @@ variable "subscribers" {
     endpoint_auto_confirms = bool
     raw_message_delivery   = bool
   }))
+  default     = {}
   description = <<-DOC
   A map of subscription configurations for SNS topics
 
@@ -179,10 +179,9 @@ variable "subscribers" {
     The endpoint to send data to, the contents will vary with the protocol. (see link for more information)
   endpoint_auto_confirms:
     Boolean indicating whether the end point is capable of auto confirming subscription e.g., PagerDuty. Default is
-    false
+    false.
   raw_message_delivery:
-    Boolean indicating whether or not to enable raw message delivery (the original message is directly passed, not wrapped in JSON with the original message in the message property).
-    Default is false
+    Boolean indicating whether or not to enable raw message delivery (the original message is directly passed, not
+    wrapped in JSON with the original message in the message property). Default is false.
   DOC
-  default     = {}
 }
