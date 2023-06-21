57ffd850222c00249cc73a29ccda1eed142efdef
Matt Calhoun <matt@cloudposse.com>
2023-06-21 10:54:29 -0400

    roll guard duty back to previous providers logic (#727)
    Co-authored-by: cloudpossebot <cloudpossebot@users.noreply.github.com>

--- modules/guardduty/providers.tf
+++ modules/guardduty/providers.tf
@@ -1,30 +1,23 @@
 provider "aws" {
   region = var.region
 
-  # Profile is deprecated in favor of terraform_role_arn. When profiles are not in use, terraform_profile_name is null.
-  profile = module.iam_roles.terraform_profile_name
-
+  profile = !var.privileged && module.iam_roles.profiles_enabled ? coalesce(var.import_profile_name, module.iam_roles.terraform_profile_name) : null
   dynamic "assume_role" {
-    # module.iam_roles.terraform_role_arn may be null, in which case do not assume a role.
-    for_each = compact([module.iam_roles.terraform_role_arn])
+    for_each = var.privileged || module.iam_roles.profiles_enabled ? [] : ["role"]
     content {
-      role_arn = module.iam_roles.terraform_role_arn
+      role_arn = coalesce(var.import_role_arn, module.iam_roles.terraform_role_arn)
     }
   }
 }
 
-
 provider "awsutils" {
   region = var.region
 
-  # Profile is deprecated in favor of terraform_role_arn. When profiles are not in use, terraform_profile_name is null.
-  profile = module.iam_roles.terraform_profile_name
-
+  profile = !var.privileged && module.iam_roles.profiles_enabled ? coalesce(var.import_profile_name, module.iam_roles.terraform_profile_name) : null
   dynamic "assume_role" {
-    # module.iam_roles.terraform_role_arn may be null, in which case do not assume a role.
-    for_each = compact([module.iam_roles.terraform_role_arn])
+    for_each = var.privileged || module.iam_roles.profiles_enabled ? [] : ["role"]
     content {
-      role_arn = module.iam_roles.terraform_role_arn
+      role_arn = coalesce(var.import_role_arn, module.iam_roles.terraform_role_arn)
     }
   }
 }
@@ -34,4 +27,16 @@ module "iam_roles" {
   privileged = var.privileged
 
   context = module.this.context
+}
+
+variable "import_profile_name" {
+  type        = string
+  default     = null
+  description = "AWS Profile name to use when importing a resource"
+}
+
+variable "import_role_arn" {
+  type        = string
+  default     = null
+  description = "IAM Role ARN to use when importing a resource"
 }
