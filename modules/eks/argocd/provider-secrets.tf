68b909af1a5741142b05bf6c90a2a8963526bd09
aknysh <andriy.knysh@gmail.com>
2023-06-25 19:05:11 -0400

    fixes

--- modules/eks/argocd/provider-secrets.tf
+++ modules/eks/argocd/provider-secrets.tf
@@ -9,7 +9,7 @@ provider "aws" {
     # module.iam_roles.terraform_role_arn may be null, in which case do not assume a role.
     for_each = compact([module.iam_roles_config_secrets.terraform_role_arn])
     content {
-      role_arn = module.iam_roles.terraform_role_arn
+      role_arn = assume_role.value
     }
   }
 }
