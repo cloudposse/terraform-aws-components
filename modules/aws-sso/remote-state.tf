b3814459c6a80292d7caaef57238c7c7202acd53
Nuru <Nuru@users.noreply.github.com>
2023-06-28 11:43:09 -0700

    Treat SSO Permission Sets like aws-teams

--- modules/aws-sso/remote-state.tf
+++ modules/aws-sso/remote-state.tf
@@ -1,23 +1,19 @@
 module "account_map" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
-  version = "1.4.1"
+  version = "1.4.3"
 
   component   = "account-map"
-  environment = var.global_environment_name
-  stage       = var.global_stage_name
+  environment = module.iam_roles.global_environment_name
+  stage       = module.iam_roles.global_stage_name
+  tenant      = module.iam_roles.global_tenant_name
   privileged  = var.privileged
 
   context = module.this.context
 }
 
-module "tfstate" {
-  source  = "cloudposse/stack-config/yaml//modules/remote-state"
-  version = "1.4.1"
+# Module "iam_roles" is usually in providers.tf, but not so for this component
 
-  component   = "tfstate-backend"
-  environment = var.tfstate_environment_name
-  stage       = var.global_stage_name
-  privileged  = var.privileged
-
+module "iam_roles" {
+  source  = "../account-map/modules/iam-roles"
   context = module.this.context
 }
