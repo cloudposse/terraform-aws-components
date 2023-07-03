cloudposse/terraform-aws-components

commit ae244d341bbe92861afe1aa19d4400bdca16b2b0

[github-oidc-provider] extra-compatible provider (#742)

modules/github-oidc-provider/providers.tf
@@ -1,19 +1,56 @@
+# This is a special provider configuration that allows us to use many different
+# versions of the Cloud Posse reference architecture to deploy this component
+# in any account, including the identity and root accounts.
+
+# If you have dynamic Terraform roles enabled and an `aws-team` (such as `managers`)
+# empowered to make changes in the identity and root accounts. Then you can
+# use those roles to deploy this component in the identity and root accounts,
+# just like almost any other component.
+#
+# If you are restricted to using the SuperAdmin role to deploy this component
+# in the identity and root accounts, then modify the stack configuration for
+# this component for the identity and/or root accounts to set `superadmin: true`
+# and backend `role_arn` to `null`.
+#
+#  components:
+#    terraform:
+#      github-oidc-provider:
+#        backend:
+#          s3:
+#            role_arn: null
+#        vars:
+#          superadmin: true
+
 provider "aws" {
   region = var.region
 
-  # Profile is deprecated in favor of terraform_role_arn. When profiles are not in use, terraform_profile_name is null.
-  profile = module.iam_roles.terraform_profile_name
-
+  profile = !var.superadmin && module.iam_roles.profiles_enabled ? module.iam_roles.terraform_profile_name : null
   dynamic "assume_role" {
-    # module.iam_roles.terraform_role_arn may be null, in which case do not assume a role.
-    for_each = compact([module.iam_roles.terraform_role_arn])
+    for_each = [
+      !var.superadmin && module.iam_roles.profiles_enabled ? null : (
+        var.superadmin ? {
+          role_arn = module.iam_roles.org_role_arn
+          } : {
+          role_arn = module.iam_roles.terraform_role_arn
+        }
+      )
+    ]
     content {
-      role_arn = assume_role.value
+      role_arn = module.iam_roles.terraform_role_arn
     }
   }
 }
 
+
 module "iam_roles" {
-  source  = "../account-map/modules/iam-roles"
+  source     = "../account-map/modules/iam-roles"
+  privileged = var.superadmin
+
   context = module.this.context
+}
+
+variable "superadmin" {
+  type        = bool
+  default     = false
+  description = "Set `true` if running as the SuperAdmin user"
 }

commit 971bb4a9087229d1ca614f59e9c65cc4f5ac87b8

Fix bug introduced in #742

modules/github-oidc-provider/providers.tf
@@ -26,17 +26,11 @@ provider "aws" {
 
   profile = !var.superadmin && module.iam_roles.profiles_enabled ? module.iam_roles.terraform_profile_name : null
   dynamic "assume_role" {
-    for_each = [
-      !var.superadmin && module.iam_roles.profiles_enabled ? null : (
-        var.superadmin ? {
-          role_arn = module.iam_roles.org_role_arn
-          } : {
-          role_arn = module.iam_roles.terraform_role_arn
-        }
-      )
-    ]
+    for_each = !var.superadmin && module.iam_roles.profiles_enabled ? [] : (
+      var.superadmin ? compact([module.iam_roles.org_role_arn]) : compact([module.iam_roles.terraform_role_arn])
+    )
     content {
-      role_arn = module.iam_roles.terraform_role_arn
+      role_arn = assume_role.value
     }
   }
 }