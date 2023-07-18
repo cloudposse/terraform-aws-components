884ab908f3afa0e60bfea9dbacb842e2d8c727a5
Jeremy White <jeremy.white@cloudposse.com>
2023-07-18 12:13:56 -0400

    fix: argocd flags, versions, and expressions (#753)
    Co-authored-by: aknysh <andriy.knysh@gmail.com>

--- modules/eks/argocd/outputs.tf
+++ modules/eks/argocd/outputs.tf
@@ -1,4 +1,9 @@
 output "metadata" {
   value       = module.argocd.metadata
-  description = "Block status of the deployed release"
+  description = "Block status of the deployed ArgoCD release"
+}
+
+output "argocd_apps_metadata" {
+  value       = module.argocd_apps.metadata
+  description = "Block status of the deployed ArgoCD apps release"
 }
--- modules/eks/platform/variables.tf
+++ modules/eks/platform/variables.tf
@@ -19,7 +19,7 @@ variable "references" {
 variable "eks_component_name" {
   type        = string
   description = "The name of the eks component"
-  default     = "eks/eks"
+  default     = "eks/cluster"
 }
 
 variable "ssm_platform_path" {
