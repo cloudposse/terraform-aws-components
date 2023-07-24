784d551e1802488a31e6b5b51b5e97be34cfba07
Michael Rosenfeld <michaeljrosenfeld22@gmail.com>
2023-07-24 15:27:40 -0500

    Update Variable Types for Cloudtrail Buckets

--- modules/cloudtrail-bucket/variables.tf
+++ modules/cloudtrail-bucket/variables.tf
@@ -10,26 +10,31 @@ variable "lifecycle_rule_enabled" {
 }
 
 variable "noncurrent_version_expiration_days" {
+  type        = number
   description = "Specifies when noncurrent object versions expire"
   default     = 90
 }
 
 variable "noncurrent_version_transition_days" {
+  type        = number
   description = "Specifies when noncurrent object versions transitions"
   default     = 30
 }
 
 variable "standard_transition_days" {
+  type        = number
   description = "Number of days to persist in the standard storage tier before moving to the infrequent access tier"
   default     = 30
 }
 
 variable "glacier_transition_days" {
+  type        = number
   description = "Number of days after which to move the data to the glacier storage tier"
   default     = 60
 }
 
 variable "expiration_days" {
+  type        = number
   description = "Number of days after which to expunge the objects"
   default     = 90
 }
