46aff660cabb7267dbfa73f9c320a402670159b5
cloudpossebot <cloudpossebot@users.noreply.github.com>
2023-07-28 09:59:59 +0000

    pre-commit fixes

--- modules/inspector2/outputs.tf
+++ modules/inspector2/outputs.tf
@@ -1,4 +1,4 @@
 output "aws_inspector2_member_association" {
-  value = aws_inspector2_member_association.default
+  value       = aws_inspector2_member_association.default
   description = "The Inspector2 member association resource."
 }
