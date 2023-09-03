c9e39a5f81542611e1dd7ae37a56829fef7128ab
aknysh <andriy.knysh@gmail.com>
2023-09-03 18:54:55 -0400

    Update `waf` component

--- modules/waf/variables.tf
+++ modules/waf/variables.tf
@@ -98,105 +98,6 @@ variable "token_domains" {
   default     = null
 }
 
-# Logging configuration
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl_logging_configuration.html
-variable "log_destination_configs" {
-  type        = list(string)
-  default     = []
-  description = "The Amazon Kinesis Data Firehose, CloudWatch Log log group, or S3 bucket Amazon Resource Names (ARNs) that you want to associate with the web ACL"
-}
-
-variable "redacted_fields" {
-  type = map(object({
-    method        = optional(bool, false)
-    uri_path      = optional(bool, false)
-    query_string  = optional(bool, false)
-    single_header = optional(list(string), null)
-  }))
-  default     = {}
-  description = <<-DOC
-    The parts of the request that you want to keep out of the logs.
-    You can only specify one of the following: `method`, `query_string`, `single_header`, or `uri_path`
-
-    method:
-      Whether to enable redaction of the HTTP method.
-      The method indicates the type of operation that the request is asking the origin to perform.
-    uri_path:
-      Whether to enable redaction of the URI path.
-      This is the part of a web request that identifies a resource.
-    query_string:
-      Whether to enable redaction of the query string.
-      This is the part of a URL that appears after a `?` character, if any.
-    single_header:
-      The list of names of the query headers to redact.
-  DOC
-  nullable    = false
-}
-
-variable "logging_filter" {
-  type = object({
-    default_behavior = string
-    filter = list(object({
-      behavior    = string
-      requirement = string
-      condition = list(object({
-        action_condition = optional(object({
-          action = string
-        }), null)
-        label_name_condition = optional(object({
-          label_name = string
-        }), null)
-      }))
-    }))
-  })
-  default     = null
-  description = <<-DOC
-    A configuration block that specifies which web requests are kept in the logs and which are dropped.
-    You can filter on the rule action and on the web request labels that were applied by matching rules during web ACL evaluation.
-  DOC
-}
-
-# Association resources
-variable "association_resource_arns" {
-  type        = list(string)
-  default     = []
-  description = <<-DOC
-    A list of ARNs of the resources to associate with the web ACL.
-    This must be an ARN of an Application Load Balancer, Amazon API Gateway stage, or AWS AppSync.
-
-    Do not use this variable to associate a Cloudfront Distribution.
-    Instead, you should use the `web_acl_id` property on the `cloudfront_distribution` resource.
-    For more details, refer to https://docs.aws.amazon.com/waf/latest/APIReference/API_AssociateWebACL.html
-  DOC
-  nullable    = false
-}
-
-variable "association_resource_component_selectors" {
-  type = list(object({
-    component            = string
-    namespace            = optional(string, null)
-    tenant               = optional(string, null)
-    environment          = optional(string, null)
-    stage                = optional(string, null)
-    component_arn_output = string
-  }))
-  default     = []
-  description = <<-DOC
-    A list of Atmos component selectors to get from the remote state and associate their ARNs with the web ACL.
-    The components must be Application Load Balancers, Amazon API Gateway stages, or AWS AppSync.
-
-    component:
-      Atmos component name
-    component_arn_output:
-      The component output that defines the component ARN
-
-    Do not use this variable to select a Cloudfront Distribution component.
-    Instead, you should use the `web_acl_id` property on the `cloudfront_distribution` resource.
-    For more details, refer to https://docs.aws.amazon.com/waf/latest/APIReference/API_AssociateWebACL.html
-  DOC
-  nullable    = false
-}
-
 # Rules
 variable "byte_match_statement_rules" {
   type        = list(any)
@@ -371,6 +272,16 @@ variable "ip_set_reference_statement_rules" {
     statement:
       arn:
         The ARN of the IP Set that this statement references.
+      ip_set:
+        Defines a new IP Set
+
+        description:
+          A friendly description of the IP Set
+        addresses:
+          Contains an array of strings that specifies zero or more IP addresses or blocks of IP addresses.
+          All addresses must be specified using Classless Inter-Domain Routing (CIDR) notation.
+        ip_address_version:
+          Specify `IPV4` or `IPV6`
       ip_set_forwarded_ip_config:
         fallback_behavior:
           The match status to assign to the web request if the request doesn't have a valid IP address in the specified position.
@@ -433,6 +344,9 @@ variable "managed_rule_group_statement_rules" {
       rule_action_override:
         Action settings to use in the place of the rule actions that are configured inside the rule group.
         You specify one override for each rule whose action you want to change.
+      managed_rule_group_configs:
+        Additional information that's used by a managed rule group. Only one rule attribute is allowed in each config.
+        Refer to https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-list.html for more details.
 
     visibility_config:
       Defines and enables Amazon CloudWatch metrics and web request sample collection.
@@ -791,4 +705,136 @@ variable "xss_match_statement_rules" {
       sampled_requests_enabled:
         Whether AWS WAF should store a sampling of the web requests that match the rules.
   DOC
+}
+
+# Logging configuration
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl_logging_configuration.html
+variable "log_destination_configs" {
+  type        = list(string)
+  default     = []
+  description = <<-DOC
+    A list of resource names/ARNs to associate Amazon Kinesis Data Firehose, Cloudwatch Log log group, or S3 bucket with the WAF logs.
+    Note: data firehose, log group, or bucket name must be prefixed with `aws-waf-logs-`,
+    e.g. `aws-waf-logs-example-firehose`, `aws-waf-logs-example-log-group`, or `aws-waf-logs-example-bucket`.
+  DOC
+}
+
+variable "redacted_fields" {
+  type = map(object({
+    method        = optional(bool, false)
+    uri_path      = optional(bool, false)
+    query_string  = optional(bool, false)
+    single_header = optional(list(string), null)
+  }))
+  default     = {}
+  description = <<-DOC
+    The parts of the request that you want to keep out of the logs.
+    You can only specify one of the following: `method`, `query_string`, `single_header`, or `uri_path`
+
+    method:
+      Whether to enable redaction of the HTTP method.
+      The method indicates the type of operation that the request is asking the origin to perform.
+    uri_path:
+      Whether to enable redaction of the URI path.
+      This is the part of a web request that identifies a resource.
+    query_string:
+      Whether to enable redaction of the query string.
+      This is the part of a URL that appears after a `?` character, if any.
+    single_header:
+      The list of names of the query headers to redact.
+  DOC
+  nullable    = false
+}
+
+variable "logging_filter" {
+  type = object({
+    default_behavior = string
+    filter = list(object({
+      behavior    = string
+      requirement = string
+      condition = list(object({
+        action_condition = optional(object({
+          action = string
+        }), null)
+        label_name_condition = optional(object({
+          label_name = string
+        }), null)
+      }))
+    }))
+  })
+  default     = null
+  description = <<-DOC
+    A configuration block that specifies which web requests are kept in the logs and which are dropped.
+    You can filter on the rule action and on the web request labels that were applied by matching rules during web ACL evaluation.
+  DOC
+}
+
+variable "log_destination_component_selectors" {
+  type = list(object({
+    component        = string
+    namespace        = optional(string, null)
+    tenant           = optional(string, null)
+    environment      = optional(string, null)
+    stage            = optional(string, null)
+    component_output = string
+  }))
+  default     = []
+  description = <<-DOC
+    A list of Atmos component selectors to get from the remote state and associate their names/ARNs with the WAF logs.
+    The components must be Amazon Kinesis Data Firehose, CloudWatch Log Group, or S3 bucket.
+
+    component:
+      Atmos component name
+    component_output:
+      The component output that defines the component name or ARN
+
+    Set `tenant`, `environment` and `stage` if the components are in different OUs, regions or accounts.
+
+    Note: data firehose, log group, or bucket name must be prefixed with `aws-waf-logs-`,
+    e.g. `aws-waf-logs-example-firehose`, `aws-waf-logs-example-log-group`, or `aws-waf-logs-example-bucket`.
+ DOC
+  nullable    = false
+}
+
+# Association resources
+variable "association_resource_arns" {
+  type        = list(string)
+  default     = []
+  description = <<-DOC
+    A list of ARNs of the resources to associate with the web ACL.
+    This must be an ARN of an Application Load Balancer, Amazon API Gateway stage, or AWS AppSync.
+
+    Do not use this variable to associate a Cloudfront Distribution.
+    Instead, you should use the `web_acl_id` property on the `cloudfront_distribution` resource.
+    For more details, refer to https://docs.aws.amazon.com/waf/latest/APIReference/API_AssociateWebACL.html
+  DOC
+  nullable    = false
+}
+
+variable "association_resource_component_selectors" {
+  type = list(object({
+    component            = string
+    namespace            = optional(string, null)
+    tenant               = optional(string, null)
+    environment          = optional(string, null)
+    stage                = optional(string, null)
+    component_arn_output = string
+  }))
+  default     = []
+  description = <<-DOC
+    A list of Atmos component selectors to get from the remote state and associate their ARNs with the web ACL.
+    The components must be Application Load Balancers, Amazon API Gateway stages, or AWS AppSync.
+
+    component:
+      Atmos component name
+    component_arn_output:
+      The component output that defines the component ARN
+
+    Set `tenant`, `environment` and `stage` if the components are in different OUs, regions or accounts.
+
+    Do not use this variable to select a Cloudfront Distribution component.
+    Instead, you should use the `web_acl_id` property on the `cloudfront_distribution` resource.
+    For more details, refer to https://docs.aws.amazon.com/waf/latest/APIReference/API_AssociateWebACL.html
+  DOC
+  nullable    = false
 }
