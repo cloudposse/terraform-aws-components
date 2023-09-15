5c030c0870db2d1b46d56251c68b8445f8f92074
Erik Osterman <erik@cloudposse.com>
2023-09-15 07:28:40 -0500

    Pinning

--- modules/s3-bucket/versions.tf
+++ modules/s3-bucket/versions.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.0"
     }
+    null = {
+      source = "hashicorp/null"
+      version = ">= 3.2.1"
+    }
     template = {
       source  = "cloudposse/template"
       version = ">= 2.2.0"
