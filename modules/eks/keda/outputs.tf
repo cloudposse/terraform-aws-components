5c63af5213e5bb9273525be99ea757836a11e161
Max Lobur <max_lobur@outlook.com>
2023-09-25 19:33:14 +0300

    Update keda component (#863)
    Co-authored-by: cloudpossebot <cloudpossebot@users.noreply.github.com>

--- modules/eks/keda/outputs.tf
+++ modules/eks/keda/outputs.tf
@@ -1,4 +1,48 @@
+## eks_iam_role
+
+output "service_account_namespace" {
+  value       = module.keda.service_account_namespace
+  description = "Kubernetes Service Account namespace"
+}
+
+output "service_account_name" {
+  value       = module.keda.service_account_name
+  description = "Kubernetes Service Account name"
+}
+
+output "service_account_role_name" {
+  value       = module.keda.service_account_role_name
+  description = "IAM role name"
+}
+
+output "service_account_role_unique_id" {
+  value       = module.keda.service_account_role_unique_id
+  description = "IAM role unique ID"
+}
+
+output "service_account_role_arn" {
+  value       = module.keda.service_account_role_arn
+  description = "IAM role ARN"
+}
+
+output "service_account_policy_name" {
+  value       = module.keda.service_account_policy_name
+  description = "IAM policy name"
+}
+
+output "service_account_policy_id" {
+  value       = module.keda.service_account_policy_id
+  description = "IAM policy ID"
+}
+
+output "service_account_policy_arn" {
+  value       = module.keda.service_account_policy_arn
+  description = "IAM policy ARN"
+}
+
+## keda
+
 output "metadata" {
-  value       = try(one(module.keda.metadata), null)
-  description = "Block status of the deployed release"
+  description = "Block status of the deployed release."
+  value       = module.keda.metadata
 }
