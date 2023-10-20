e97d0a330bc362001f81a1a0a3b12d9867e86387
milldr <miller0daniel@gmail.com>
2023-10-20 15:45:21 -0700

    adding override for additional policies

--- modules/eks/external-secrets-operator/remote-state.tf
+++ modules/eks/external-secrets-operator/remote-state.tf
@@ -8,11 +8,13 @@ module "eks" {
 }
 
 module "account_map" {
-  source      = "cloudposse/stack-config/yaml//modules/remote-state"
-  version     = "1.5.0"
+  source  = "cloudposse/stack-config/yaml//modules/remote-state"
+  version = "1.5.0"
+
   component   = "account-map"
   tenant      = module.iam_roles.global_tenant_name
   environment = module.iam_roles.global_environment_name
   stage       = module.iam_roles.global_stage_name
-  context     = module.this.context
+
+  context = module.this.context
 }
