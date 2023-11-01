92f993d5425ce291fc65b7afcd5fd02234999ae7
Jeremy White <jeremy.white@cloudposse.com>
2023-10-31 20:15:33 -0400

    fix(aws-saml/okta-user): fully handle enabled flag

--- modules/aws-saml/modules/okta-user/main.tf
+++ modules/aws-saml/modules/okta-user/main.tf
@@ -30,13 +30,15 @@ resource "aws_iam_policy" "default" {
 }
 
 resource "aws_iam_user_policy_attachment" "default" {
-  user       = aws_iam_user.default.name
+  count      = local.enabled ? 1 : 0
+  user       = one(aws_iam_user.default[*].name)
   policy_arn = aws_iam_policy.default.arn
 }
 
 # Generate API credentials
 resource "aws_iam_access_key" "default" {
-  user = aws_iam_user.default.name
+  count = local.enabled ? 1 : 0
+  user  = one(aws_iam_user.default[*].name)
 }
 
 resource "aws_ssm_parameter" "okta_user_access_key_id" {
