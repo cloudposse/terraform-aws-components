a392b5c9a280da0e88240e40041a1181254520bb
milldr <miller0daniel@gmail.com>
2023-11-08 16:23:07 -0800

    upstream changes to satisfy tflint, remove used options, add define vars that arent used

--- modules/spacelift/admin-stack/root-admin-stack.tf
+++ modules/spacelift/admin-stack/root-admin-stack.tf
@@ -3,7 +3,7 @@
 # such stack is allowed in the Spacelift organization.
 module "root_admin_stack_config" {
   source  = "cloudposse/cloud-infrastructure-automation/spacelift//modules/spacelift-stacks-from-atmos-config"
-  version = "1.0.0"
+  version = "1.4.0"
 
   enabled = local.create_root_admin_stack
 
@@ -15,7 +15,7 @@ module "root_admin_stack_config" {
 # This gets the atmos stack config for all of the administrative stacks
 module "all_admin_stacks_config" {
   source  = "cloudposse/cloud-infrastructure-automation/spacelift//modules/spacelift-stacks-from-atmos-config"
-  version = "1.0.0"
+  version = "1.4.0"
 
   enabled = local.create_root_admin_stack
   context_filters = {
@@ -25,7 +25,7 @@ module "all_admin_stacks_config" {
 
 module "root_admin_stack" {
   source  = "cloudposse/cloud-infrastructure-automation/spacelift//modules/spacelift-stack"
-  version = "1.0.0"
+  version = "1.4.0"
 
   enabled    = local.create_root_admin_stack
   depends_on = [null_resource.spaces_precondition, null_resource.workers_precondition]
@@ -37,8 +37,8 @@ module "root_admin_stack" {
   after_perform                           = try(local.root_admin_stack_config.settings.spacelift.after_perform, [])
   after_plan                              = try(local.root_admin_stack_config.settings.spacelift.after_plan, [])
   atmos_stack_name                        = try(local.root_admin_stack_config.stack, null)
-  autodeploy                              = try(local.root_admin_stack_config.settings.spacelift.autodeploy, false)
-  autoretry                               = try(local.root_admin_stack_config.settings.spacelift.autoretry, false)
+  autodeploy                              = try(local.root_admin_stack_config.settings.spacelift.autodeploy, var.autodeploy)
+  autoretry                               = try(local.root_admin_stack_config.settings.spacelift.autoretry, var.autoretry)
   aws_role_enabled                        = try(local.root_admin_stack_config.settings.aws_role_enabled, var.aws_role_enabled)
   aws_role_arn                            = try(local.root_admin_stack_config.settings.aws_role_arn, var.aws_role_arn)
   aws_role_external_id                    = try(local.root_admin_stack_config.settings.aws_role_external_id, var.aws_role_external_id)
@@ -50,11 +50,11 @@ module "root_admin_stack" {
   before_plan                             = try(local.root_admin_stack_config.settings.spacelift.before_plan, [])
   branch                                  = try(local.root_admin_stack_config.branch, var.branch)
   commit_sha                              = var.commit_sha != null ? var.commit_sha : try(local.root_admin_stack_config.commit_sha, null)
-  component_env                           = try(local.root_admin_stack_config.env, {})
+  component_env                           = try(local.root_admin_stack_config.env, var.component_env)
   component_name                          = try(local.root_admin_stack_config.component, null)
   component_root                          = try(join("/", [var.component_root, local.root_admin_stack_config.metadata.component]), null)
-  component_vars                          = try(local.root_admin_stack_config.vars, null)
-  context_attachments                     = try(local.root_admin_stack_config.context_attachments, [])
+  component_vars                          = try(local.root_admin_stack_config.vars, var.component_vars)
+  context_attachments                     = try(local.root_admin_stack_config.context_attachments, var.context_attachments)
   description                             = try(local.root_admin_stack_config.description, var.description)
   drift_detection_enabled                 = try(local.root_admin_stack_config.settings.spacelift.drift_detection_enabled, var.drift_detection_enabled)
   drift_detection_reconcile               = try(local.root_admin_stack_config.settings.spacelift.drift_detection_reconcile, var.drift_detection_reconcile)
@@ -63,29 +63,30 @@ module "root_admin_stack" {
   labels                                  = concat(try(local.root_admin_stack_config.labels, []), try(var.labels, []))
   local_preview_enabled                   = try(local.root_admin_stack_config.local_preview_enabled, var.local_preview_enabled)
   manage_state                            = try(local.root_admin_stack_config.manage_state, var.manage_state)
-  protect_from_deletion                   = try(local.root_admin_stack_config.settings.spacelift.protect_from_deletion, false)
+  protect_from_deletion                   = try(local.root_admin_stack_config.settings.spacelift.protect_from_deletion, var.protect_from_deletion)
   repository                              = var.repository
   runner_image                            = try(local.root_admin_stack_config.settings.spacelift.runner_image, var.runner_image)
-  space_id                                = "root"
+  space_id                                = var.space_id
   spacelift_run_enabled                   = coalesce(try(local.root_admin_stack_config.settings.spacelift.spacelift_run_enabled, null), var.spacelift_run_enabled)
+  spacelift_stack_dependency_enabled      = try(local.root_admin_stack_config.settings.spacelift.spacelift_stack_dependency_enabled, var.spacelift_stack_dependency_enabled)
   stack_destructor_enabled                = try(local.root_admin_stack_config.settings.spacelift.stack_destructor_enabled, var.stack_destructor_enabled)
   stack_name                              = var.stack_name != null ? var.stack_name : local.root_admin_stack_name
-  terraform_smart_sanitization            = try(local.root_admin_stack_config.terraform_smart_sanitization, false)
+  terraform_smart_sanitization            = try(local.root_admin_stack_config.terraform_smart_sanitization, var.terraform_smart_sanitization)
   terraform_version                       = lookup(var.terraform_version_map, try(local.root_admin_stack_config.terraform_version, ""), var.terraform_version)
-  terraform_workspace                     = try(local.root_admin_stack_config.workspace, null)
+  terraform_workspace                     = try(local.root_admin_stack_config.workspace, var.terraform_workspace)
   webhook_enabled                         = try(local.root_admin_stack_config.webhook_enabled, var.webhook_enabled)
   webhook_endpoint                        = try(local.root_admin_stack_config.webhook_endpoint, var.webhook_endpoint)
   webhook_secret                          = try(local.root_admin_stack_config.webhook_secret, var.webhook_secret)
   worker_pool_id                          = local.worker_pools[var.worker_pool_name]
 
-  azure_devops         = try(local.root_admin_stack_config.settings.spacelift.azure_devops, null)
-  bitbucket_cloud      = try(local.root_admin_stack_config.settings.spacelift.bitbucket_cloud, null)
-  bitbucket_datacenter = try(local.root_admin_stack_config.settings.spacelift.bitbucket_datacenter, null)
-  cloudformation       = try(local.root_admin_stack_config.settings.spacelift.cloudformation, null)
-  github_enterprise    = try(local.root_admin_stack_config.settings.spacelift.github_enterprise, null)
-  gitlab               = try(local.root_admin_stack_config.settings.spacelift.gitlab, null)
-  pulumi               = try(local.root_admin_stack_config.settings.spacelift.pulumi, null)
-  showcase             = try(local.root_admin_stack_config.settings.spacelift.showcase, null)
+  azure_devops         = try(local.root_admin_stack_config.settings.spacelift.azure_devops, var.azure_devops)
+  bitbucket_cloud      = try(local.root_admin_stack_config.settings.spacelift.bitbucket_cloud, var.bitbucket_cloud)
+  bitbucket_datacenter = try(local.root_admin_stack_config.settings.spacelift.bitbucket_datacenter, var.bitbucket_datacenter)
+  cloudformation       = try(local.root_admin_stack_config.settings.spacelift.cloudformation, var.cloudformation)
+  github_enterprise    = try(local.root_admin_stack_config.settings.spacelift.github_enterprise, var.github_enterprise)
+  gitlab               = try(local.root_admin_stack_config.settings.spacelift.gitlab, var.gitlab)
+  pulumi               = try(local.root_admin_stack_config.settings.spacelift.pulumi, var.pulumi)
+  showcase             = try(local.root_admin_stack_config.settings.spacelift.showcase, var.showcase)
 }
 
 resource "spacelift_policy_attachment" "root" {
