f2d38418cb0ad3005e5f09c9dfaa430aacfec6e5
milldr <miller0daniel@gmail.com>
2023-11-09 15:26:39 -0800

    triggers for slack notifications

--- modules/argocd-repo/applicationset.tf
+++ modules/argocd-repo/applicationset.tf
@@ -5,13 +5,14 @@ resource "github_repository_file" "application_set" {
   branch     = local.github_repository.default_branch
   file       = "${each.value.tenant != null ? format("%s/", each.value.tenant) : ""}${each.value.environment}-${each.value.stage}${length(each.value.attributes) > 0 ? format("-%s", join("-", each.value.attributes)) : ""}/${local.manifest_kubernetes_namespace}/applicationset.yaml"
   content = templatefile("${path.module}/templates/applicationset.yaml.tpl", {
-    environment        = each.key
-    auto-sync          = each.value.auto-sync
-    ignore-differences = each.value.ignore-differences
-    name               = module.this.namespace
-    namespace          = local.manifest_kubernetes_namespace
-    ssh_url            = local.github_repository.ssh_clone_url
-    notifications      = var.github_default_notifications_enabled
+    environment                 = each.key
+    auto-sync                   = each.value.auto-sync
+    ignore-differences          = each.value.ignore-differences
+    name                        = module.this.namespace
+    namespace                   = local.manifest_kubernetes_namespace
+    ssh_url                     = local.github_repository.ssh_clone_url
+    notifications               = var.github_default_notifications_enabled
+    slack_notifications_channel = var.slack_notifications_channel
   })
   commit_message      = "Initialize environment: `${each.key}`."
   commit_author       = var.github_user
