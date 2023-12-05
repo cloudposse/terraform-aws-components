d2357a03647ee8476aa672f2c9f9d1fc4be2dd37
milldr <miller0daniel@gmail.com>
2023-12-05 09:05:24 -0800

    removed volume mount

--- modules/bastion/templates/user-data.sh
+++ modules/bastion/templates/user-data.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Mount additional volume
-echo "Mounting additional volume..."
-while [ ! -b $(readlink -f /dev/sdh) ]; do echo 'waiting for device /dev/sdh'; sleep 5 ; done
-blkid $(readlink -f /dev/sdh) || mkfs -t ext4 $(readlink -f /dev/sdh)
-e2label $(readlink -f /dev/sdh) sdh-volume
-grep -q ^LABEL=sdh-volume /etc/fstab || echo 'LABEL=sdh-volume /mnt ext4 defaults' >> /etc/fstab
-grep -q \"^$(readlink -f /dev/sdh) /mnt \" /proc/mounts || mount /mnt
-
 # Install docker
 echo "Installing docker..."
 amazon-linux-extras install docker
