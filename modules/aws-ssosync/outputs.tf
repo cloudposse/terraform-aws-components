d2006b9b9132850680ab1b4ca7661e5554b928ce
milldr <miller0daniel@gmail.com>
2023-12-26 15:33:31 -0800

    fix outputs

--- modules/aws-ssosync/outputs.tf
+++ modules/aws-ssosync/outputs.tf
@@ -1,14 +1,14 @@
 output "arn" {
   description = "ARN of the lambda function"
-  value       = local.enabled ? values(aws_lambda_function.ssosync)[*].arn : null
+  value       = local.enabled ? aws_lambda_function.ssosync[*].arn : null
 }
 
 output "invoke_arn" {
   description = "Invoke ARN of the lambda function"
-  value       = local.enabled ? values(aws_lambda_function.ssosync)[*].invoke_arn : null
+  value       = local.enabled ? aws_lambda_function.ssosync[*].invoke_arn : null
 }
 
 output "qualified_arn" {
   description = "ARN identifying your Lambda Function Version (if versioning is enabled via publish = true)"
-  value       = local.enabled ? values(aws_lambda_function.ssosync)[*].qualified_arn : null
+  value       = local.enabled ? aws_lambda_function.ssosync[*].qualified_arn : null
 }
