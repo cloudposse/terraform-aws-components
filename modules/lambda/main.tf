43dbc2f722cc0d06173b3fa29207857d9534aca2
sawani.soman <sawani.soman@savvymoney.com>
2024-01-30 14:41:05 -0800

    fix-lambda-function-name-null

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -2,8 +2,8 @@ locals {
   enabled             = module.this.enabled
   iam_policy_enabled  = local.enabled && (try(length(var.iam_policy), 0) > 0 || var.policy_json != null)
   s3_bucket_full_name = var.s3_bucket_name != null ? format("%s-%s-%s-%s-%s", module.this.namespace, module.this.tenant, module.this.environment, module.this.stage, var.s3_bucket_name) : null
-
-  cicd_s3_key_format = var.cicd_s3_key_format != null ? var.cicd_s3_key_format : "stage/${module.this.stage}/lambda/${var.function_name}/%s"
+  function_name = coalesce(var.function_name, module.label.id)
+  cicd_s3_key_format = var.cicd_s3_key_format != null ? var.cicd_s3_key_format : "stage/${module.this.stage}/lambda/${local.function_name}/%s"
   s3_key             = var.s3_bucket_name == null ? null : (var.s3_key != null ? var.s3_key : format(local.cicd_s3_key_format, coalesce(one(data.aws_ssm_parameter.cicd_ssm_param[*].value), "example")))
 
 }
@@ -53,7 +53,7 @@ module "lambda" {
   source  = "cloudposse/lambda-function/aws"
   version = "0.4.1"
 
-  function_name      = coalesce(var.function_name, module.label.id)
+  function_name      = local.function_name
   description        = var.description
   handler            = var.handler
   lambda_environment = var.lambda_environment
