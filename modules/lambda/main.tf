cfe2b77d1c92aa2666ea3eebb6c8bc49c1abc184
Benjamin Smith <ben.smith.developer@gmail.com>
2024-01-31 14:35:21 -0800

    update with s3 full bucket name var

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,8 +1,10 @@
 locals {
-  enabled            = module.this.enabled
-  iam_policy_enabled = local.enabled && (try(length(var.iam_policy), 0) > 0 || var.policy_json != null)
+  enabled                 = module.this.enabled
+  iam_policy_enabled      = local.enabled && (try(length(var.iam_policy), 0) > 0 || var.policy_json != null)
+  s3_bucket_computed_name = var.s3_bucket_name != null ? format("%s-%s-%s-%s-%s", module.this.namespace, module.this.tenant, module.this.environment, module.this.stage, var.s3_bucket_name) : null
 
-  function_name = coalesce(var.function_name, module.label.id)
+  s3_full_bucket_name = coalesce(var.s3_full_bucket_name, local.s3_bucket_computed_name, "none") == "none" ? null : coalesce(var.s3_full_bucket_name, local.s3_bucket_computed_name)
+  function_name       = coalesce(var.function_name, module.label.id)
 
   cicd_s3_key_format = var.cicd_s3_key_format != null ? var.cicd_s3_key_format : "stage/${module.this.stage}/lambda/${local.function_name}/%s"
   s3_key             = var.s3_bucket_name == null ? null : (var.s3_key != null ? var.s3_key : format(local.cicd_s3_key_format, coalesce(one(data.aws_ssm_parameter.cicd_ssm_param[*].value), "example")))
@@ -62,7 +64,7 @@ module "lambda" {
   image_config       = var.image_config
 
   filename          = var.filename
-  s3_bucket         = var.s3_bucket_name
+  s3_bucket         = local.s3_full_bucket_name
   s3_key            = local.s3_key
   s3_object_version = var.s3_object_version
 
