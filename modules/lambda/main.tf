b53fc16c92be1f8f3cde096148d6589c27bfe61b
Benjamin Smith <ben.smith.developer@gmail.com>
2024-01-31 13:53:56 -0800

    remove formatting of bucket name

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,7 +1,6 @@
 locals {
-  enabled             = module.this.enabled
-  iam_policy_enabled  = local.enabled && (try(length(var.iam_policy), 0) > 0 || var.policy_json != null)
-  s3_bucket_full_name = var.s3_bucket_name != null ? format("%s-%s-%s-%s-%s", module.this.namespace, module.this.tenant, module.this.environment, module.this.stage, var.s3_bucket_name) : null
+  enabled            = module.this.enabled
+  iam_policy_enabled = local.enabled && (try(length(var.iam_policy), 0) > 0 || var.policy_json != null)
 
   cicd_s3_key_format = var.cicd_s3_key_format != null ? var.cicd_s3_key_format : "stage/${module.this.stage}/lambda/${var.function_name}/%s"
   s3_key             = var.s3_bucket_name == null ? null : (var.s3_key != null ? var.s3_key : format(local.cicd_s3_key_format, coalesce(one(data.aws_ssm_parameter.cicd_ssm_param[*].value), "example")))
@@ -61,7 +60,7 @@ module "lambda" {
   image_config       = var.image_config
 
   filename          = var.filename
-  s3_bucket         = local.s3_bucket_full_name
+  s3_bucket         = var.s3_bucket_name
   s3_key            = local.s3_key
   s3_object_version = var.s3_object_version
 
