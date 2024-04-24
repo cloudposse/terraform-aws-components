80988f9150c230c74b37d0d4befaef4ada738ab6
Matt Calhoun <matt@mattcalhoun.com>
2024-04-24 11:23:04 -0400

    Revert "feat: `ec2-instance` Component (#1024)"
    This reverts commit 57b74313e42733edc8d803abc0caf7e646273cbc.

--- modules/ec2-instance/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  value       = module.ec2_instance[*].id
-  description = "Instance ID"
-}
-
-output "private_ip" {
-  value       = module.ec2_instance[*].private_ip
-  description = "Private IP of the instance"
-}
--- modules/ec2-instance/remote-state.tf
+++
@@ -1,8 +0,0 @@
-module "vpc" {
-  source  = "cloudposse/stack-config/yaml//modules/remote-state"
-  version = "1.5.0"
-
-  component = "vpc"
-
-  context = module.this.context
-}
