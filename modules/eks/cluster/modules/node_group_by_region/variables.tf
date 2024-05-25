8eec081dbd7d102c9f7ac12dde990f7b933628d9
Nuru <Nuru@users.noreply.github.com>
2024-05-24 23:20:27 -0700

    Add support for kubelet extra args, etc.

--- modules/eks/cluster/modules/node_group_by_region/variables.tf
+++ modules/eks/cluster/modules/node_group_by_region/variables.tf
@@ -31,6 +31,12 @@ variable "cluster_context" {
       value  = string
       effect = string
     }))
+    node_userdata = object({
+      before_cluster_joining_userdata = optional(string)
+      bootstrap_extra_args            = optional(string)
+      kubelet_extra_args              = optional(string)
+      after_cluster_joining_userdata  = optional(string)
+    })
     kubernetes_version    = string
     module_depends_on     = optional(any)
     resources_to_tag      = list(string)
