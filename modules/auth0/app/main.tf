822f5265841b269573045379796da5527fb4a642
milldr <miller0daniel@gmail.com>
2024-09-04 19:26:20 -0400

    terraform fmt

--- modules/auth0/app/main.tf
+++ modules/auth0/app/main.tf
@@ -1,9 +1,9 @@
 locals {
   enabled = module.this.enabled
 
-  ssm_path                  = coalesce(var.provider_ssm_base_path, module.this.id)
-  client_id_ssm_path  =  format("/%s/client_id", local.ssm_path)
-  client_secret_ssm_path =  format("/%s/client_secret", local.ssm_path)
+  ssm_path               = coalesce(var.provider_ssm_base_path, module.this.id)
+  client_id_ssm_path     = format("/%s/client_id", local.ssm_path)
+  client_secret_ssm_path = format("/%s/client_secret", local.ssm_path)
 }
 
 resource "auth0_client" "this" {
