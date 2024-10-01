f5d46ac5563fbb5342b48f63852b0687e0dc1e67
Igor Rodionov <goruha@gmail.com>
2024-10-01 16:01:35 +0300

    Update versions.tf

--- modules/redshift-serverless/versions.tf
+++ modules/redshift-serverless/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 4.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = ">= 3.0"
     }
   }
 }
