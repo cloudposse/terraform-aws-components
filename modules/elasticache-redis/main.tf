29f90e02017d201bfedc29e89998763d38c3e9fb
RB <7775707+nitrocode@users.noreply.github.com>
2024-10-30 16:06:10 -0500

    feat(elasticache-redis): add engine input for valkey support (#1170)

--- modules/elasticache-redis/main.tf
+++ modules/elasticache-redis/main.tf
@@ -69,6 +69,7 @@ module "redis_clusters" {
   num_replicas           = lookup(each.value, "num_replicas", 1)
   num_shards             = lookup(each.value, "num_shards", 0)
   replicas_per_shard     = lookup(each.value, "replicas_per_shard", 0)
+  engine                 = lookup(each.value, "engine", "redis")
   engine_version         = each.value.engine_version
   create_parameter_group = lookup(each.value, "create_parameter_group", true)
   parameters             = lookup(each.value, "parameters", null)
